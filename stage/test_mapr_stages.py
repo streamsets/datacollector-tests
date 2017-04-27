ee5a922d708ea6771bc18ada5fd6912074e6cff0
Dima Spivak <dima@streamsets.com>
2017-04-27 16:12:45 -0700

    SDC-6003. Write MapR FS origin upgrade test
    Change-Id: I3c8a61ca38917c945d9296e0d09267d6ed430631
    Reviewed-on: https://review.streamsets.net/7984
    Reviewed-by: Kirti Velankar <kirti@streamsets.com>

--- stage/test_mapr_stages.py
+++ stage/test_mapr_stages.py
@@ -15,6 +15,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+"""The tests in this module follow a pattern of creating pipelines with
+:py:obj:`testframework.sdc_models.PipelineBuilder` in one version of SDC and then importing and running them in
+another.
+"""
+
 import logging
 import os
 import string
@@ -27,23 +33,10 @@ from testframework.utils import get_random_string
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-@pytest.fixture(scope='module')
-def cluster(args):
-    return environment.Cluster()
-
-@pytest.fixture(scope='module')
-def data_collector(cluster):
-    data_collector = sdc.DataCollector()
-    data_collector.configure_for_environment(cluster)
-    data_collector.start()
-    yield data_collector
-    if data_collector.tear_down_on_exit:
-        data_collector.tear_down()
-
 # Specify a port for SDC RPC stages to use.
 SDC_RPC_PORT = 20000
 
-def test_mapr_fs_origin_simple(data_collector, cluster):
+def test_mapr_fs_origin_simple(sdc_builder, sdc_executor, cluster):
     """Write a simple file into a MapR FS folder with a randomly-generated name and confirm that the MapR FS origin
     successfully reads it. Because cluster mode pipelines don't support snapshots, we do this verification using a
     second standalone pipeline whose origin is an SDC RPC written to by the MapR FS pipeline. Specifically, this would
@@ -58,7 +51,7 @@ def test_mapr_fs_origin_simple(data_collector, cluster):
     mapr_fs_folder = os.path.join(os.sep, get_random_string(string.ascii_letters, 10))
 
     # Build the MapR FS pipeline.
-    builder = data_collector.get_pipeline_builder()
+    builder = sdc_builder.get_pipeline_builder()
     builder.add_error_stage('Discard')
 
     mapr_fs_origin = builder.add_stage(name='com_streamsets_pipeline_stage_origin_maprfs_ClusterMapRFSDSource')
@@ -66,16 +59,16 @@ def test_mapr_fs_origin_simple(data_collector, cluster):
     mapr_fs_origin.input_paths.append(mapr_fs_folder)
 
     sdc_rpc_destination = builder.add_stage(name='com_streamsets_pipeline_stage_destination_sdcipc_SdcIpcDTarget')
-    sdc_rpc_destination.rpc_connections.append('{}:{}'.format(data_collector.server_host,
+    sdc_rpc_destination.rpc_connections.append('{}:{}'.format(sdc_executor.server_host,
                                                               SDC_RPC_PORT))
     sdc_rpc_destination.rpc_id = get_random_string(string.ascii_letters, 10)
 
     mapr_fs_origin >> sdc_rpc_destination
-    mapr_fs_pipeline = builder.build(title='MapR FS pipeline')
+    mapr_fs_pipeline = builder.build(title='MapR FS pipeline').configure_for_environment(cluster)
     mapr_fs_pipeline.configuration['executionMode'] = 'CLUSTER_BATCH'
 
     # Build the Snapshot pipeline.
-    builder = data_collector.get_pipeline_builder()
+    builder = sdc_builder.get_pipeline_builder()
     builder.add_error_stage('Discard')
 
     sdc_rpc_origin = builder.add_stage(name='com_streamsets_pipeline_stage_origin_sdcipc_SdcIpcDSource')
@@ -91,7 +84,7 @@ def test_mapr_fs_origin_simple(data_collector, cluster):
     snapshot_pipeline = builder.build(title='Snapshot pipeline')
 
     # Add both pipelines we just created to SDC and start writing files to MapR FS with the HDFS client.
-    data_collector.add_pipeline(mapr_fs_pipeline, snapshot_pipeline)
+    sdc_executor.add_pipeline(mapr_fs_pipeline, snapshot_pipeline)
 
     try:
         lines_in_file = ['hello', 'hi', 'how are you?']
@@ -105,14 +98,14 @@ def test_mapr_fs_origin_simple(data_collector, cluster):
         # wait_for_finished function. That way, we can switch over and start the MapR FS pipeline. Once that one
         # completes, we can go back and do an assert on the snapshot pipeline's snapshot.
         logger.debug('Starting snapshot pipeline and capturing snapshot ...')
-        snapshot_pipeline_command = data_collector.capture_snapshot(snapshot_pipeline,
-                                                                    start_pipeline=True)
+        snapshot_pipeline_command = sdc_executor.capture_snapshot(snapshot_pipeline,
+                                                                  start_pipeline=True)
 
         logger.debug('Starting MapR FS pipeline and waiting for it to finish ...')
-        data_collector.start_pipeline(mapr_fs_pipeline).wait_for_finished()
+        sdc_executor.start_pipeline(mapr_fs_pipeline).wait_for_finished()
 
         snapshot = snapshot_pipeline_command.wait_for_finished().snapshot
-        data_collector.stop_pipeline(snapshot_pipeline)
+        sdc_executor.stop_pipeline(snapshot_pipeline)
         lines_from_snapshot = [record.value['value']['text']['value']
                                for record in snapshot[snapshot_pipeline[0].instance_name].output]
 
