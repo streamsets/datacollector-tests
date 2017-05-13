df7b3a44498e269ae73fb10607617872c3178515
Dima Spivak <dima@streamsets.com>
2017-05-12 20:34:37 -0700

    SDC-6184. Convert test_users.py to use PipelineBuilder
    Change-Id: Ica805860eb95118b6758e0f91d01400b62a40e8c
    Reviewed-on: https://review.streamsets.net/8252
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

--- pipeline/test_users.py
+++ pipeline/test_users.py
@@ -25,66 +25,75 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 @pytest.fixture(scope='module')
-def dc(args, pipeline):
-    dc = sdc.DataCollector(version=args.sdc_version)
-    dc.add_user("jarcec", roles=["admin"], groups=["jarcec", "employee"])
-    dc.add_user("dima", roles=["admin"], groups=["dima", "employee"])
-    dc.add_user("bryan", roles=["manager", "creator"], groups=["bryan", "contractor"])
-    dc.add_user("arvind", roles=["guest"], groups=["arvind", "guests"])
-    # The way our framework works, this pipeline will always be created as user "admin"
-    dc.add_pipeline(pipeline)
-    dc.start()
-    yield dc
-    dc.tear_down()
+def data_collector():
+    data_collector = sdc.DataCollector()
+    data_collector.add_user('jarcec', roles=['admin'], groups=['jarcec', 'employee'])
+    data_collector.add_user('dima', roles=['admin'], groups=['dima', 'employee'])
+    data_collector.add_user('bryan', roles=['manager', 'creator'], groups=['bryan', 'contractor'])
+    data_collector.add_user('arvind', roles=['guest'], groups=['arvind', 'guests'])
+    data_collector.start()
+    yield data_collector
+    if data_collector.tear_down_on_exit:
+        data_collector.tear_down()
+
 
 @pytest.fixture(scope='module')
-def pipeline():
-    pipeline = sdc_models.Pipeline(join(dirname(__file__),
-                                   'pipelines',
-                                   'random_expression_trash.json'))
+def pipeline(data_collector):
+    builder = data_collector.get_pipeline_builder()
+
+    dev_data_generator = builder.add_stage('Dev Data Generator')
+    trash = builder.add_stage('Trash')
+
+    dev_data_generator >> trash
+
+    pipeline = builder.build()
+    data_collector.set_user('admin')
+    data_collector.add_pipeline(pipeline)
+
     yield pipeline
 
-# Validate "current" user switching and getting the proper groups and roles.
-def test_current_user(dc):
-    dc.set_user("admin")
-    user = dc.current_user()
-    assert user.name == "admin"
 
-    dc.set_user("jarcec")
-    user = dc.current_user()
-    assert user.name == "jarcec"
-    assert user.groups == ["all", "jarcec", "employee"]
-    assert user.roles == ["admin"]
+# Validate "current" user switching and getting the proper groups and roles.
+def test_current_user(data_collector):
+    data_collector.set_user('admin')
+    user = data_collector.current_user()
+    assert user.name == 'admin'
+
+    data_collector.set_user('jarcec')
+    user = data_collector.current_user()
+    assert user.name == 'jarcec'
+    assert user.groups == ['all', 'jarcec', 'employee']
+    assert user.roles == ['admin']
+
 
 # Ensure that the operations are indeed executed by the current user.
-def test_pipeline_history(dc, pipeline):
-    dc.set_user("jarcec")
-    dc.start_pipeline(pipeline).wait_for_status('RUNNING')
+def test_pipeline_history(data_collector, pipeline):
+    data_collector.set_user('jarcec')
+    data_collector.start_pipeline(pipeline).wait_for_status('RUNNING')
 
-    dc.set_user("dima")
-    dc.stop_pipeline(pipeline).wait_for_stopped()
+    data_collector.set_user('dima')
+    data_collector.stop_pipeline(pipeline).wait_for_stopped()
 
-    history = dc.pipeline_history(pipeline)
+    history = data_collector.pipeline_history(pipeline)
 
     # History is in descending order.
 
     entry = history.entries[0]
-    assert entry["user"] == "dima"
-    assert entry["status"] == "STOPPED"
+    assert entry['user'] == 'dima'
+    assert entry['status'] == 'STOPPED'
 
     entry = history.entries[1]
-    assert entry["user"] == "dima"
-    assert entry["status"] == "STOPPING"
+    assert entry['user'] == 'dima'
+    assert entry['status'] == 'STOPPING'
 
     entry = history.entries[2]
-    assert entry["user"] == "jarcec"
-    assert entry["status"] == "RUNNING"
+    assert entry['user'] == 'jarcec'
+    assert entry['status'] == 'RUNNING'
 
     entry = history.entries[3]
-    assert entry["user"] == "jarcec"
-    assert entry["status"] == "STARTING"
+    assert entry['user'] == 'jarcec'
+    assert entry['status'] == 'STARTING'
 
     entry = history.entries[4]
-    assert entry["user"] == "admin"
-    assert entry["status"] == "EDITED"
-
+    assert entry['user'] == 'admin'
+    assert entry['status'] == 'EDITED'
