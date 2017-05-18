458b964c2d93f7569a3438e11990f943c44aaec9
Dima Spivak <dima@streamsets.com>
2017-05-18 15:04:21 -0700

    SDC-6131. Write STF test for HBase lookup processor
    Change-Id: Ie416fe48b9c071c8b46847c5fd1967486a33974a
    Reviewed-on: https://review.streamsets.net/8358
    Reviewed-by: Kirti Velankar <kirti@streamsets.com>

--- stage/test_cdh_stages.py
+++ stage/test_cdh_stages.py
@@ -163,6 +163,64 @@ def test_hbase_destination(sdc_builder, sdc_executor, cluster):
 
 
 @cluster('cdh')
+def test_hbase_lookup_processor(sdc_builder, sdc_executor, cluster):
+    """Simple HBase Lookup processor test.
+
+    Pipeline will enrich records with the name of Grand Tours by adding a field containing the year
+    of their first editions, which will come from an HBase table.
+
+    dev_raw_data_source >> hbase_lookup >> trash
+    """
+    # Generate some silly data.
+    bike_races = [dict(name='Tour de France', first_edition='1903'),
+                  dict(name="Giro d'Italia", first_edition='1909'),
+                  dict(name='Vuelta a Espana', first_edition='1935')]
+
+    # Convert to raw data for the Dev Raw Data Source.
+    raw_data = '\n'.join(bike_race['name'] for bike_race in bike_races)
+
+    # Generate HBase Lookup's attributes.
+    lookup_parameters = [dict(rowExpr="${record:value('/text')}",
+                             columnExpr='info:first_edition',
+                             outputFieldPath='/founded',
+                             timestampExpr='')]
+    table_name = get_random_string(string.ascii_letters, 10)
+
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+    dev_raw_data_source.set_attributes(data_format='TEXT', raw_data=raw_data)
+
+    hbase_lookup = pipeline_builder.add_stage('HBase Lookup')
+    hbase_lookup.set_attributes(lookup_parameters=lookup_parameters, table_name=table_name)
+
+    trash = pipeline_builder.add_stage('Trash')
+
+    dev_raw_data_source >> hbase_lookup >> trash
+    pipeline = pipeline_builder.build().configure_for_environment(cluster)
+    sdc_executor.add_pipeline(pipeline)
+
+    try:
+        logger.info('Creating HBase table %s ...', table_name)
+        cluster.hbase.client.create_table(name=table_name, families={'info': {}})
+        # Use HappyBase's `Batch` instance to avoid unnecessary calls to HBase.
+        batch = cluster.hbase.client.table(table_name).batch()
+        for bike_race in bike_races:
+            # Use of str.encode() below is because HBase (and HappyBase) speaks in byte arrays.
+            batch.put(bike_race['name'].encode(), {b'info:first_edition': bike_race['first_edition'].encode()})
+        batch.send()
+
+        snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True).wait_for_finished().snapshot
+        sdc_executor.stop_pipeline(pipeline)
+
+        assert [dict(name=record.value['value']['text']['value'],
+                     first_edition=record.value['value']['founded']['value'])
+                for record in snapshot[hbase_lookup.instance_name].output] == bike_races
+    finally:
+        logger.info('Deleting HBase table %s ...', table_name)
+        cluster.hbase.client.delete_table(name=table_name, disable=True)
+
+
+@cluster('cdh')
 def test_kafka_destination(sdc_builder, sdc_executor, cluster):
     """Send simple text messages into Kafka Destination from Dev Raw Data Source and
        confirm that Kafka successfully reads them using KafkaConsumer from cluster.
