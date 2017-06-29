5aeebe5c30a7873cf2f2e1df321ec0d20d07e75e
Srid Banoor <srid@streamsets.com>
2017-06-29 14:54:50 -0700

    TEST-134. MongoDB marker related changes
    Change-Id: I2447dfa45a7bd701cb7f504c07372126f1a0c5c8
    Reviewed-on: https://review.streamsets.net/9054
    Reviewed-by: Dima Spivak <dima@streamsets.com>
    Reviewed-by: Kirti Velankar <kirti@streamsets.com>

--- stage/test_mongodb_stages.py
+++ stage/test_mongodb_stages.py
@@ -16,14 +16,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""The tests in this module follow a pattern of creating pipelines with
+:py:obj:`testframework.sdc_models.PipelineBuilder` in one version of SDC and then importing and running them in
+another.
+"""
+
 import copy
 import logging
 import time
 from string import ascii_letters
 
-import pymongo
-
-from testframework.markers import *
+from testframework.markers import mongodb
 from testframework.utils import get_random_string
 
 logger = logging.getLogger(__name__)
@@ -38,10 +41,11 @@ DATA = ['To be or not to be.',
         'Excellence is not an act, it is a habit.',
         'No pains, no gains.']
 
-@database
-def test_mongodb_oplog_origin(sdc_builder, sdc_executor, database):
+
+@mongodb
+def test_mongodb_oplog_origin(sdc_builder, sdc_executor, mongodb):
     """
-    Insert data in MongoDB and then check if MongoDB Oplog origin captures changes in data or the database correctly.
+    Insert data in MongoDB and then check if MongoDB Oplog origin captures changes in data from MongoDB correctly.
 
     The pipeline looks like:
         mongodb_oplog >> trash
@@ -57,13 +61,13 @@ def test_mongodb_oplog_origin(sdc_builder, sdc_executor, database):
 
     trash = pipeline_builder.add_stage('Trash')
     mongodb_oplog >> trash
-    pipeline = pipeline_builder.build().configure_for_environment(database)
+    pipeline = pipeline_builder.build().configure_for_environment(mongodb)
 
     try:
         # Insert documents in MongoDB using PyMongo.
         # First a database is created. Then a collection is created inside that database.
         # Then documents are inserted in that collection.
-        mongodb_database = database.engine[database_name]
+        mongodb_database = mongodb.engine[database_name]
         mongodb_collection = mongodb_database[get_random_string(ascii_letters, 10)]
         input_rec_count = 6
         inserted_list = mongodb_collection.insert_many([{'x': i} for i in range(input_rec_count)])
@@ -83,10 +87,11 @@ def test_mongodb_oplog_origin(sdc_builder, sdc_executor, database):
 
     finally:
         logger.info('Dropping %s database...', database_name)
-        database.engine.drop_database(database_name)
+        mongodb.engine.drop_database(database_name)
 
-@database
-def test_mongodb_origin_simple(sdc_builder, sdc_executor, database):
+
+@mongodb
+def test_mongodb_origin_simple(sdc_builder, sdc_executor, mongodb):
     """
     Create 3 simple documents in MongoDB and confirm that MongoDB origin reads them.
 
@@ -103,21 +108,21 @@ def test_mongodb_origin_simple(sdc_builder, sdc_executor, database):
 
     trash = pipeline_builder.add_stage('Trash')
     mongodb_origin >> trash
-    pipeline = pipeline_builder.build().configure_for_environment(database)
+    pipeline = pipeline_builder.build().configure_for_environment(mongodb)
 
     try:
-        # MongoDB and PyMongo add '_id' to the dictionary entries e.g. DOCS_IN_DATABASE
+        # MongoDB and PyMongo add '_id' to the dictionary entries e.g. docs_in_database
         # when used for inserting in collection. Hence the deep copy.
-        DOCS_IN_DATABASE = copy.deepcopy(ORIG_DOCS)
+        docs_in_database = copy.deepcopy(ORIG_DOCS)
 
         # Create documents in MongoDB using PyMongo.
         # First a database is created. Then a collection is created inside that database.
         # Then documents are created in that collection.
         logger.info('Adding documents into %s collection using PyMongo...', mongodb_origin.collection)
-        mongodb_database = database.engine[mongodb_origin.database]
+        mongodb_database = mongodb.engine[mongodb_origin.database]
         mongodb_collection = mongodb_database[mongodb_origin.collection]
-        insert_list = [mongodb_collection.insert_one(doc) for doc in DOCS_IN_DATABASE]
-        assert len(insert_list) == len(DOCS_IN_DATABASE)
+        insert_list = [mongodb_collection.insert_one(doc) for doc in docs_in_database]
+        assert len(insert_list) == len(docs_in_database)
 
         # Start pipeline and verify the documents using snaphot.
         sdc_executor.add_pipeline(pipeline)
@@ -132,10 +137,11 @@ def test_mongodb_origin_simple(sdc_builder, sdc_executor, database):
 
     finally:
         logger.info('Dropping %s database...', mongodb_origin.database)
-        database.engine.drop_database(mongodb_origin.database)
+        mongodb.engine.drop_database(mongodb_origin.database)
 
-@database
-def test_mongodb_destination(sdc_builder, sdc_executor, database):
+
+@mongodb
+def test_mongodb_destination(sdc_builder, sdc_executor, mongodb):
     """
     Send simple text into MongoDB destination from Dev Raw Data Source and
         confirm that MongoDB correctly received them using PyMongo.
@@ -150,9 +156,10 @@ def test_mongodb_destination(sdc_builder, sdc_executor, database):
     dev_raw_data_source.set_attributes(data_format='TEXT', raw_data='\n'.join(DATA))
 
     expression_evaluator = pipeline_builder.add_stage('Expression Evaluator')
-    # MongoDB destination uses the CRUD operation in the sdc.operation.type record header attribute when writing to MongoDB.
-    # value 4 specified below is for UPSERT.
-    expression_evaluator.header_expressions = [{'attributeToSet': 'sdc.operation.type', 'headerAttributeExpression': '4'}]
+    # MongoDB destination uses the CRUD operation in the sdc.operation.type record header attribute when writing
+    # to MongoDB. Value 4 specified below is for UPSERT.
+    expression_evaluator.header_expressions = [{'attributeToSet': 'sdc.operation.type',
+                                                'headerAttributeExpression': '4'}]
 
     mongodb_dest = pipeline_builder.add_stage('MongoDB', type='destination')
     mongodb_dest.set_attributes(database=get_random_string(ascii_letters, 5),
@@ -160,7 +167,7 @@ def test_mongodb_destination(sdc_builder, sdc_executor, database):
                                 unique_key_field='/text')
 
     dev_raw_data_source >> expression_evaluator >> mongodb_dest
-    pipeline = pipeline_builder.build().configure_for_environment(database)
+    pipeline = pipeline_builder.build().configure_for_environment(mongodb)
 
     try:
         # Data is generated in dev_raw_data_source and sent to MongoDB using pipeline.
@@ -171,9 +178,8 @@ def test_mongodb_destination(sdc_builder, sdc_executor, database):
         # Verify data is received correctly using PyMongo.
         # Similar to writing, while reading data, we specify MongoDB database and the collection inside it.
         logger.info('Verifying docs received with PyMongo...')
-        assert [item['text'] for item in database.engine[mongodb_dest.database][mongodb_dest.collection].find()] == DATA
+        assert [item['text'] for item in mongodb.engine[mongodb_dest.database][mongodb_dest.collection].find()] == DATA
 
     finally:
         logger.info('Dropping %s database...', mongodb_dest.database)
-        database.engine.drop_database(mongodb_dest.database)
-
+        mongodb.engine.drop_database(mongodb_dest.database)
