676ff760668c3d54ac6198fcdf9f4814d2b03ab7
Kirti Velankar <kirti@streamsets.com>
2017-08-08 15:44:46 -0700

    SDC-6916. Write STF test for Google Pub-Sub destination
    Change-Id: Id39182b230a0625576e7df74a62751c242c8ae2f
    Reviewed-on: https://review.streamsets.net/9736
    Reviewed-by: Sridhar Banoor <sridhar@streamsets.com>

---
+++ stage/test_gcp_stages.py
@@ -0,0 +1,161 @@
+# Copyright 2017 StreamSets Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""The tests in this module follow a pattern of creating pipelines with
+:py:obj:`testframework.sdc_models.PipelineBuilder` in one version of SDC and then importing and running them in
+another.
+"""
+
+import logging
+import time
+from string import ascii_letters
+
+from testframework.markers import gcp
+from testframework.utils import get_random_string
+
+logger = logging.getLogger(__name__)
+MSG_DATA = 'Hello World from SDC and DPM!'
+
+@gcp
+def test_google_pubsub_subscriber(sdc_builder, sdc_executor, gcp):
+    """
+    Publish messages using Google pub/sub client and then check if Google pub/sub subscriber receives them using snapshot.
+
+    The pipeline looks like:
+        google_pubsub_subscriber >> trash
+    """
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    subscription_id = get_random_string(ascii_letters, 5)
+    topic_name = get_random_string(ascii_letters, 5)
+    google_pubsub_subscriber = pipeline_builder.add_stage('Google PubSub Subscriber')
+    google_pubsub_subscriber.set_attributes(subscription_id=subscription_id,
+                                                   data_format='TEXT',
+                                                   max_batch_size=10,
+                                                   batch_wait_time_in_millisecs=20000)
+
+    trash = pipeline_builder.add_stage('Trash')
+    google_pubsub_subscriber >> trash
+    pipeline = pipeline_builder.build(title='Google Pub/Sub Subscriber').configure_for_environment(gcp)
+
+    try:
+        # Using Google pub/sub client, create topic and subscription
+        logger.info('Creating topic %s using Google pub/sub client ...', topic_name)
+        pubsub_client = gcp.pubsub_client
+        topic = pubsub_client.topic(topic_name)
+        topic.create()
+        assert topic.exists()
+        subscription = topic.subscription(subscription_id)
+        subscription.create()
+        assert subscription.exists()
+
+        # Start pipeline and verify messages are received using snaphot.
+        logger.info('Starting pipeline and snapshot')
+        sdc_executor.add_pipeline(pipeline)
+        snapshot_command = sdc_executor.capture_snapshot(pipeline, start_pipeline=True)
+        sdc_executor.get_status_pipeline(pipeline).wait_for_status('RUNNING')
+
+        logger.info('Publishing messages to topic %s using Google pub/sub client ...', topic_name)
+        encoded_msg_data = MSG_DATA.encode()
+        for _ in range(10):
+            topic.publish(encoded_msg_data)
+
+        snapshot = snapshot_command.wait_for_finished().snapshot
+        sdc_executor.stop_pipeline(pipeline)
+        rows_from_snapshot = [record.value['value']['text']['value']
+                              for record in snapshot[google_pubsub_subscriber].output]
+
+        assert rows_from_snapshot == [MSG_DATA] * 10
+
+    finally:
+        logger.info('Deleting topic %s and subscription %s ...', topic_name, subscription_id)
+        if subscription is not None:
+            subscription.delete()
+        if topic is not None:
+            topic.delete()
+
+
+@gcp
+def test_google_pubsub_publisher(sdc_builder, sdc_executor, gcp):
+    """
+    Send simple text messages to Google pub/sub from Dev Raw Data Source and
+    confirm that Google pub/sub successfully reads them using Google pub/sub client from gcp.
+
+    The pipeline looks like:
+        dev_raw_data_source >> google_pubsub_publisher
+    """
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+    dev_raw_data_source.data_format = 'TEXT'
+    dev_raw_data_source.raw_data = MSG_DATA
+
+    subscription_id = get_random_string(ascii_letters, 5)
+    topic_name = get_random_string(ascii_letters, 5)
+    google_pubsub_publisher = pipeline_builder.add_stage('Google PubSub Publisher')
+    google_pubsub_publisher.set_attributes(topic_id=topic_name,
+                                                       data_format='TEXT')
+
+    dev_raw_data_source >> google_pubsub_publisher
+    pipeline = pipeline_builder.build(title='Google Pub/Sub Publisher').configure_for_environment(gcp)
+    pipeline.rate_limit = 1
+
+    try:
+        # Using Google pub/sub client, create topic and subscription
+        logger.info('Creating topic %s using Google pub/sub client ...', topic_name)
+        pubsub_client = gcp.pubsub_client
+        topic = pubsub_client.topic(topic_name)
+        topic.create()
+        assert topic.exists()
+        logger.info('Creating subscription')
+        subscription = topic.subscription(subscription_id)
+        subscription.create()
+        assert subscription.exists()
+
+        # Send messages using pipeline to Google pub/sub publisher Destination.
+        logger.debug('Starting Publisher pipeline and waiting for it to produce records ...')
+        sdc_executor.add_pipeline(pipeline)
+        sdc_executor.start_pipeline(pipeline).wait_for_pipeline_batch_count(10)
+
+        logger.debug('Stopping Publisher pipeline and getting the count of records produced in total ...')
+        sdc_executor.stop_pipeline(pipeline).wait_for_stopped()
+
+        history = sdc_executor.pipeline_history(pipeline)
+        msgs_sent_count = history.latest.metrics.counter('pipeline.batchOutputRecords.counter').count
+        logger.info('No. of messages sent in the pipeline = %s', msgs_sent_count)
+
+        # Receive messages in a loop until timeout or expected no. of messages are received
+        results = []
+        msgs_received_count = msgs_sent_count
+        timeout = 5 # in seconds
+        start_time = time.time()
+        while time.time() < start_time + timeout and msgs_received_count > 0:
+            messages = subscription.pull(max_messages=msgs_sent_count)
+            results.extend(messages)
+            msgs_received_count  = msgs_received_count - len(messages)
+
+        # Acknowldege the received messages so that the pub/sub does not resend them
+        if results:
+            subscription.acknowledge([ack_id for ack_id, message in results])
+
+        # Verify
+        msgs_received = [message[1].data.decode().rstrip('\n') for message in results]
+        assert msgs_received == [dev_raw_data_source.raw_data] * msgs_sent_count
+
+    finally:
+        logger.info('Deleting topic %s and subscription %s ...', topic_name, subscription_id)
+        if subscription is not None:
+            subscription.delete()
+        if topic is not None:
+            topic.delete()
