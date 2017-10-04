415b70f2dafd2edea508f808548e7825181b85c5
Jeff Evans <jeff@streamsets.com>
2017-10-04 13:40:43 -0500

    SDC-7464. Fix JDBC multitable test to wait for pipeline stop before dropping tables
    Add wait_for_stopped() call for pipeline stop
    
    Change-Id: I2467a57ec87e54944bef1a18d68e504ba78199ed
    Reviewed-on: https://review.streamsets.net/10732
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

--- pipeline/test_jdbc_multitable_multithreaded.py
+++ pipeline/test_jdbc_multitable_multithreaded.py
@@ -195,6 +195,6 @@ def test_jdbc_multitable_consumer_to_jdbc(sdc_builder, sdc_executor, database,
         wait_for_no_data_event(database)
         assert_tables_replicated(database, src_table_names)
     finally:
-        sdc_executor.stop_pipeline(pipeline) # must to stop pipeline before tables can be dropped.
+        sdc_executor.stop_pipeline(pipeline).wait_for_stopped() # must to stop pipeline before tables can be dropped.
         logger.info('Dropping test related tables in %s database...', database.type)
         teardown_tables(database, src_table_names + target_table_names + [EVENT_TABLE_NAME])
