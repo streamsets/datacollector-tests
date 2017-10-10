2a9fb3526320764f7c3d33c27c096d545a106a98
Kirti Velankar <kirti@streamsets.com>
2017-10-09 22:03:35 -0700

    SDC-6626. Write STF test for Salesforce Lookup processor
    Change-Id: Id344e2da1e4f641148c87c52c47b01e86fd34841
    Reviewed-on: https://review.streamsets.net/10792
    Reviewed-by: Dima Spivak <dima@streamsets.com>

--- stage/test_salesforce_stages.py
+++ stage/test_salesforce_stages.py
@@ -11,12 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-"""The tests in this module follow a pattern of creating pipelines with
-:py:obj:`testframework.sdc_models.PipelineBuilder` in one version of SDC and then importing and running them in
-another.
-"""
-
+import copy
 import logging
 import os
 import time
@@ -26,14 +21,11 @@ from testframework.markers import salesforce
 
 logger = logging.getLogger(__name__)
 
-CSV_DATA_TO_INSERT = ['firstName,lastName,email',
-                      'Test1,User1,xtest1@example.com',
-                      'Test2,User2,xtest2@example.com',
-                      'Test3,User3,xtest3@example.com']
-
 DATA_TO_INSERT = [{'FirstName': 'Test1', 'LastName': 'User1', 'Email': 'xtest1@example.com'},
                   {'FirstName': 'Test2', 'LastName': 'User2', 'Email': 'xtest2@example.com'},
                   {'FirstName': 'Test3', 'LastName': 'User3', 'Email': 'xtest3@example.com'}]
+CSV_DATA_TO_INSERT = [','.join(DATA_TO_INSERT[0].keys())] + [','.join(item.values()) for item in DATA_TO_INSERT]
+LOOKUP_RAW_DATA = ['Email'] + [row['Email'] for row in DATA_TO_INSERT]
 
 
 @salesforce
@@ -46,16 +38,12 @@ def test_salesforce_destination(sdc_builder, sdc_executor, salesforce):
         dev_raw_data_source >> salesforce_destination
     """
     pipeline_builder = sdc_builder.get_pipeline_builder()
-
-    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
-    dev_raw_data_source.set_attributes(data_format='DELIMITED',
-                                       header_line='WITH_HEADER',
-                                       raw_data='\n'.join(CSV_DATA_TO_INSERT))
+    dev_raw_data_source = get_dev_raw_data_source(pipeline_builder, CSV_DATA_TO_INSERT)
 
     salesforce_destination = pipeline_builder.add_stage('Salesforce', type='destination')
-    field_mapping = [{'sdcField': '/firstName', 'salesforceField': 'FirstName'},
-                     {'sdcField': '/lastName', 'salesforceField': 'LastName'},
-                     {'sdcField': '/email', 'salesforceField': 'Email'}]
+    field_mapping = [{'sdcField': '/FirstName', 'salesforceField': 'FirstName'},
+                     {'sdcField': '/LastName', 'salesforceField': 'LastName'},
+                     {'sdcField': '/Email', 'salesforceField': 'Email'}]
     salesforce_destination.set_attributes(default_operation='INSERT',
                                           field_mapping=field_mapping,
                                           sobject_type='Contact')
@@ -66,15 +54,14 @@ def test_salesforce_destination(sdc_builder, sdc_executor, salesforce):
     sdc_executor.add_pipeline(pipeline)
     read_ids = None
 
+    client = salesforce.client
     try:
         # Produce Salesforce records using pipeline.
-        logger.info('Starting Salesforce destination pipeline and waiting for it to produce records...')
+        logger.info('Starting Salesforce destination pipeline and waiting for it to produce records ...')
         sdc_executor.start_pipeline(pipeline).wait_for_pipeline_batch_count(1)
-
-        sdc_executor.stop_pipeline(pipeline).wait_for_stopped()
+        sdc_executor.stop_pipeline(pipeline)
 
         # Using Salesforce connection, read the contents in the Salesforce destination.
-        client = salesforce.client
         # Changing " with ' and vice versa in following string makes the query execution fail.
         query_str = "SELECT Id, FirstName, LastName, Email FROM Contact WHERE Email LIKE 'xtest%' ORDER BY Id"
         result = client.query(query_str)
@@ -87,9 +74,8 @@ def test_salesforce_destination(sdc_builder, sdc_executor, salesforce):
         assert CSV_DATA_TO_INSERT[1:] == read_data
 
     finally:
-        if client is not None and read_ids is not None:
-            logger.info('Deleting records ...')
-            client.bulk.Contact.delete(read_ids)
+        logger.info('Deleting records ...')
+        client.bulk.Contact.delete(read_ids)
 
 @salesforce
 def test_salesforce_origin(sdc_builder, sdc_executor, salesforce):
@@ -116,33 +102,72 @@ def test_salesforce_origin(sdc_builder, sdc_executor, salesforce):
     pipeline = pipeline_builder.build(title='Salesforce Origin').configure_for_environment(salesforce)
     sdc_executor.add_pipeline(pipeline)
 
-    client = salesforce.client
-    inserted_ids = None
+    verify_by_snapshot(sdc_executor, pipeline, salesforce_origin, DATA_TO_INSERT, salesforce)
 
+
+def get_dev_raw_data_source(pipeline_builder, raw_data):
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+    dev_raw_data_source.set_attributes(data_format='DELIMITED',
+                                       header_line='WITH_HEADER',
+                                       raw_data='\n'.join(raw_data))
+    return dev_raw_data_source
+
+
+def verify_by_snapshot(sdc_executor, pipeline, stage_name, expected_data, salesforce):
+    client = salesforce.client
     try:
-        # Using Salesforce client, create  rows in Contact
-        logger.info('Creating rows using Salesforce client...')
+        # Using Salesforce client, create rows in Contact.
+        logger.info('Creating rows using Salesforce client ...')
         client.bulk.Contact.insert(DATA_TO_INSERT)
 
-        # Start pipeline and verify correct rows are received using snaphot.
         logger.info('Starting pipeline and snapshot')
         snapshot_command = sdc_executor.capture_snapshot(pipeline, start_pipeline=True)
-        sdc_executor.get_status_pipeline(pipeline).wait_for_status('RUNNING')
         snapshot = snapshot_command.wait_for_finished().snapshot
         rows_from_snapshot = [record.value['value']
-                              for record in snapshot[salesforce_origin].output]
+                              for record in snapshot[stage_name].output]
 
         inserted_ids = [dict([(rec['sqpath'].strip('/'), rec['value'])
                              for rec in item if rec['sqpath'] == '/Id'])
                         for item in rows_from_snapshot]
 
+        # Verify correct rows are received using snaphot.
         data_from_snapshot = [dict([(rec['sqpath'].strip('/'), rec['value'])
                                    for rec in item if rec['sqpath'] != '/Id'])
                               for item in rows_from_snapshot]
-
-        assert data_from_snapshot == DATA_TO_INSERT
+        assert data_from_snapshot == expected_data
 
     finally:
-        if client is not None and inserted_ids is not None:
-            logger.info('Deleting records...')
-            client.bulk.Contact.delete(inserted_ids)
+        logger.info('Deleting records ...')
+        client.bulk.Contact.delete(inserted_ids)
+
+
+@salesforce
+def test_salesforce_lookup_processor(sdc_builder, sdc_executor, salesforce):
+    """Simple Salesforce Lookup processor test.
+    Pipeline will enrich records with the 'LastName' of contacts by adding a field as 'surName'.
+
+    The pipeline looks like:
+        dev_raw_data_source >> salesforce_lookup >> trash
+    """
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+    dev_raw_data_source = get_dev_raw_data_source(pipeline_builder, LOOKUP_RAW_DATA)
+
+    salesforce_lookup = pipeline_builder.add_stage('Salesforce Lookup')
+    # Changing " with ' and vice versa in following string makes the query execution fail.
+    query_str = ("SELECT Id, FirstName, LastName FROM Contact "
+                 "WHERE Email = '${record:value(\"/Email\")}'")
+    field_mappings = [dict(dataType='USE_SALESFORCE_TYPE',
+                           salesforceField='LastName',
+                           sdcField='/surName')]
+    salesforce_lookup.set_attributes(soql_query=query_str,
+                                     field_mappings=field_mappings)
+
+    trash = pipeline_builder.add_stage('Trash')
+    dev_raw_data_source >> salesforce_lookup >> trash
+    pipeline = pipeline_builder.build(title='Salesforce Lookup').configure_for_environment(salesforce)
+    sdc_executor.add_pipeline(pipeline)
+
+    LOOKUP_EXPECTED_DATA = copy.deepcopy(DATA_TO_INSERT)
+    for record in LOOKUP_EXPECTED_DATA:
+        record['surName'] = record.pop('LastName')
+    verify_by_snapshot(sdc_executor, pipeline, salesforce_lookup, LOOKUP_EXPECTED_DATA, salesforce)
