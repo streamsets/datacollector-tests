8cfafcc66a34ffb974e769c9129e810f9c9fdbd7
Jeff Evans <jeff@streamsets.com>
2017-11-10 15:47:14 -0600

    SDC-7735. Create test for non-incremental multi table JDBC source
    Adding new pytest param for non-incremental mode, which results in not
    using a primary key when creating the table
    
    Change-Id: I28f17e4f8c5834cbea1c72121864a800a008c247
    Reviewed-on: https://review.streamsets.net/11363
    Reviewed-by: Sridhar Banoor <sridhar@streamsets.com>

--- pipeline/test_jdbc_multitable_multithreaded.py
+++ pipeline/test_jdbc_multitable_multithreaded.py
@@ -19,7 +19,9 @@
 another.
 """
 
+from collections import namedtuple
 import logging
+import random
 import re
 import string
 from time import sleep, time
@@ -36,7 +38,7 @@ logger = logging.getLogger(__name__)
 
 # lower case tables have better compatibility with databases (e.g., PostgreSQL)
 EVENT_COLUMN_NAME = 'event_status'
-PRIMARY_KEY = 'pid'
+FIRST_COLUMN = 'pid'
 OTHER_COLUMN = 'randomstring'
 NO_OF_SRC_ROWS = 60
 PARTITION_SIZE = '10'
@@ -46,22 +48,23 @@ TGT_TABLE_PREFIX = get_random_string(string.ascii_lowercase, 6)
 
 TABLE_PREFIX_NAME_FMT = '{table_prefix}_{table_name}'
 
+TableInfo = namedtuple('TableInfo', ['name', 'use_primary_key'])
 
-def assert_tables_replicated(database=None, src_table_names=None):
+def assert_tables_replicated(database=None, src_tables=None):
     """Goes through all source tables and checks the corresponding mapping to a target table."""
     db_engine = database.engine
-    for src_table_name in src_table_names:
-        target_table_name = re.sub(SRC_TABLE_PREFIX, TGT_TABLE_PREFIX, src_table_name, 1)
-        logger.info('Comparing Source Table : %s and Target Table : %s', src_table_name, target_table_name)
+    for src_table_info in src_tables:
+        target_table_name = re.sub(SRC_TABLE_PREFIX, TGT_TABLE_PREFIX, src_table_info.name, 1)
+        logger.info('Comparing Source Table : %s and Target Table : %s', src_table_info.name, target_table_name)
 
-        src_table = sqlalchemy.Table(src_table_name, sqlalchemy.MetaData(), autoload=True, autoload_with=db_engine)
-        src_result = db_engine.execute(src_table.select().order_by(src_table.c[PRIMARY_KEY]))
+        src_table = sqlalchemy.Table(src_table_info.name, sqlalchemy.MetaData(), autoload=True, autoload_with=db_engine)
+        src_result = db_engine.execute(src_table.select().order_by(src_table.c[FIRST_COLUMN]))
         src_result_list = src_result.fetchall()
         src_result.close()
 
         target_table = sqlalchemy.Table(target_table_name, sqlalchemy.MetaData(),
                                         autoload=True, autoload_with=db_engine)
-        target_result = db_engine.execute(target_table.select().order_by(target_table.c[PRIMARY_KEY]))
+        target_result = db_engine.execute(target_table.select().order_by(target_table.c[FIRST_COLUMN]))
         target_result_list = target_result.fetchall()
         target_result.close()
 
@@ -89,26 +92,32 @@ def wait_for_no_data_event(event_table_name, database=None, timeout_sec=240):
     raise Exception('Timed out after %s seconds while waiting for no data event.')
 
 
-def setup_tables(database, src_table_names, target_table_names, event_table_name):
+def setup_tables(database, src_tables, target_tables, event_table_name):
     """Creates source, target and event tables, inserts rows to the source table and
     insert 0 for event table's event column.
     """
     db_engine = database.engine
-    for table_name in src_table_names:
-        logger.info('Creating source table %s in %s database ...', table_name, database.type)
-        table = sqlalchemy.Table(table_name, sqlalchemy.MetaData(),
-                                 sqlalchemy.Column(PRIMARY_KEY, sqlalchemy.Integer, primary_key=True),
+
+    for src_table in src_tables:
+        first_col = sqlalchemy.Column(FIRST_COLUMN, sqlalchemy.Integer, primary_key=src_table.use_primary_key)
+        logger.info('Creating source table %s in %s database ...', src_table.name, database.type)
+        table = sqlalchemy.Table(src_table.name, sqlalchemy.MetaData(), first_col,
                                  sqlalchemy.Column(OTHER_COLUMN, sqlalchemy.String(20)))
         table.create(db_engine)
-        logger.info('Inserting data into source table %s in %s database ...', table_name, database.type)
-        for src_row_id in range(1, NO_OF_SRC_ROWS+1):  # some databases (like MySQL) will start from 1
-            db_engine.execute(table.insert(), [{PRIMARY_KEY: src_row_id,
-                                                OTHER_COLUMN: get_random_string(string.ascii_lowercase, 20)}])
+        logger.info('Inserting data into source table %s in %s database ...', src_table.name, database.type)
+        row_ids = list(range(1, NO_OF_SRC_ROWS+1))
+        if not src_table.use_primary_key:
+            # shuffle the first col values for non-incremental mode
+            random.shuffle(row_ids)
+        for src_row_id in row_ids:  # some databases (like MySQL) will start from 1
+            insert_data = {FIRST_COLUMN: src_row_id,
+                           OTHER_COLUMN: get_random_string(string.ascii_lowercase, 20)}
+            db_engine.execute(table.insert(), [insert_data])
 
-    for table_name in target_table_names:
-        logger.info('Creating target table %s in %s database ...', table_name, database.type)
-        table = sqlalchemy.Table(table_name, sqlalchemy.MetaData(),
-                                 sqlalchemy.Column(PRIMARY_KEY, sqlalchemy.Integer, primary_key=True),
+    for target_table in target_tables:
+        first_col = sqlalchemy.Column(FIRST_COLUMN, sqlalchemy.Integer, primary_key=target_table.use_primary_key)
+        logger.info('Creating target table %s in %s database ...', target_table.name, database.type)
+        table = sqlalchemy.Table(target_table.name, sqlalchemy.MetaData(), first_col,
                                  sqlalchemy.Column(OTHER_COLUMN, sqlalchemy.String(20)))
         table.create(db_engine)
 
@@ -137,6 +146,7 @@ def teardown_tables(database, table_names):
 @pytest.mark.parametrize('no_of_threads', [1, 5])
 @pytest.mark.parametrize('batch_strategy', ["SWITCH_TABLES", "PROCESS_ALL_AVAILABLE_ROWS_FROM_TABLE"])
 @pytest.mark.parametrize('partitioning_mode', ["DISABLED", "BEST_EFFORT"])
+@pytest.mark.parametrize('non_incremental', [True, False])
 @pytest.mark.timeout(300)
 def test_jdbc_multitable_consumer_to_jdbc(sdc_builder, sdc_executor, database,
                                           table_name_characters,
@@ -144,7 +154,8 @@ def test_jdbc_multitable_consumer_to_jdbc(sdc_builder, sdc_executor, database,
                                           no_of_tables,
                                           no_of_threads,
                                           batch_strategy,
-                                          partitioning_mode):
+                                          partitioning_mode,
+                                          non_incremental):
     """Tests Multithreaded Multi-table JDBC source. Replicates a set of tables with prefix 'src' to a another
     set of tables with 'target' prefix. Also leveraging the NO_MORE_DATA EVENT by the Multi-table JDBC source after
     no data in tables. On Event path a JDBC Executor updates a special event table to mark data read to stop pipeline.
@@ -162,7 +173,8 @@ def test_jdbc_multitable_consumer_to_jdbc(sdc_builder, sdc_executor, database,
                                             max_pool_size=no_of_threads, min_idle_connections=no_of_threads,
                                             table_configuration=[{"tablePattern": f'{SRC_TABLE_PREFIX}%',
                                                                   'partitioningMode': partitioning_mode,
-                                                                  'partitionSize': PARTITION_SIZE}])
+                                                                  'partitionSize': PARTITION_SIZE,
+                                                                  'enableNonIncremental': non_incremental}])
 
     if partitioning_mode == 'BEST_EFFORT':
         # for partitioning, increase the max allowed queries per second (will be changed as SDC default via SDC-7867)
@@ -175,7 +187,7 @@ def test_jdbc_multitable_consumer_to_jdbc(sdc_builder, sdc_executor, database,
                   f"'{SRC_TABLE_PREFIX if not database.type == 'Oracle' else SRC_TABLE_PREFIX.upper()}',"
                   f"'{TGT_TABLE_PREFIX}')}}")
     query = (f"INSERT into {table_name} values "
-             f"(${{record:value('/{PRIMARY_KEY if not database.type == 'Oracle' else PRIMARY_KEY.upper()}')}}"
+             f"(${{record:value('/{FIRST_COLUMN if not database.type == 'Oracle' else FIRST_COLUMN.upper()}')}}"
              f", '${{record:value('/{OTHER_COLUMN if not database.type == 'Oracle' else OTHER_COLUMN.upper()}')}}')")
 
     jdbc_query_dest.set_attributes(sql_query=query)
@@ -184,26 +196,36 @@ def test_jdbc_multitable_consumer_to_jdbc(sdc_builder, sdc_executor, database,
 
     jdbc_multitable_consumer >> jdbc_query_dest
     jdbc_multitable_consumer >= jdbc_query_event
-    pipeline_name = f'JDBC multitable consumer pipeline - {batch_strategy} batch strategy, {no_of_threads} threads, {partitioning_mode} partitioning'
+    non_inc = ', non-incremental' if non_incremental else ''
+    pipeline_name = f'JDBC multitable consumer pipeline - {batch_strategy} batch strategy, {no_of_threads} threads, {partitioning_mode} partitioning{non_inc}'
     pipeline = pipeline_builder.build(pipeline_name).configure_for_environment(database)
     sdc_executor.add_pipeline(pipeline)
 
     # Generate random table names.
-    table_names = [get_random_string(table_name_characters, table_name_length).lower() + '_' + str(tableNo)
+    table_names = ['{}_{}'.format(get_random_string(table_name_characters, table_name_length).lower(), tableNo)
                    for tableNo in range(0, no_of_tables)]
-    src_table_names = ([TABLE_PREFIX_NAME_FMT.format(table_prefix=SRC_TABLE_PREFIX,
-                                                     table_name=table_name)
-                        for table_name in table_names])
-    target_table_names = ([TABLE_PREFIX_NAME_FMT.format(table_prefix=TGT_TABLE_PREFIX,
-                                                        table_name=table_name)
-                           for table_name in table_names])
+
+    random.shuffle(table_names)
+
+    # when using non-incremental mode, give only half the tables primary keys
+    pk_tables = table_names[:len(table_names)//2] if non_incremental else table_names
+
+    # build tuples with table name, and whether to use a primary key
+    src_tables = [TableInfo(name=TABLE_PREFIX_NAME_FMT.format(table_prefix=SRC_TABLE_PREFIX,
+                                                              table_name=table_name),
+                            use_primary_key=table_name in pk_tables)
+                  for table_name in table_names]
+    target_tables = [TableInfo(name=TABLE_PREFIX_NAME_FMT.format(table_prefix=TGT_TABLE_PREFIX,
+                                                                 table_name=table_name),
+                               use_primary_key=table_name in pk_tables)
+                     for table_name in table_names]
     try:
-        setup_tables(database, src_table_names, target_table_names, event_table_name)
+        setup_tables(database, src_tables, target_tables, event_table_name)
         sdc_executor.start_pipeline(pipeline)
         wait_for_no_data_event(event_table_name, database)
-        assert_tables_replicated(database, src_table_names)
+        assert_tables_replicated(database, src_tables)
     finally:
         # must stop pipeline before tables can be dropped.
         sdc_executor.stop_pipeline(pipeline)
         logger.info('Dropping test related tables in %s database...', database.type)
-        teardown_tables(database, src_table_names + target_table_names + [event_table_name])
+        teardown_tables(database, [table.name for table in src_tables + target_tables] + [event_table_name])
