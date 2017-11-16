259d6fb77dd0c4d13e4d143ba9b6f97d72d4f703
Junko <junko@streamsets.com>
2017-11-16 10:04:12 -0800

    SDC-7705. Create test for Kudu UNIXTIME_MICROS and millisecond-based timestamp column handling
    Change-Id: I3dafdd00f508cd68ae6ac82dda3af6f60c5f9e9c
    Reviewed-on: https://review.streamsets.net/11583
    Reviewed-by: Kirti Velankar <kirti@streamsets.com>

--- stage/test_cdh_stages.py
+++ stage/test_cdh_stages.py
@@ -20,10 +20,11 @@
 :py:obj:`testframework.sdc_models.PipelineBuilder` in one version of SDC and then importing and running them in
 another.
 """
-
 import json
 import logging
 import string
+import time
+from datetime import datetime
 
 import pytest
 import sqlalchemy
@@ -109,6 +110,73 @@ def test_kudu_destination(sdc_builder, sdc_executor, cluster):
         result = connection.execute(sqlalchemy.sql.select([tdf_contenders_table]).order_by('rank'))
         assert list(result) == [tuple([item['favorite_rank'], item['name'], item['wins']])
                                 for item in sorted(tour_de_france_contenders, key=lambda key: key['favorite_rank'])]
+    finally:
+        logger.info('Dropping Kudu table %s ...', kudu_table_name)
+        tdf_contenders_table.drop(engine)
+
+
+@cluster('cdh')
+def test_kudu_destination_unixtime_micro_datatype(sdc_builder, sdc_executor, cluster):
+    """
+    Test Kudu's UNIXTIME_MICRO data type support.
+    dev_raw_data_source >> kudu
+    """
+    if not hasattr(cluster, 'kudu'):
+        pytest.skip('Kudu tests only run against clusters with the Kudu service present.')
+
+    # Generate some data. Kudu does not store microsecond so set it 0.
+    now = datetime.now().replace(microsecond=0)
+    now_millisecond = time.mktime(now.timetuple()) * 1000
+    input_data = [dict(id=1, time=now_millisecond)]
+
+    raw_data = ''.join([json.dumps(contender) for contender in input_data])
+    field_to_column_mapping = [dict(field='/id', columnName='id'),
+                               dict(field='/time', columnName='unixtime_micro')]
+
+    kudu_table_name = get_random_string(string.ascii_letters, 10)
+    kudu_master_address = f'{cluster.server_host}:{DEFAULT_KUDU_PORT}'
+
+    # Build the pipeline.
+    builder = sdc_builder.get_pipeline_builder()
+    dev_raw_data_source = builder.add_stage('Dev Raw Data Source').set_attributes(data_format='JSON',
+                                                                                  raw_data=raw_data)
+    kudu = builder.add_stage('Kudu',
+                             type='destination').set_attributes(table_name=kudu_table_name,
+                                                                default_operation='INSERT',
+                                                                field_to_column_mapping=field_to_column_mapping)
+    dev_raw_data_source >> kudu
+
+    pipeline = builder.build().configure_for_environment(cluster)
+    pipeline.delivery_guarantee = 'AT_MOST_ONCE'
+    # We want to write data once and then stop, but Dev Raw Data Source will keep looping, so we set the rate limit to
+    # a low value and will rely upon pipeline metrics to know when to stop the pipeline.
+    pipeline.rate_limit = 4
+
+    metadata = sqlalchemy.MetaData()
+    test_table = sqlalchemy.Table(kudu_table_name,
+                                  metadata,
+                                  sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+                                  sqlalchemy.Column('unixtime_micro', sqlalchemy.TIMESTAMP),
+                                  impala_partition_by='HASH PARTITIONS 16',
+                                  impala_stored_as='KUDU',
+                                  impala_table_properties={
+                                      'kudu.table_name': kudu_table_name,
+                                      'kudu.master_addresses': kudu_master_address,
+                                      'kudu.num_tablet_replicas': '1'
+                                  })
+
+    try:
+        logger.info('Creating Kudu table %s ...', kudu_table_name)
+        engine = cluster.kudu.engine
+        test_table.create(engine)
+
+        sdc_executor.add_pipeline(pipeline)
+        sdc_executor.start_pipeline(pipeline).wait_for_pipeline_batch_count(len(input_data))
+        sdc_executor.stop_pipeline(pipeline)
+
+        connection = engine.connect()
+        result = connection.execute(sqlalchemy.sql.select([test_table])).fetchone()
+        assert list(result) == [1, now]
     finally:
         logger.info('Dropping Kudu table %s ...', kudu_table_name)
         tdf_contenders_table.drop(engine)
