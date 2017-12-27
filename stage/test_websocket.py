30a64b72c86306e8a269bd1fed05762c49034248
Jarek Jarcec Cecho <jarcec@apache.org>
2017-12-27 10:42:44 -0800

    SDC-8145: Update license headers in test repository
    Change-Id: I1b0a21209f112b8e8a2d511f17f5f5e33abe3503
    Reviewed-on: https://review.streamsets.net/12192
    Reviewed-by: Dima Spivak <dima@streamsets.com>

--- stage/test_websocket.py
+++ stage/test_websocket.py
@@ -1,12 +1,8 @@
 # Copyright 2017 StreamSets Inc.
 #
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
@@ -15,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 
 import logging
 from collections import namedtuple
