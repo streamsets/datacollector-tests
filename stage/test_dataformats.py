3462794a4cfc2c2f5166ea9515e0ba783cd5f8f8
Jarek Jarcec Cecho <jarcec@apache.org>
2018-06-30 11:22:50 -0700

    SDC-9395: Add test coverage for all text-based data formats via data parser processor
    This is first patch in a line will try to add coverage for all the
    various file formats that we support via the 'simple' data parser and
    data generator processors that are underneath utilizing services
    concept. Currently the coverage is for all text-based file formats on
    the parsing side.
    
    Change-Id: I9e8b992850f31a520b11b0427d467ddbebffc800
    Reviewed-on: https://review.streamsets.net/15291
    Reviewed-by: Dima Spivak <dima@streamsets.com>
    Reviewed-by: Sridhar Banoor <sridhar@streamsets.com>
    Reviewed-by: Keith Burns <keith@streamsets.com>
    Reviewed-by: Ji Sun Kim <jisun@streamsets.com>

---
+++ stage/test_dataformats.py
@@ -0,0 +1,107 @@
+# Copyright 2018 StreamSets Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+#
+# Text base file format parsing via Data Parser processor
+#
+
+
+def create_text_pipeline(sdc_builder, data_format, content):
+    builder = sdc_builder.get_pipeline_builder()
+
+    origin = builder.add_stage('Dev Raw Data Source')
+    origin.data_format = 'TEXT'
+    origin.raw_data = content
+
+    parser = builder.add_stage('Data Parser')
+
+    parser.field_to_parse = '/text'
+    parser.target_field = '/'
+    parser.data_format = data_format
+
+    trash = builder.add_stage('Trash')
+
+    origin >> parser >> trash
+
+    return builder.build('Parse {}'.format(data_format))
+
+
+def test_parse_json(sdc_builder, sdc_executor):
+    """Validate parsing of JSON content via the Data Parser processor."""
+    pipeline = create_text_pipeline(sdc_builder, 'JSON', '{"key" : "value"}')
+
+    sdc_executor.add_pipeline(pipeline)
+    snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True).snapshot
+    sdc_executor.stop_pipeline(pipeline)
+
+    assert len(snapshot['DataParser_01'].output) == 1
+    assert snapshot['DataParser_01'].output[0].get_field_data('/key') == 'value'
+
+
+def test_parse_delimited(sdc_builder, sdc_executor):
+    """Validate parsing of delimited content via the Data Parser processor."""
+    pipeline = create_text_pipeline(sdc_builder, 'DELIMITED', '1,2,3')
+
+    sdc_executor.add_pipeline(pipeline)
+    snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True).snapshot
+    sdc_executor.stop_pipeline(pipeline)
+
+    assert len(snapshot['DataParser_01'].output) == 1
+    assert snapshot['DataParser_01'].output[0].get_field_data('[0]') == '1'
+    assert snapshot['DataParser_01'].output[0].get_field_data('[1]') == '2'
+    assert snapshot['DataParser_01'].output[0].get_field_data('[2]') == '3'
+
+
+def test_parse_log(sdc_builder, sdc_executor):
+    """Validate parsing of log content via the Data Parser processor."""
+    pipeline = create_text_pipeline(sdc_builder, 'LOG', '127.0.0.1 ss h [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326')
+
+    sdc_executor.add_pipeline(pipeline)
+    snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True).snapshot
+    sdc_executor.stop_pipeline(pipeline)
+
+    assert len(snapshot['DataParser_01'].output) == 1
+    assert snapshot['DataParser_01'].output[0].get_field_data('/request') == '/apache_pb.gif'
+    assert snapshot['DataParser_01'].output[0].get_field_data('/clientip') == '127.0.0.1'
+
+
+def test_parse_syslog(sdc_builder, sdc_executor):
+    """Validate parsing of syslog content via the Data Parser processor."""
+    pipeline = create_text_pipeline(sdc_builder, 'SYSLOG', "<34>Oct 11 22:14:15 mymachine su: 'su root' failed for lonvick on /dev/pts/")
+
+    sdc_executor.add_pipeline(pipeline)
+    snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True).snapshot
+    sdc_executor.stop_pipeline(pipeline)
+
+    assert len(snapshot['DataParser_01'].output) == 1
+    assert snapshot['DataParser_01'].output[0].get_field_data('/severity') == 2
+    assert snapshot['DataParser_01'].output[0].get_field_data('/host') == 'mymachine'
+
+
+def test_parse_xml(sdc_builder, sdc_executor):
+    """Validate parsing of xml content via the Data Parser processor."""
+    pipeline = create_text_pipeline(sdc_builder, 'XML', "<root><key>value</key></root>")
+
+    sdc_executor.add_pipeline(pipeline)
+    snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True).snapshot
+    sdc_executor.stop_pipeline(pipeline)
+
+    assert len(snapshot['DataParser_01'].output) == 1
+    assert snapshot['DataParser_01'].output[0].value2['key'][0]['value'] == 'value'
+# TLKT-53
+#    assert snapshot['DataParser_01'].output[0].get_field_data('/key[0]') == 'value'
