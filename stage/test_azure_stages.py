61a222aae8768c6b050495737cdc10903d981867
Santhosh Kumar Manavasi Lakshminarayanan <santhosh@streamsets.com>
2018-08-31 20:25:33 -0700

    SDC-9939. Test MaxRecordsPerFile Setting in AzureDataLakeTarget.
    This patch adds a test case to test max records per file setting Azure Data Lake Destination.
    
    Change-Id: I8b6e1f61e0544b6d3bfcf95f156610c805aed2a6
    Reviewed-on: https://review.streamsets.net/16523
    Reviewed-by: Ji Sun Kim <jisun@streamsets.com>
    Reviewed-by: Sridhar Banoor <sridhar@streamsets.com>

--- stage/test_azure_stages.py
+++ stage/test_azure_stages.py
@@ -17,6 +17,7 @@
 import json
 import logging
 import string
+from operator import itemgetter
 
 from azure import servicebus
 from streamsets.testframework.markers import azure, sdc_min_version
@@ -59,7 +60,8 @@ def test_datalake_destination(sdc_builder, sdc_executor, azure):
     to_error = builder.add_stage('To Error')
 
     azure_data_lake_store_destination = builder.add_stage('Azure Data Lake Store', type='destination')
-    azure_data_lake_store_destination.set_attributes(directory_template=directory_name, files_prefix=files_prefix,
+    azure_data_lake_store_destination.set_attributes(directory_template=directory_name,
+                                                     files_prefix=files_prefix,
                                                      files_suffix=files_suffix)
 
     dev_raw_data_source >> record_deduplicator >> azure_data_lake_store_destination
@@ -67,15 +69,15 @@ def test_datalake_destination(sdc_builder, sdc_executor, azure):
 
     datalake_dest_pipeline = builder.build(title='Azure Data Lake dest pipeline').configure_for_environment(azure)
     sdc_executor.add_pipeline(datalake_dest_pipeline)
-
+    dl_fs = azure.datalake.file_system
     try:
         # start pipeline and capture pipeline messages to assert
         logger.info('Azure Data Lake directory %s will be created with files prefix %s', directory_name, files_prefix)
-        sdc_executor.start_pipeline(datalake_dest_pipeline).wait_for_pipeline_output_records_count(1)
+        sdc_executor.start_pipeline(datalake_dest_pipeline).wait_for_pipeline_output_records_count(2)
         sdc_executor.stop_pipeline(datalake_dest_pipeline)
 
-        dl_fs = azure.datalake.file_system
         dl_files = dl_fs.ls(directory_name)
+
         # assert Data Lake files generated
         assert len(dl_files) == 1
 
@@ -89,8 +91,67 @@ def test_datalake_destination(sdc_builder, sdc_executor, azure):
 
         assert raw_list == result_list
     finally:
+        dl_files = dl_fs.ls(directory_name)
         # Note: Non-empty directory is not allowed to be removed, hence remove all files first.
         logger.info('Azure Data Lake directory %s and underlying files will be deleted.', directory_name)
+        for dl_file in dl_files:
+            dl_fs.rm(dl_file)
+        dl_fs.rmdir(directory_name)
+
+
+@azure('datalake')
+@sdc_min_version('3.0.0.0')
+def test_datalake_destination_max_records(sdc_builder, sdc_executor, azure):
+    """Test for Data Lake Store target stage setting max number of records per file as 1.
+       The pipeline looks like:
+
+        Data Lake Store Destination pipeline:
+            dev_data_generator >> azure_data_lake_store_destination
+    """
+    directory_name = get_random_string(string.ascii_letters, 10)
+    files_prefix = get_random_string(string.ascii_letters, 10)
+    files_suffix = 'json'
+
+    raw_data = [dict(id=1, name='abc'), dict(id=2, name='def'),
+                dict(id=3, name='ghi'), dict(id=4, name='jkl'),
+                dict(id=5, name='mno'), dict(id=6, name='pqr'),
+                dict(id=7, name='stu'), dict(id=8, name='vwx'),
+                dict(id=9, name='y'), dict(id=10, name='z')]
+
+    dev_raw_data_source_data = ''.join(json.dumps(d) for d in raw_data)
+
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+    dev_raw_data_source.set_attributes(data_format='JSON',
+                                       raw_data=dev_raw_data_source_data,
+                                       stop_after_first_batch=True)
+
+    azure_data_lake_store = pipeline_builder.add_stage('Azure Data Lake Store', type='destination')
+    azure_data_lake_store.set_attributes(directory_template=directory_name,
+                                         files_prefix=files_prefix,
+                                         files_suffix=files_suffix,
+                                         max_records_in_file=1)
+    dev_raw_data_source >> azure_data_lake_store
+
+    pipeline = pipeline_builder.build(title='Azure Data Lake Test - Max Records').configure_for_environment(azure)
+    sdc_executor.add_pipeline(pipeline)
+    dl_fs = azure.datalake.file_system
+
+    try:
+        sdc_executor.start_pipeline(pipeline).wait_for_finished()
+
+        dl_files = dl_fs.ls(directory_name)
+        assert len(dl_files) == len(raw_data)
+        for dl_file in dl_files:
+            dl_file_name = dl_file.split('/')[-1]
+            assert dl_file_name.startswith(files_prefix) and dl_file_name.endswith(files_suffix)
+        dl_file_contents = [json.loads(dl_fs.cat(dl_file).decode()) for dl_file in dl_files]
+
+        assert sorted(dl_file_contents, key=itemgetter('id')) == sorted(raw_data, key=itemgetter('id'))
+    finally:
+        dl_files = dl_fs.ls(directory_name)
+        logger.info('Azure Data Lake directory %s and underlying files will be deleted.', directory_name)
+        # Note: Non-empty directory is not allowed to be removed, hence remove all files first.
         for dl_file in dl_files:
             dl_fs.rm(dl_file)
         dl_fs.rmdir(directory_name)
