bf7e4f63e8be5732af9d089ab1190df51d9d363f
Xavier Baques <xavi@streamsets.com>
2018-09-03 15:44:20 +0200

    SDC-9944. Add TCP Server with SSL integration test
    Change-Id: I4e51f21af9bcf345a3120bc957bceb3854e38df0
    Reviewed-on: https://review.streamsets.net/16527
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

--- stage/test_tcp_server.py
+++ stage/test_tcp_server.py
@@ -13,15 +13,19 @@
 # limitations under the License.
 
 import logging
+import socket
+import ssl
 from collections import namedtuple
 
 import pytest
 from streamsets.testframework.environment import TCPClient
+from streamsets.testframework.markers import sdc_min_version
 
 logger = logging.getLogger(__name__)
 
 # TODO: convert to pipeline param. seems to not work (see below)
 TCP_PORT = 17892
+TCP_SSL_FILE_PATH = './resources/tcp_server/file.txt'
 
 
 @pytest.fixture(scope='module')
@@ -69,3 +73,78 @@ def test_tcp_server_simple(sdc_executor, tcp_server_pipeline):
 
     # Stop the pipelines.
     sdc_executor.stop_pipeline(tcp_server_pipeline.pipeline)
+
+
+@sdc_min_version('3.4.2')
+def test_tcp_server_ssl(sdc_builder, sdc_executor):
+    """Runs a test using the TCP server origin pipeline with Enable TLS set and asserts that the file is received"""
+    expected_msg = get_expected_message(TCP_SSL_FILE_PATH)
+
+    # Start TCP server pipeline.
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    tcp_server = pipeline_builder.add_stage('TCP Server')
+    tcp_server.set_attributes(data_format='TEXT',
+                              port=[str(TCP_PORT)],
+                              tcp_mode='DELIMITED_RECORDS',
+                              use_tls=True,
+                              keystore_file='keystore.jks',
+                              keystore_type='JKS',
+                              keystore_password='password',
+                              keystore_key_algorithm='SunX509',
+                              use_default_protocols=True,
+                              use_default_cipher_suites=True)
+
+    trash = pipeline_builder.add_stage('Trash')
+
+    tcp_server >> trash
+    tcp_server_ssl_pipeline = pipeline_builder.build(title='TCP Server SSL pipeline')
+
+    sdc_executor.add_pipeline(tcp_server_ssl_pipeline)
+
+    try:
+        # Capture snapshot for HTTP Server pipeline.
+        snapshot_cmd = sdc_executor.capture_snapshot(tcp_server_ssl_pipeline, start_pipeline=True, batches=1,
+                                                     batch_size=2, wait=False)
+
+        # Send twice the data. Even though batch_size = 2, 2 batches are sent (1 for each connection).
+        send_tcp_ssl_file(sdc_executor)
+        send_tcp_ssl_file(sdc_executor)
+
+        # Wait for snapshot to finish then stop the pipeline in order to get the summary later.
+        snapshot = snapshot_cmd.wait_for_finished().snapshot
+        sdc_executor.stop_pipeline(tcp_server_ssl_pipeline)
+
+        # Verify the results. First check number of batches received is 2.
+        history = sdc_executor.get_pipeline_history(tcp_server_ssl_pipeline)
+        assert history.latest.metrics.counter('pipeline.batchCount.counter').count == 2
+        # Then check last batch received in snapshot contains the expected message.
+        origin_data = snapshot[tcp_server.instance_name]
+        assert len(origin_data.output) == 1
+        assert str(origin_data.output[0].field['text']) in expected_msg.decode("utf-8")
+
+    finally:
+        if sdc_executor.get_pipeline_status(tcp_server_ssl_pipeline) == 'RUNNING':
+            sdc_executor.stop_pipeline(tcp_server_ssl_pipeline)
+
+
+def send_tcp_ssl_file(sdc_executor):
+    """Sends a file through tcp using ssl"""
+    hostname = sdc_executor.server_host
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
+    with socket.create_connection((hostname, TCP_PORT)) as sock:
+        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+            file_to_send = open(TCP_SSL_FILE_PATH, 'rb')
+            ssock.sendfile(file_to_send)
+            file_to_send.close()
+            ssock.close()
+
+
+def get_expected_message(file_path):
+    file_to_read = open(file_path, 'rb')
+    message = file_to_read.readline()
+    file_to_read.close()
+    return message
