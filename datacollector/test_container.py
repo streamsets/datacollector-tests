81f40e34116d620e23f7508327e5420aaa26849e
Tony McKeehan <anthony@streamsets.com>
2019-03-12 11:01:18 -0700

    SDC-11035. Force SDC container HTTP server to disallow TRACE method
    Change-Id: I791ccb1b931d3742501c7f4887150d1a276a92d2
    Reviewed-on: https://review.streamsets.net/c/20084
    Reviewed-by: Robert Kanter <rkanter@streamsets.com>

---
+++ datacollector/test_container.py
@@ -0,0 +1,33 @@
+# Copyright 2019 StreamSets Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import http.client
+import pytest
+from streamsets.testframework.markers import sdc_min_version
+
+pytestmark = sdc_min_version('3.8.0')
+
+# Main SDC WebServerTask should refuse connection requests
+# using HTTP methods TRACK and TRACE
+@pytest.mark.parametrize(('method'), [
+    'TRACK',
+    'TRACE'
+])
+def test_restricted_http_methods(sdc_executor, method):
+    port = sdc_executor.server_url.split(':')[-1]
+    h1 = http.client.HTTPConnection(sdc_executor.server_host, port)
+    h1.request(method, '/')
+
+    resp = h1.getresponse()
+    assert resp.status == 405
