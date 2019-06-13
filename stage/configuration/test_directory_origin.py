f549035b40a6367090c9e228aee512bf6c72b35f
Manish Zope <manish.zope@talentica.com>
2019-06-13 15:07:49 +0530

    SDC-11384 Add test case for Directory origin’s File Name Pattern Mode configuration
    * Util functions rebased from master

--- stage/configuration/test_directory_origin.py
+++ stage/configuration/test_directory_origin.py
@@ -658,27 +658,26 @@ def test_directory_origin_configuration_file_name_pattern_mode(sdc_builder, sdc_
     with regex we match only 1st file and with glob both files.
     """
     files_name = ['pattern_check_processing_1.txt', 'pattern_check_processing_2.txt']
-    files_content = [DirectoryOriginCommon.get_text_file_content(1, 1),
-                     DirectoryOriginCommon.get_text_file_content(2, 1)]
+    files_content = [get_text_file_content(1, 1),
+                     get_text_file_content(2, 1)]
     file_name_pattern = '*.txt' if file_name_pattern_mode == 'GLOB' else '^p(.*)([0-9]{1})(\.txt)'
     number_of_batches = 2 if file_name_pattern_mode == 'GLOB' else 1
 
     try:
-        files_directory = DirectoryOriginCommon.create_file_directory(files_name[0], files_content[0], shell_executor,
-                                                                      file_writer)
+        files_directory = create_file_and_directory(files_name[0], files_content[0], shell_executor, file_writer)
         file_writer(os.path.join(files_directory, files_name[1]), files_content[1])
 
         attributes = {'data_format':'TEXT',
                       'files_directory':files_directory,
                       'file_name_pattern_mode':file_name_pattern_mode,
                       'file_name_pattern':file_name_pattern}
-        directory, pipeline = DirectoryOriginCommon.get_directory_trash_pipeline(sdc_builder, attributes)
+        directory, pipeline = get_directory_to_trash_pipeline(sdc_builder, attributes)
 
         sdc_executor.add_pipeline(pipeline)
         snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True, batches=number_of_batches).snapshot
 
         raw_data = '\n'.join(files_content)
-        processed_data = '\n'.join(DirectoryOriginCommon.snapshot_content(snapshot, directory))
+        processed_data = '\n'.join(snapshot_content(snapshot, directory))
         if file_name_pattern_mode == 'GLOB':
             assert raw_data == processed_data
         else:
