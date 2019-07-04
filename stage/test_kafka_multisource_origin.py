eb07e5ea2f92468f15b873488946f1ad441bcc8e
Xavier Baques <xavi@streamsets.com>
2019-07-04 10:26:43 +0200

    SDC-11934. Change SDC_RPC_LISTENING_PORT in test_kafka_multisource_origin
    Change-Id: I9f726b68354d01e9463ef9d9632b18cc93cd7f18
    Reviewed-on: https://review.streamsets.net/c/23167
    Reviewed-by: Xavier Baqués <xavi@streamsets.com>

--- stage/test_kafka_multisource_origin.py
+++ stage/test_kafka_multisource_origin.py
@@ -27,7 +27,7 @@ logger.setLevel(logging.DEBUG)
 
 # Specify a port for SDC RPC stages to use.
 SNAPSHOT_TIMEOUT_SEC = 120
-SDC_RPC_LISTENING_PORT = 20000
+SDC_RPC_LISTENING_PORT = 21512
 
 
 @pytest.fixture(autouse=True)
