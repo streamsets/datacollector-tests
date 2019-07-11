65548d5c019241d987455ad2c55abca27a0893f4
Xavier Baques <xavi@streamsets.com>
2019-07-11 17:23:51 +0200

    SDC-12031. Test LIBS-455. Navigator lib is not correctly filtering empty pipeline parameters nor tags with incorrect characters
    Change-Id: Ifbf54f043d4e5e0f9c9e256e7958d4e3a1c9a88f
    Reviewed-on: https://review.streamsets.net/c/23357
    Reviewed-by: Jarcec Cecho <jarcec@streamsets.com>

--- pipeline/test_navigator.py
+++ pipeline/test_navigator.py
@@ -107,3 +107,122 @@ def test_navigator_basic(sdc_builder, sdc_executor, cluster):
         sdc_executor.stop_pipeline(pipeline)
         logger.info('Deleting Hadoop FS directory %s ...', hdfs_directory)
         cluster.hdfs.client.delete(hdfs_directory, recursive=True)
+
+
+@navigator
+def test_navigator_empty_params(sdc_builder, sdc_executor, cluster):
+    """ Test that SDC can publish events to Cloudera Navigator when the pipeline has empty parameters.
+
+        Pipeline looks like:
+
+        dev_raw_data_source >> record_deduplicator >> hadoop_fs
+        record_deduplicator >> trash
+    """
+    hdfs_directory = f'/tmp/out/{get_random_string(string.ascii_letters, 10)}'
+    files_prefix, files_suffix = 'stages', 'txt'
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    hadoop_fs = pipeline_builder.add_stage('Hadoop FS', type='destination')
+    hadoop_fs.set_attributes(data_format='JSON',
+                             directory_template=hdfs_directory,
+                             files_prefix=files_prefix,
+                             files_suffix=files_suffix)
+
+    pipeline = _create_hadoop_fs_dest_pipeline(pipeline_builder,
+                                               'Cloudera Navigator Empty Parameters',
+                                               hdfs_directory,
+                                               hadoop_fs)
+    pipeline.add_parameters(fields='f1,f2,f3', fromField='/f1', toField='/f1changed', emptyField1='', emptyField2='')
+    sdc_executor.add_pipeline(pipeline.configure_for_environment(cluster))
+
+    try:
+        sdc_executor.start_pipeline(pipeline).wait_for_pipeline_output_records_count(len(TENNIS_CHAMPIONS_DATA))
+
+        # Fetch entities with a query using pipeline id.
+        query_str = f'"properties":"pipelineId": "{pipeline.id}"'
+        entities = cluster.navigator.get_entities(query=query_str)
+        extractor_run_id = entities[0]['extractorRunId']
+
+        # Fetch specific entities with the above extractorRunId and HDFS directory specified in pipeline.
+        query_str = f'(extractorRunId:"{extractor_run_id}")AND("parentPath": "{hdfs_directory}")'
+        entities = cluster.navigator.get_entities(query=query_str)
+        entity = entities[0]
+
+        # Check validity of data in lineage events entity fetched above.
+        hdfs_fs_files = cluster.hdfs.client.list(hdfs_directory)
+        assert len(hdfs_fs_files) == 1
+        hdfs_fs_filename_expected = hdfs_fs_files[0]
+
+        hdfs_fs_filepath_actual = entity['description']
+        assert os.path.join(hdfs_directory, hdfs_fs_filename_expected) == hdfs_fs_filepath_actual
+        assert hdfs_fs_filepath_actual.startswith(os.path.join(hdfs_directory, files_prefix))
+        assert hdfs_fs_filepath_actual.endswith(files_suffix)
+
+        assert 'admin' == entity['properties']['pipeline_user']
+        assert 'sdc' == entity['packageName']
+        assert 'sdc_dataset' == entity['metaClassName']
+
+    finally:
+        sdc_executor.stop_pipeline(pipeline)
+        logger.info('Deleting Hadoop FS directory %s ...', hdfs_directory)
+        cluster.hdfs.client.delete(hdfs_directory, recursive=True)
+
+
+@navigator
+def test_navigator_label_contains_invalid_characters(sdc_builder, sdc_executor, cluster):
+    """ Test that SDC can publish events to Cloudera Navigator when the pipeline has a label with invalid characters
+    like 'xavi/test' where the slash '/' would be an invalid character.
+
+        Pipeline looks like:
+
+        dev_raw_data_source >> record_deduplicator >> hadoop_fs
+        record_deduplicator >> trash
+    """
+    hdfs_directory = f'/tmp/out/{get_random_string(string.ascii_letters, 10)}'
+    files_prefix, files_suffix = 'stages', 'txt'
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    hadoop_fs = pipeline_builder.add_stage('Hadoop FS', type='destination')
+    hadoop_fs.set_attributes(data_format='JSON',
+                             directory_template=hdfs_directory,
+                             files_prefix=files_prefix,
+                             files_suffix=files_suffix)
+
+    pipeline = _create_hadoop_fs_dest_pipeline(pipeline_builder,
+                                               'Cloudera Navigator Invalid Characters in Label',
+                                               hdfs_directory,
+                                               hadoop_fs)
+    pipeline.metadata['labels'] = ['xavi/test']
+    sdc_executor.add_pipeline(pipeline.configure_for_environment(cluster))
+
+    try:
+        sdc_executor.start_pipeline(pipeline).wait_for_pipeline_output_records_count(len(TENNIS_CHAMPIONS_DATA))
+
+        # Fetch entities with a query using pipeline id.
+        query_str = f'"properties":"pipelineId": "{pipeline.id}"'
+        entities = cluster.navigator.get_entities(query=query_str)
+        extractor_run_id = entities[0]['extractorRunId']
+
+        # Fetch specific entities with the above extractorRunId and HDFS directory specified in pipeline.
+        query_str = f'(extractorRunId:"{extractor_run_id}")AND("parentPath": "{hdfs_directory}")'
+        entities = cluster.navigator.get_entities(query=query_str)
+        entity = entities[0]
+
+        # Check validity of data in lineage events entity fetched above.
+        hdfs_fs_files = cluster.hdfs.client.list(hdfs_directory)
+        assert len(hdfs_fs_files) == 1
+        hdfs_fs_filename_expected = hdfs_fs_files[0]
+
+        hdfs_fs_filepath_actual = entity['description']
+        assert os.path.join(hdfs_directory, hdfs_fs_filename_expected) == hdfs_fs_filepath_actual
+        assert hdfs_fs_filepath_actual.startswith(os.path.join(hdfs_directory, files_prefix))
+        assert hdfs_fs_filepath_actual.endswith(files_suffix)
+
+        assert 'admin' == entity['properties']['pipeline_user']
+        assert 'sdc' == entity['packageName']
+        assert 'sdc_dataset' == entity['metaClassName']
+
+    finally:
+        sdc_executor.stop_pipeline(pipeline)
+        logger.info('Deleting Hadoop FS directory %s ...', hdfs_directory)
+        cluster.hdfs.client.delete(hdfs_directory, recursive=True)
