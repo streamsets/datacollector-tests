868c737b42c433541f2771088eb01e4ddaffd69e
Jarek Jarcec Cecho <jarcec@apache.org>
2019-07-28 10:30:30 +0200

    SDC-12171: stage.configuration.test_directory_origin.test_directory_origin_configuration_excel_header_option is missing min sdc version
    Change-Id: I0c983990b6c9ddad744920a7380a0ee6c456d680
    Reviewed-on: https://review.streamsets.net/c/24254
    Reviewed-by: Alex Sanchez Cabana <alex.sanchez@streamsets.com>
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/1095
    Reviewed-by: Dima Spivak <dima@streamsets.com>

--- stage/configuration/test_directory_origin.py
+++ stage/configuration/test_directory_origin.py
@@ -23,6 +23,7 @@ from collections import OrderedDict
 
 import pytest
 from streamsets.sdk.sdc_api import StartError
+from streamsets.testframework.markers import sdc_min_version
 from streamsets.testframework.utils import get_random_string
 from xml.etree import ElementTree
 
@@ -685,6 +686,7 @@ def test_directory_origin_configuration_escape_character(sdc_builder, sdc_execut
     pass
 
 
+@sdc_min_version('3.4.0')
 @pytest.mark.parametrize('data_format', ['EXCEL'])
 @pytest.mark.parametrize('excel_header_option', ['IGNORE_HEADER', 'NO_HEADER', 'WITH_HEADER'])
 def test_directory_origin_configuration_excel_header_option(sdc_builder, sdc_executor,
