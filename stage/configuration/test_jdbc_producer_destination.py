958effdb5309d45a3253db056ddc21b4eb9accdc
Dimbeswar-Rabha <dimbeswar.rabha@talentica.com>
2019-08-19 16:00:42 +0530

    SDC-12245 Add test case for JDBC producer required fields.
    Change-Id: Ib712a811bea58000f34d6c28bb263c68b693931f
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/1135
    Reviewed-by: Dima Spivak <dima@streamsets.com>

--- stage/configuration/test_jdbc_producer_destination.py
+++ stage/configuration/test_jdbc_producer_destination.py
@@ -1,4 +1,12 @@
+import json
+import logging
+
 import pytest
+import sqlalchemy
+from streamsets.testframework.markers import database
+from streamsets.testframework.utils import get_random_string
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.mark.skip('Not yet implemented')
@@ -96,9 +104,45 @@ def test_configuration_preconditions(sdc_builder, sdc_executor):
     pass
 
 
-@pytest.mark.skip('Not yet implemented')
-def test_configuration_required_fields(sdc_builder, sdc_executor):
-    pass
+@database
+def test_required_fields(sdc_builder, sdc_executor, database, keep_data):
+    """Verify that setting a required field sends records without that field to the error stream while records with
+    the field are passed through properly.
+    """
+    DATA = [{'id': 1, 'name': 'Dima'}, {'name': 'Jarcec'}, {'id': 3, 'name': 'Arvind'}]
+    EXPECTED_OUTPUT = [(1, 'Dima'), (3, 'Arvind')]
+
+    COLUMNS = [sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True, autoincrement=False),
+               sqlalchemy.Column('name', sqlalchemy.String(100))]
+    table_name = get_random_string()
+    table = create_table(database, COLUMNS, table_name)
+
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+    raw_data = ''.join(json.dumps(record) for record in DATA)
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source').set_attributes(data_format='JSON',
+                                                                                           raw_data=raw_data)
+    jdbc_producer = pipeline_builder.add_stage('JDBC Producer').set_attributes(default_operation='INSERT',
+                                                                               field_to_column_mapping=[],
+                                                                               required_fields=['/id'],
+                                                                               table_name=table_name)
+    pipeline_finisher = pipeline_builder.add_stage('Pipeline Finisher Executor')
+    dev_raw_data_source >> [jdbc_producer, pipeline_finisher]
+    pipeline = pipeline_builder.build().configure_for_environment(database)
+    sdc_executor.add_pipeline(pipeline)
+    try:
+        snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True).snapshot
+        with database.engine.connect() as connection:
+            result = connection.execute(table.select())
+            data_from_database = sorted(result.fetchall(), key=lambda row: row[0])  # order by id
+
+        error_records = snapshot[jdbc_producer].error_records
+        assert (data_from_database == EXPECTED_OUTPUT and
+                ['COMMON_0001'] == [error_record.header['errorCode']
+                                    for error_record in snapshot[jdbc_producer].error_records])
+    finally:
+        if not keep_data:
+            logger.info('Dropping table %s in %s database ...', table_name, database.type)
+            delete_table([table], database)
 
 
 @pytest.mark.parametrize('rollback_batch_on_error', [False, True])
@@ -152,3 +196,21 @@ def test_configuration_use_multi_row_operation(sdc_builder, sdc_executor, use_mu
 def test_configuration_username(sdc_builder, sdc_executor, use_credentials):
     pass
 
+
+# utility function
+def create_table(database, columns, table_name):
+    metadata = sqlalchemy.MetaData()
+    table = sqlalchemy.Table(
+        table_name,
+        metadata,
+        *columns
+    )
+    logger.info('Creating table %s in %s database ...', table_name, database.type)
+    table.create(database.engine)
+    return table
+
+
+def delete_table(tables, database):
+    for table in tables:
+        logger.info('Dropping table %s in %s database...', table.name, database.type)
+        table.drop(database.engine)
