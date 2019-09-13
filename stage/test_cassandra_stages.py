629b15764b97fad8eedb61b1993c896e8b2d0ad4
jsalareixach <joan.sala@streamsets.com>
2019-09-13 12:39:41 -0700

    SDC-12319. Add STF for Cassandra in no-batch mode
    Change-Id: I6f3cf4a30bc2b66036301a70a9f5f1d275caf477
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/1161
    Reviewed-by: Hugo Prol <hugo@streamsets.com>

--- stage/test_cassandra_stages.py
+++ stage/test_cassandra_stages.py
@@ -17,6 +17,7 @@ import logging
 import string
 
 from streamsets.testframework.markers import cassandra
+from streamsets.testframework.markers import sdc_min_version
 from streamsets.testframework.utils import get_random_string
 
 logger = logging.getLogger(__name__)
@@ -47,6 +48,66 @@ def test_cassandra_destination(sdc_builder, sdc_executor, cassandra):
         {'field': '/contact/phone', 'columnName': 'phone'}],
                                          fully_qualified_table_name=f'{cassandra_keyspace}.{cassandra_table}',
                                          protocol_version='V4')
+    if cassandra.kerberos_enabled:
+        cassandra_destination.set_attributes(authentication_provider='KERBEROS')
+    else:
+        cassandra_destination.set_attributes(authentication_provider='PLAINTEXT', password=cassandra.password,
+                                             username=cassandra.username)
+
+    dev_raw_data_source >> cassandra_destination
+    pipeline = builder.build(title='Cassandra Destination pipeline').configure_for_environment(cassandra)
+    sdc_executor.add_pipeline(pipeline)
+
+    try:
+        client = cassandra.client
+        cluster = client.cluster
+        session = client.session
+
+        # create Cassandra required tables before pipeline can put data
+        session.execute(f"CREATE KEYSPACE {cassandra_keyspace} WITH replication = "
+                        f"{{'class': 'SimpleStrategy', 'replication_factor': 1}}")
+        session.execute(f'CREATE TABLE {cassandra_keyspace}.{cassandra_table} '
+                        f'(name text PRIMARY KEY, zip_code int, phone int)')
+
+        sdc_executor.start_pipeline(pipeline).wait_for_pipeline_batch_count(1)
+        sdc_executor.stop_pipeline(pipeline)
+
+        # read data from Cassandra and assert to what pipeline has ingested
+        rows = session.execute(f'SELECT * FROM {cassandra_keyspace}.{cassandra_table}').current_rows
+        assert len(rows) == len(raw_dict)
+        for index, row in enumerate(rows):
+            for key, value in raw_dict[index]['contact'].items():
+                assert getattr(row, key) == value
+    finally:
+        # drop table and keyspace from Cassandra
+        session.execute(f'DROP TABLE {cassandra_keyspace}.{cassandra_table}')
+        session.execute(f'DROP KEYSPACE {cassandra_keyspace}')
+        cluster.shutdown()
+
+
+@cassandra
+@sdc_min_version('3.11.0')
+def test_cassandra_destination_no_batch(sdc_builder, sdc_executor, cassandra):
+    """Test for Cassandra destination stage. Support to test Kerberos or plain text only. The pipeline looks like:
+
+        dev_raw_data_source >> cassandra_destination
+    """
+    raw_dict = [dict(contact=dict(name='Jane Smith', phone=2124050000, zip_code=27023)),
+                dict(contact=dict(name='San', phone=2120998998, zip_code=14305))]
+    raw_data = json.dumps(raw_dict)
+    cassandra_keyspace = get_random_string(string.ascii_letters, 10)
+    cassandra_table = 'contact'
+
+    builder = sdc_builder.get_pipeline_builder()
+    dev_raw_data_source = builder.add_stage('Dev Raw Data Source')
+    dev_raw_data_source.set_attributes(data_format='JSON', json_content='ARRAY_OBJECTS', raw_data=raw_data)
+    cassandra_destination = builder.add_stage('Cassandra', type='destination')
+    cassandra_destination.set_attributes(field_to_column_mapping=[
+        {'field': '/contact/name', 'columnName': 'name'},
+        {'field': '/contact/zip_code', 'columnName': 'zip_code'},
+        {'field': '/contact/phone', 'columnName': 'phone'}],
+                                         fully_qualified_table_name=f'{cassandra_keyspace}.{cassandra_table}',
+                                         protocol_version='V4', disable_batch_insert=True)
     if cassandra.kerberos_enabled:
         cassandra_destination.set_attributes(authentication_provider='KERBEROS')
     else:
