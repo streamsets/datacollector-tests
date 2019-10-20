eb6f135e14c4db635c0a6e7604afbd87051e4899
Dima Spivak <dima@streamsets.com>
2019-10-19 20:39:41 -0700

    SDC-12685. Add stage.configuration.test_field_splitter::test_field_to_split
    Change-Id: I2c9c4c11391cd0beb536eddd432bcc412de75d83
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/1270
    Reviewed-by: KirtiVelankar <kirti@streamsets.com>

--- stage/configuration/test_field_splitter_processor.py
+++ stage/configuration/test_field_splitter_processor.py
@@ -1,5 +1,6 @@
-import pytest
+import json
 
+import pytest
 from streamsets.testframework.decorators import stub
 
 
@@ -9,9 +10,28 @@ def test_field_for_remaining_splits(sdc_builder, sdc_executor, stage_attributes)
     pass
 
 
-@stub
 def test_field_to_split(sdc_builder, sdc_executor):
-    pass
+    try:
+        DATA = dict(name='Al Gore', birthplace='Washington, D.C.')
+        EXPECTED_SPLIT_DATA = dict(fieldSplit1='Al', fieldSplit2='Gore', birthplace='Washington, D.C.')
+
+        pipeline_builder = sdc_builder.get_pipeline_builder()
+
+        dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+        dev_raw_data_source.raw_data = json.dumps(DATA)
+        field_splitter = pipeline_builder.add_stage('Field Splitter').set_attributes(field_to_split='/name',
+                                                                                     separator='[ ]')
+        trash = pipeline_builder.add_stage('Trash')
+
+        dev_raw_data_source >> field_splitter >> trash
+        pipeline = pipeline_builder.build()
+
+        sdc_executor.add_pipeline(pipeline)
+        snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True).snapshot
+        record = snapshot[field_splitter].output[0]
+        assert record.field == EXPECTED_SPLIT_DATA
+    finally:
+        sdc_executor.stop_pipeline(pipeline)
 
 
 @stub
