daacaf5da66fe5d5ccea18f948b9695af6e9402f
Dima Spivak <dima@streamsets.com>
2019-10-19 17:11:47 -0700

    SDC-12684. Add configuration tests for Send Response to Origin destination
    Change-Id: Ibc59121f6597ab4bc4c6927b20ec8faa1100b2db
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/1269
    Reviewed-by: Dima Spivak <dima@streamsets.com>

--- stage/configuration/test_send_response_to_origin_destination.py
+++ stage/configuration/test_send_response_to_origin_destination.py
@@ -1,9 +1,29 @@
-import pytest
+import requests
 
-from streamsets.testframework.decorators import stub
+APPLICATION_ID = 'keanu'
+HTTP_LISTENING_PORT = 8000
+STATUS_CODE = 867
 
-
-@stub
 def test_status_code(sdc_builder, sdc_executor):
-    pass
+    try:
+        pipeline_builder = sdc_builder.get_pipeline_builder()
 
+        rest_service = pipeline_builder.add_stage('REST Service')
+        rest_service.application_id = APPLICATION_ID
+        rest_service.http_listening_port = HTTP_LISTENING_PORT
+
+        send_response_to_origin = pipeline_builder.add_stage('Send Response to Origin')
+        send_response_to_origin.status_code = STATUS_CODE
+
+        rest_service >> send_response_to_origin
+        pipeline = pipeline_builder.build()
+
+        sdc_executor.add_pipeline(pipeline)
+        sdc_executor.start_pipeline(pipeline)
+
+        protocol = 'https' if sdc_executor.https else 'http'
+        rest_service_url = f'{protocol}://{sdc_executor.server_host}:{HTTP_LISTENING_PORT}'
+        assert requests.get(rest_service_url,
+                            headers={'X-SDC-APPLICATION-ID': APPLICATION_ID}).status_code == STATUS_CODE
+    finally:
+        sdc_executor.stop_pipeline(pipeline)
