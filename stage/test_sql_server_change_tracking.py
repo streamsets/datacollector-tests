58a1680b3c6792939d93f177bf48022dbd350f66
Toni <toni@streamsets.com>
2019-11-05 12:02:14 +0100

    SDC-12797. Reserved words for table names on SQLServerCT test
    Change-Id: I6193ebbe8fdc1e5b05da99aa1c531a74ece432b8
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/1308
    Reviewed-by: Xavier Baques Arcusa <xavi@streamsets.com>

--- stage/test_sql_server_change_tracking.py
+++ stage/test_sql_server_change_tracking.py
@@ -151,3 +151,72 @@ def test_sql_server_change_tracking_with_committed_offset(sdc_builder, sdc_execu
     finally:
         logger.info('Dropping table %s...', table_name)
         table.drop(database.engine)
+
+
+@database('sqlserver')
+def test_sql_server_change_tracking_reserved_words(sdc_builder, sdc_executor, database):
+    """Validate the pipeline with committed offset on a table that has a reserved word 'Order'.
+    Start the pipeline run the first batch, stop the pipeline and restart the pipeline
+    with committed offset. The pipeline will look like:
+        sqlserver_change_tracking >> trash
+    """
+    if not database.is_ct_enabled:
+        pytest.skip('Test only runs against SQL Server with CT enabled.')
+
+    table_name = 'Order'
+
+    table = sqlalchemy.Table(table_name, sqlalchemy.MetaData(),
+                             sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+                             sqlalchemy.Column('name', sqlalchemy.String(25)),
+                             sqlalchemy.Column('dt', sqlalchemy.String(20)))
+
+    rows_in_database = [{'id': 1, 'name': 'Ji Sun', 'dt': '2017-05-03'},
+                        {'id': 2, 'name': 'Jarcec', 'dt': '2017-05-03'},
+                        {'id': 3, 'name': 'Santhosh', 'dt': '2017-05-03'}]
+
+    table_configs = [{'initialOffset': 0, 'schema': 'dbo', 'tablePattern': f'{table_name}'}]
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+    sql_server_change_tracking = pipeline_builder.add_stage('SQL Server Change Tracking Client')
+    sql_server_change_tracking.set_attributes(include_the_latest_data_in_the_record=True,
+                                              table_configs=table_configs)
+
+    trash = pipeline_builder.add_stage('Trash')
+
+    sql_server_change_tracking >> trash
+    pipeline = pipeline_builder.build().configure_for_environment(database)
+    sdc_executor.add_pipeline(pipeline)
+
+    # insert sample data into the table
+    try:
+        logger.info('Creating table %s...', table_name)
+        table.create(database.engine)
+
+        connection = database.engine.connect()
+
+        # enable change tracking on table
+        connection.execute(f'ALTER TABLE [{table_name}] ENABLE change_tracking WITH (track_columns_updated = on)')
+
+        # insert sample data
+        logger.info('Adding %s rows into %s...', len(rows_in_database), table_name)
+        connection.execute(table.insert(), rows_in_database)
+
+        sdc_executor.start_pipeline(pipeline).wait_for_pipeline_batch_count(3)
+        sdc_executor.stop_pipeline(pipeline)
+
+        # delete sample data
+        logger.info('Deleting %s rows into %s...', len(rows_in_database), table_name)
+        connection.execute(table.delete(), rows_in_database)
+
+        snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True, batch_size=10).snapshot
+        sdc_executor.stop_pipeline(pipeline)
+
+        # assert all the data captured have the same raw_data
+        output_records = snapshot[sql_server_change_tracking.instance_name].output
+
+        assert 3 == len(output_records)
+
+        for i in range(0, 3):
+            assert output_records[i].get_field_data('/id') == rows_in_database[i].get('id')
+    finally:
+        logger.info('Dropping table %s...', table_name)
+        table.drop(database.engine)
