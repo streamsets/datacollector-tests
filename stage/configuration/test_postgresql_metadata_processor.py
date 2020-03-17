8eee1b4cb887b1cbc21c26e5fe75b045e459191e
Kirti <kirti@streamsets.com>
2020-03-17 12:39:13 -0700

    SDC-13927. Create test_stubs for branded PostgreSQL
    Change-Id: Ic92457ec81d61971ed0625b93e2f89ad32914924
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/1682
    Reviewed-by: KirtiVelankar <kirti@streamsets.com>

--- stage/configuration/test_postgresql_metadata_processor.py
+++ stage/configuration/test_postgresql_metadata_processor.py
@@ -1,120 +1,153 @@
+import logging
+import string
+
+
 import pytest
-
+import sqlalchemy
 from streamsets.testframework.decorators import stub
+from streamsets.testframework.markers import category, sdc_min_version
+from streamsets.testframework.utils import get_random_string
+
+
+logger = logging.getLogger(__name__)
+
+
+pytestmark = [pytest.mark.sdc_min_version('3.15.0'), pytest.mark.database('postgresql')
 
 
 @stub
-def test_additional_jdbc_configuration_properties(sdc_builder, sdc_executor):
+@category('advanced')
+def test_additional_jdbc_configuration_properties(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_connection_health_test_query(sdc_builder, sdc_executor):
+@category('advanced')
+def test_connection_health_test_query(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_connection_timeout_in_seconds(sdc_builder, sdc_executor):
+@category('advanced')
+def test_connection_timeout_in_seconds(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_decimal_precision_attribute(sdc_builder, sdc_executor):
+@category('basic')
+def test_decimal_precision_attribute(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_decimal_scale_attribute(sdc_builder, sdc_executor):
+@category('basic')
+def test_decimal_scale_attribute(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_idle_timeout_in_seconds(sdc_builder, sdc_executor):
+@category('advanced')
+def test_idle_timeout_in_seconds(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_init_query(sdc_builder, sdc_executor):
+@category('advanced')
+def test_init_query(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_jdbc_connection_string(sdc_builder, sdc_executor):
+@category('basic')
+def test_jdbc_connection_string(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_jdbc_driver_class_name(sdc_builder, sdc_executor):
+@category('advanced')
+def test_jdbc_driver_class_name(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_max_connection_lifetime_in_seconds(sdc_builder, sdc_executor):
+@category('advanced')
+def test_max_connection_lifetime_in_seconds(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_maximum_pool_size(sdc_builder, sdc_executor):
+@category('advanced')
+def test_maximum_pool_size(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_minimum_idle_connections(sdc_builder, sdc_executor):
+@category('advanced')
+def test_minimum_idle_connections(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
+@category('basic')
 @pytest.mark.parametrize('stage_attributes', [{'on_record_error': 'DISCARD'},
                                               {'on_record_error': 'STOP_PIPELINE'},
                                               {'on_record_error': 'TO_ERROR'}])
-def test_on_record_error(sdc_builder, sdc_executor, stage_attributes):
+def test_on_record_error(sdc_builder, sdc_executor, database, stage_attributes):
     pass
 
 
 @stub
+@category('basic')
 @pytest.mark.parametrize('stage_attributes', [{'use_credentials': True}])
-def test_password(sdc_builder, sdc_executor, stage_attributes):
+def test_password(sdc_builder, sdc_executor, database, stage_attributes):
     pass
 
 
 @stub
-def test_preconditions(sdc_builder, sdc_executor):
+@category('basic')
+def test_preconditions(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_required_fields(sdc_builder, sdc_executor):
+@category('basic')
+def test_required_fields(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_schema_name(sdc_builder, sdc_executor):
+@category('basic')
+def test_schema_name(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
-def test_table_name(sdc_builder, sdc_executor):
+@category('basic')
+def test_table_name(sdc_builder, sdc_executor, database):
     pass
 
 
 @stub
+@category('advanced')
 @pytest.mark.parametrize('stage_attributes', [{'transaction_isolation': 'DEFAULT'},
                                               {'transaction_isolation': 'TRANSACTION_READ_COMMITTED'},
                                               {'transaction_isolation': 'TRANSACTION_READ_UNCOMMITTED'},
                                               {'transaction_isolation': 'TRANSACTION_REPEATABLE_READ'},
                                               {'transaction_isolation': 'TRANSACTION_SERIALIZABLE'}])
-def test_transaction_isolation(sdc_builder, sdc_executor, stage_attributes):
+def test_transaction_isolation(sdc_builder, sdc_executor, database, stage_attributes):
     pass
 
 
 @stub
+@category('advanced')
 @pytest.mark.parametrize('stage_attributes', [{'use_credentials': False}, {'use_credentials': True}])
-def test_use_credentials(sdc_builder, sdc_executor, stage_attributes):
+def test_use_credentials(sdc_builder, sdc_executor, database, stage_attributes):
     pass
 
 
 @stub
+@category('basic')
 @pytest.mark.parametrize('stage_attributes', [{'use_credentials': True}])
-def test_username(sdc_builder, sdc_executor, stage_attributes):
+def test_username(sdc_builder, sdc_executor, database, stage_attributes):
     pass
 
