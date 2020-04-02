1f92dc110c3c0112b4f557e44627f92b1bf083d1
Ji Sun <jisun@streamsets.com>
2020-04-02 15:52:39 -0700

    SDC-9417. SQL Server Change Tracking Origin on composite primary key
    Change-Id: I2b865dab1dd02da60233f5931f2be5c6963af8e4
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/1796
    Reviewed-by: Karthik Iyer <karthik@streamsets.com>

--- stage/test_sql_server_change_tracking.py
+++ stage/test_sql_server_change_tracking.py
@@ -222,3 +222,71 @@ def test_sql_server_change_tracking_reserved_words(sdc_builder, sdc_executor, da
         logger.info('Dropping table %s...', table_name)
         table.drop(database.engine)
 
+
+@database('sqlserver')
+@pytest.mark.parametrize('include_record', [True, False])
+def test_sql_server_change_tracking_with_composite_primary_key(sdc_builder, sdc_executor, database, include_record):
+    """Validate the pipeline on composite primary key. Start the pipeline run the first batch, stop the pipeline and restart the pipeline
+    with committed offset. The pipeline will look like:
+        sqlserver_change_tracking >> trash
+    """
+    if not database.is_ct_enabled:
+        pytest.skip('Test only runs against SQL Server with CT enabled.')
+
+    table_name = get_random_string(string.ascii_lowercase, 20)
+
+    table = sqlalchemy.Table(table_name, sqlalchemy.MetaData(),
+                             sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+                             sqlalchemy.Column('name', sqlalchemy.String(25), primary_key=True),
+                             sqlalchemy.Column('pk', sqlalchemy.Integer, primary_key=True),
+                             sqlalchemy.Column('dt', sqlalchemy.String(20)))
+    rows_in_database = [{'id': 1, 'name': 'Ji Sun', 'pk': 1, 'dt': '2017-05-03'},
+                        {'id': 2, 'name': 'Jarcec', 'pk': 2, 'dt': '2017-05-03'},
+                        {'id': 3, 'name': 'Santhosh', 'pk': 3, 'dt': '2017-05-03'}]
+    table_configs = [{'initialOffset': 0, 'schema': 'dbo', 'tablePattern': f'{table_name}'}]
+
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+    sql_server_change_tracking = pipeline_builder.add_stage('SQL Server Change Tracking Client')
+    sql_server_change_tracking.set_attributes(include_the_latest_data_in_the_record=include_record,
+                                              table_configs=table_configs)
+    trash = pipeline_builder.add_stage('Trash')
+
+    sql_server_change_tracking >> trash
+    pipeline = pipeline_builder.build().configure_for_environment(database)
+    sdc_executor.add_pipeline(pipeline)
+
+    try:
+        logger.info('Creating table %s...', table_name)
+        table.create(database.engine)
+        connection = database.engine.connect()
+        # enable change tracking on table
+        connection.execute(f'ALTER TABLE {table_name} ENABLE change_tracking WITH (track_columns_updated = on)')
+        # insert sample data
+        logger.info('Adding %s rows into %s...', len(rows_in_database), table_name)
+        connection.execute(table.insert(), rows_in_database)
+
+        sdc_executor.start_pipeline(pipeline).wait_for_pipeline_batch_count(len(rows_in_database))
+        sdc_executor.stop_pipeline(pipeline)
+
+        # Let's validate offset
+        offset = sdc_executor.api_client.get_pipeline_committed_offsets(pipeline.id).response.json()
+        assert offset is not None
+        assert offset['offsets'] is not None
+        expected_key = [f"tableName=dbo.{table_name};;;partitioned=false;;;partitionSequence=-1;;;partitionStartOffsets=;;;partitionMaxOffsets=;;;usingNonIncrementalLoad=false"]
+        list(offset['offsets'].keys()) == expected_key
+
+        # delete sample data
+        logger.info('Deleting %s rows into %s...', len(rows_in_database), table_name)
+        connection.execute(table.delete(), rows_in_database)
+        snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True, batch_size=10).snapshot
+        sdc_executor.stop_pipeline(pipeline)
+
+        # assert all the data captured have the same raw_data
+        output_records = snapshot[sql_server_change_tracking.instance_name].output
+        assert 3 == len(output_records)
+        for i in range(0, 3):
+            assert output_records[i].get_field_data('/id') == rows_in_database[i].get('id')
+    finally:
+        logger.info('Dropping table %s...', table_name)
+        table.drop(database.engine)
+
