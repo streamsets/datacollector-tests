c96e03d2ffd3d052a487b18d393b7b9f5e1f6b65
Jarek Jarcec Cecho <jarcec@jarcec.net>
2020-06-26 09:43:36 -0700

    SDC-14971: Missing configs are not added while validating Test Origin
    Change-Id: Ice8f1f1b0dc4680c990b8b2b48a6c89316de7072
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/2202
    Reviewed-by: Santhosh Kumar Manavasi Lakshminarayanan <santhosh@streamsets.com>

--- datacollector/test_pipeline.py
+++ datacollector/test_pipeline.py
@@ -14,6 +14,15 @@
 
 import pytest
 
+import sqlalchemy
+import logging
+import string
+
+from streamsets.testframework.environments.databases import OracleDatabase
+from streamsets.testframework.markers import database
+from streamsets.testframework.utils import get_random_string
+
+logger = logging.getLogger(__name__)
 
 def test_pipeline_downgrade(sdc_executor):
     """Ensure that when user tries to downgrade pipeline, we issue a proper error message."""
@@ -33,3 +42,49 @@ def test_pipeline_downgrade(sdc_executor):
         sdc_executor.validate_pipeline(pipeline)
 
     assert 'VALIDATION_0096' in e.value.issues
+
+
+@database
+def test_pipeline_preview_with_test_stage(sdc_builder, sdc_executor, database):
+    """Test preview with test origin."""
+    if isinstance(database, OracleDatabase):
+        pytest.skip('This test does not support oracle and its upper casing of column names.')
+
+    metadata = sqlalchemy.MetaData()
+    table_name = get_random_string(string.ascii_lowercase, 20)
+    table = sqlalchemy.Table(
+        table_name,
+        metadata,
+        sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+        sqlalchemy.Column('name', sqlalchemy.String(32))
+    )
+    table.create(database.engine)
+
+    builder = sdc_builder.get_pipeline_builder()
+    generator = builder.add_stage(label='Dev Data Generator')
+    trash = builder.add_stage(label='Trash')
+
+    jdbc = builder.add_test_origin_stage(label='JDBC Query Consumer')
+    jdbc.incremental_mode = True
+    jdbc.sql_query = 'SELECT * FROM {0} WHERE '.format(table_name) + 'id > ${OFFSET} ORDER BY id'
+    jdbc.initial_offset = '0'
+    jdbc.offset_column = 'id'
+
+    generator >> trash
+    pipeline = builder.build().configure_for_environment(database)
+
+    sdc_executor.add_pipeline(pipeline)
+
+    try:
+        connection = database.engine.connect()
+        connection.execute(table.insert(), [{'id': 1}])
+
+        preview = sdc_executor.run_pipeline_preview(pipeline, test_origin=True).preview
+        assert preview is not None
+        assert preview.issues.issues_count == 0
+
+        assert len(preview[jdbc].output) == 1
+        assert preview[jdbc].output[0].field['id'].value == 1
+    finally:
+        logger.info('Dropping table %s in %s database...', table_name, database.type)
+        table.drop(database.engine)
