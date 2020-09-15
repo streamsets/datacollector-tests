4bf55a19dd83b7349ee740ee43f4f199b75bad2f
Jarek Jarcec Cecho <jarcec@apache.org>
2020-09-14 17:26:12 -0700

    SDC-15746: JDBC Metadata Processor throws NullPointerException on unsupported type
    Change-Id: I7e5afb33679cf9af264b31d8c82db8053732f2eb
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/2534
    Reviewed-by: Alex Sánchez <alex.sanchez@streamsets.com>

---
+++ stage/test_postgresql_metadata_processor.py
@@ -0,0 +1,67 @@
+# Copyright 2020 StreamSets Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import string
+
+from streamsets.testframework.markers import database
+from streamsets.testframework.utils import get_random_string
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+# SDC-15746: JDBC Metadata Processor throws NullPointerException on unsupported type
+@database('postgresql')
+def test_unsupported_type_in_precreated_table(sdc_builder, sdc_executor, database, keep_data):
+    table_name = get_random_string(string.ascii_lowercase, 20)
+    connection = database.engine.connect()
+    try:
+        # We create table manually to get the right type
+        # Smallint is currently not supported in PostgreSQL Metadata Processor
+        connection.execute(f"""
+            CREATE TABLE {table_name}(
+                id int primary key,
+                code smallint
+            )
+        """)
+
+        builder = sdc_builder.get_pipeline_builder()
+
+        source = builder.add_stage('Dev Raw Data Source')
+        source.stop_after_first_batch = True
+        source.data_format = 'JSON'
+        source.raw_data = '{"id":1, "code": 2}'
+
+        processor = builder.add_stage('PostgreSQL Metadata')
+        processor.table_name = table_name
+
+        wiretap = builder.add_wiretap()
+
+        source >> processor >> wiretap.destination
+
+        # Create & run the pipeline
+        pipeline = builder.build().configure_for_environment(database)
+        sdc_executor.add_pipeline(pipeline)
+        sdc_executor.start_pipeline(pipeline).wait_for_finished()
+
+        # The record should be sent to error with proper error code
+        errors = wiretap.error_records
+        assert len(errors) == 1
+        assert errors[0].header['errorCode'] == 'JDBC_91'
+    finally:
+        if not keep_data:
+            logger.info('Dropping table %s in %s database ...', table_name, database.type)
+            connection.execute(f"DROP TABLE IF EXISTS {table_name}")
+
