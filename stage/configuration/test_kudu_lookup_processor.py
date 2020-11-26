6c2f1755db17a635e42f20f859f7980f1088e91f
Konstantin Golub <konstantin@streamsets.com>
2020-11-26 03:06:30 +0000

    SDC-16247. Implement tests for Cache doesn't work in Kudu Lookup Processor
    We want to test that when the caching is enabled we get obsolete data.
    So we will insert a record in the storage and then we will create
    a file. The pipeline reads the file, then loads a record from the
    storage and replaces the value we have read from the file with the one
    we have read from the storage.
    
    If we update the record in the storage and then create the second file
    on the file system then due to the caching enabled the value in the
    file will be replaced with the storage value we have loaded before and
    the updated record will not be loaded.
    
    If the caching is disabled the new value will be used.
    
    Seems there is a bug in the current implementation of Kudu Lookup
    Processor and the caching doesn't work, thus this test fails.
    If the issue we identified is really a bug then after fixing it the
    test will pass as expected.
    
    Please review the test and confirm that the scenario is valid and the
    test must pass if the cache implementation is not broken.
    
    Change-Id: Ibe58292b02ba68a3c7bc369749a8a85a9d6440e4
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/2793
    Reviewed-by: Alex Sánchez <alex.sanchez@streamsets.com>
    Reviewed-by: Hugo Prol <hugo@streamsets.com>

--- stage/configuration/test_kudu_lookup_processor.py
+++ stage/configuration/test_kudu_lookup_processor.py
@@ -1,7 +1,17 @@
 import pytest
+import os
+import tempfile
+import string
+import sqlalchemy
+import json
 
 from streamsets.testframework.decorators import stub
+from streamsets.testframework.utils import get_random_string
+from streamsets.testframework.markers import cluster, sdc_min_version
 
+
+DEFAULT_IMPALA_DB = 'default'
+DEFAULT_KUDU_PORT = 7051
 
 @stub
 def test_admin_operation_timeout_in_milliseconds(sdc_builder, sdc_executor):
@@ -19,10 +29,105 @@ def test_column_to_output_field_mapping(sdc_builder, sdc_executor):
     pass
 
 
-@stub
+@cluster('cdh')
 @pytest.mark.parametrize('stage_attributes', [{'enable_local_caching': False}, {'enable_local_caching': True}])
-def test_enable_local_caching(sdc_builder, sdc_executor, stage_attributes):
-    pass
+def test_enable_local_caching(sdc_builder, sdc_executor, stage_attributes, cluster):
+    """
+    We want to test that when the caching is enabled we get obsolete data.
+    So we will insert a record in the storage and then we will create
+    a file. The pipeline reads the file, then loads a record from the
+    storage and replaces the value we have read from the file with the one
+    we have read from the storage.
+
+    If we update the record in the storage and then create the second file
+    on the file system then due to the caching enabled the value in the
+    file will be replaced with the storage value we have loaded before and
+    the updated record will not be loaded.
+
+    If the caching is disabled the new value will be used.
+
+    The pipeline is as follows:
+
+    Directory >> Kudu Lookup Processor >> Wiretap
+    """
+
+    dir_path = os.path.join(tempfile.gettempdir(), get_random_string(string.ascii_lowercase))
+    table_name = get_random_string(string.ascii_lowercase, 10)
+
+    builder = sdc_builder.get_pipeline_builder()
+    directory = builder.add_stage('Directory', type='origin')
+    directory.data_format = 'JSON'
+    directory.json_content = 'MULTIPLE_OBJECTS'
+    directory.files_directory = dir_path
+    directory.file_name_pattern = '*.json'
+    directory.file_name_pattern_mode = 'GLOB'
+
+    kudu = builder.add_stage('Kudu Lookup')
+    kudu.kudu_table_name = f'impala::default.{table_name}'
+    kudu.key_columns_mapping = [dict(field='/f1', columnName='id')]
+    kudu.column_to_output_field_mapping = [dict(field='/d1', columnName='name', defaultValue='no_name')]
+    kudu.missing_lookup_behavior = 'PASS_RECORD_ON'
+    kudu.enable_table_caching = True
+    kudu.eviction_policy_type = 'EXPIRE_AFTER_WRITE'
+    kudu.expiration_time = 1
+    kudu.time_unit = 'HOURS'
+    kudu.set_attributes(**stage_attributes)
+
+    wiretap = builder.add_wiretap()
+
+    directory >> kudu >> wiretap.destination
+
+    pipeline = builder.build().configure_for_environment(cluster)
+
+    sdc_executor.add_pipeline(pipeline)
+
+    metadata = sqlalchemy.MetaData()
+    table = sqlalchemy.Table(table_name,
+                             metadata,
+                             sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+                             sqlalchemy.Column('name', sqlalchemy.String),
+                             impala_partition_by='HASH PARTITIONS 16',
+                             impala_stored_as='KUDU',
+                             impala_table_properties={
+                                 'kudu.master_addresses': f'{cluster.server_host}:{DEFAULT_KUDU_PORT}',
+                                 'kudu.num_tablet_replicas': '1'
+                             })
+
+    engine = cluster.kudu.engine
+    table.create(engine)
+
+    try:
+        sdc_executor.execute_shell(f'mkdir -p {dir_path}')
+        sdc_executor.write_file(os.path.join(dir_path, 'a.json'), json.dumps({"f1": 1, "d1": "old_name1"}))
+
+        conn = engine.connect()
+        conn.execute(table.insert(), [{'id': 1, 'name': 'name1'}])
+
+        status = sdc_executor.start_pipeline(pipeline)
+        status.wait_for_pipeline_batch_count(2)
+
+        conn.execute(table.update().where(table.c.id == 1).values(name='name2'))
+
+        sdc_executor.write_file(os.path.join(dir_path, 'b.json'), json.dumps({"f1": 1, "d1": "old_name2"}))
+
+        status.wait_for_pipeline_batch_count(4)
+
+        output_records = [{
+            'id': record.field['f1'].value,
+            'name': record.field['d1'].value
+        } for record in wiretap.output_records]
+
+        if stage_attributes['enable_local_caching']:
+            assert [{'id': 1, 'name': 'name1'}, {'id': 1, 'name': 'name1'}] == output_records
+        else:
+            assert [{'id': 1, 'name': 'name1'}, {'id': 1, 'name': 'name2'}] == output_records
+
+    finally:
+        try:
+            sdc_executor.stop_pipeline(pipeline)
+        finally:
+            table.drop(engine)
+            sdc_executor.execute_shell(f'rm -fr {dir_path}')
 
 
 @stub
@@ -132,4 +237,3 @@ def test_required_fields(sdc_builder, sdc_executor):
                                               {'enable_local_caching': True, 'time_unit': 'SECONDS'}])
 def test_time_unit(sdc_builder, sdc_executor, stage_attributes):
     pass
-
