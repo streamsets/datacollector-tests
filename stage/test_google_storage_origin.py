91334d7aa746261fbea17290984bc158ba9d0448
Alex Sánchez <alex.sanchez@streamsets.com>
2020-12-22 11:26:46 +0100

    SDC-16360. Isolate Google Storage origin tests and get rid of snapshots
    Change-Id: I6bd2c0ce036a69624336f577423e5149949da226
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/2938
    Reviewed-by: <marina@streamsets.com>

---
+++ stage/test_google_storage_origin.py
@@ -0,0 +1,251 @@
+# Copyright 2020 StreamSets Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import time
+from string import ascii_letters, ascii_lowercase
+
+import pytest
+from streamsets.testframework.markers import gcp, sdc_min_version
+from streamsets.testframework.utils import get_random_string
+
+logger = logging.getLogger(__name__)
+
+
+@gcp
+@sdc_min_version('3.0.0.0')
+def test_google_storage_origin(sdc_builder, sdc_executor, gcp):
+    """
+    Write data to Google cloud storage using Storage Client
+    and then check if Google Storage Origin receives them using wiretap.
+
+    The pipeline looks like:
+        google_cloud_storage_origin >> wiretap
+    """
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    bucket_name = get_random_string(ascii_lowercase, 10)
+
+    storage_client = gcp.storage_client
+
+    google_cloud_storage = pipeline_builder.add_stage('Google Cloud Storage', type='origin')
+
+    google_cloud_storage.set_attributes(bucket=bucket_name,
+                                        common_prefix='gcs-test',
+                                        prefix_pattern='**/*.txt',
+                                        data_format='TEXT')
+    wiretap = pipeline_builder.add_wiretap()
+
+    google_cloud_storage >> wiretap.destination
+
+    pipeline = pipeline_builder.build().configure_for_environment(gcp)
+    sdc_executor.add_pipeline(pipeline)
+
+    created_bucket = storage_client.create_bucket(bucket_name)
+    try:
+        data = [get_random_string(ascii_letters, 100) for _ in range(10)]
+        blob = created_bucket.blob('gcs-test/a/b/c/d/e/sdc-test.txt')
+        blob.upload_from_string('\n'.join(data))
+
+        logger.info('Starting GCS Origin pipeline and wait until the information is read ...')
+        sdc_executor.start_pipeline(pipeline)
+        sdc_executor.wait_for_pipeline_metric(pipeline, 'input_record_count', 10)
+        sdc_executor.stop_pipeline(pipeline)
+
+        rows_from_wiretap = [record.field['text'] for record in wiretap.output_records]
+
+        assert len(data) == len(rows_from_wiretap)
+        assert rows_from_wiretap == data
+    finally:
+        created_bucket.delete(force=True)
+
+
+@gcp
+@sdc_min_version('3.0.0.0')
+def test_google_storage_origin_idle_start(sdc_builder, sdc_executor, gcp):
+    """
+    Start Google Cloud Storage pipeline with no previous offset and no data to consume.
+    Then produce messages and check that the pipeline hasn't stopped and it consumes all the new data.
+
+    The pipeline looks like:
+        google_cloud_storage_origin >> trash
+    """
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    bucket_name = get_random_string(ascii_lowercase, 10)
+
+    storage_client = gcp.storage_client
+
+    google_cloud_storage = pipeline_builder.add_stage('Google Cloud Storage', type='origin')
+
+    google_cloud_storage.set_attributes(bucket=bucket_name,
+                                        common_prefix='gcs-test',
+                                        prefix_pattern='**/*.txt',
+                                        data_format='TEXT')
+    wiretap = pipeline_builder.add_wiretap()
+
+    google_cloud_storage >> wiretap.destination
+
+    pipeline = pipeline_builder.build().configure_for_environment(gcp)
+    sdc_executor.add_pipeline(pipeline)
+
+    created_bucket = storage_client.create_bucket(bucket_name)
+    try:
+        logger.info('Starting GCS Origin pipeline...')
+        start_command = sdc_executor.start_pipeline(pipeline)
+
+        # Wait for 5 seconds - in this time the pipeline should have ended
+        time.sleep(5)
+
+        if pipeline and sdc_executor.get_pipeline_status(pipeline).response.json().get('status') == 'RUNNING':
+            data = [get_random_string(ascii_letters, length=100) for _ in range(10)]
+            blob = created_bucket.blob('gcs-test/a/b/c/d/e/sdc-test.txt')
+            blob.upload_from_string('\n'.join(data))
+
+            start_command.wait_for_pipeline_output_records_count(10)
+            sdc_executor.stop_pipeline(pipeline)
+
+            rows = [record.field['text']
+                    for record in wiretap.output_records]
+
+            assert rows == data
+        else:
+            pytest.fail('Pipeline stopped before data was produced in the bucket.')
+    finally:
+        created_bucket.delete(force=True)
+
+
+@gcp
+@sdc_min_version('3.0.0.0')
+def test_google_storage_origin_stop_resume(sdc_builder, sdc_executor, gcp):
+    """
+    Write data to Google cloud storage using Storage Client
+    and then check if Google Storage Origin receives them using wiretap.
+    Stop the pipeline, add more data, resume the pipeline and check the new data.
+
+    The pipeline looks like:
+        google_cloud_storage_origin >> wiretap
+    """
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    bucket_name = get_random_string(ascii_lowercase, 10)
+
+    storage_client = gcp.storage_client
+
+    google_cloud_storage = pipeline_builder.add_stage('Google Cloud Storage', type='origin')
+
+    google_cloud_storage.set_attributes(bucket=bucket_name,
+                                        common_prefix='gcs-test',
+                                        prefix_pattern='**/*.txt',
+                                        data_format='TEXT')
+    wiretap = pipeline_builder.add_wiretap()
+
+    google_cloud_storage >> wiretap.destination
+
+    pipeline = pipeline_builder.build().configure_for_environment(gcp)
+    sdc_executor.add_pipeline(pipeline)
+
+    created_bucket = storage_client.create_bucket(bucket_name)
+    try:
+        data = [get_random_string(ascii_letters, length=100) for _ in range(10)]
+        blob = created_bucket.blob('gcs-test/a/b/c/d/e/sdc-test-1.txt')
+        blob.upload_from_string('\n'.join(data))
+
+        logger.info('Starting GCS Origin pipeline and waiting for it to produce records...')
+        sdc_executor.start_pipeline(pipeline)
+        sdc_executor.wait_for_pipeline_metric(pipeline, 'input_record_count', 10)
+        sdc_executor.stop_pipeline(pipeline)
+
+        rows_from_wiretap = [record.field['text'] for record in wiretap.output_records]
+
+        assert len(data) == len(rows_from_wiretap)
+        assert rows_from_wiretap == data
+
+        data = [get_random_string(ascii_letters, length=100) for _ in range(10)]
+        blob = created_bucket.blob('gcs-test/a/b/c/d/e/sdc-test-1.txt')
+        blob.upload_from_string('\n'.join(data))
+
+        wiretap.reset()
+
+        logger.info('Starting Second Time GCS Origin pipeline and waiting for it to produce records again...')
+        sdc_executor.start_pipeline(pipeline)
+        sdc_executor.wait_for_pipeline_metric(pipeline, 'input_record_count', 10)
+        sdc_executor.stop_pipeline(pipeline)
+
+        rows_from_wiretap = [record.field['text'] for record in wiretap.output_records]
+
+        assert len(data) == len(rows_from_wiretap)
+        assert rows_from_wiretap == data
+    finally:
+        created_bucket.delete(force=True)
+
+
+@gcp
+@sdc_min_version('3.0.0.0')
+def test_google_storage_small_batch_size(sdc_builder, sdc_executor, gcp):
+    """
+    Write Excel file to Google Cloud Storage, create an origin with a batch size smaller than the number of
+    records in the file and check that no data is duplicated when reading the file.
+
+    The pipeline looks like:
+        google_cloud_storage_origin >> wiretap.destination
+    """
+
+    gcp_file_name = 'test_9_records.xls'
+
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    bucket_name = get_random_string(ascii_lowercase, 10)
+
+    storage_client = gcp.storage_client
+
+    google_cloud_storage = pipeline_builder.add_stage('Google Cloud Storage', type='origin')
+
+    google_cloud_storage.set_attributes(bucket=bucket_name,
+                                        common_prefix='gcs-test',
+                                        prefix_pattern=gcp_file_name,
+                                        data_format='EXCEL',
+                                        max_batch_size_in_records=1,
+                                        excel_header_option='WITH_HEADER')
+
+    pipeline_finisher_executor = pipeline_builder.add_stage('Pipeline Finisher Executor')
+    pipeline_finisher_executor.set_attributes(preconditions=['${record:eventType() == \'no-more-data\'}'],
+                                              on_record_error='DISCARD')
+
+    google_cloud_storage >= pipeline_finisher_executor
+
+    wiretap = pipeline_builder.add_wiretap()
+    google_cloud_storage >> wiretap.destination
+
+    pipeline = pipeline_builder.build(title='Google Cloud Storage').configure_for_environment(gcp)
+    sdc_executor.add_pipeline(pipeline)
+
+    created_bucket = storage_client.create_bucket(bucket_name)
+    try:
+        blob = created_bucket.blob('gcs-test/' + gcp_file_name)
+        blob.upload_from_filename('resources/gcp/' + gcp_file_name)
+
+        # Start the pipeline and make sure the timeout is defined, since the original bug involved
+        # an infinite loop reading the same file over and over again
+        sdc_executor.start_pipeline(pipeline).wait_for_finished(timeout_sec=300)
+
+        # Check that the file has been read through exactly once
+        output_records = [record.field for record in wiretap.output_records]
+        assert len(output_records) == 9
+        # Check that no error records were generated
+        error_records = [record.field for record in wiretap.error_records]
+        assert len(error_records) == 0
+
+    finally:
+        created_bucket.delete(force=True)
