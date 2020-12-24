f28a49a7663d23574713f901e11feb0e65d1638d
Alex Sánchez <alex.sanchez@streamsets.com>
2020-12-24 10:46:14 +0100

    SDC-16377. Migrate Field Renamer processor tests to wiretap
    Change-Id: Ia7aed4ea06ce8df148f1e1111aefcfa2492ebe18
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/2953
    Reviewed-by: Toni Martinez Gonzalez

--- stage/configuration/test_field_renamer_processor.py
+++ stage/configuration/test_field_renamer_processor.py
@@ -1,3 +1,17 @@
+# Copyright 2020 StreamSets Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 
 import pytest
@@ -5,28 +19,26 @@ from streamsets.testframework.decorators import stub
 
 
 def test_fields_to_rename(sdc_builder, sdc_executor):
-    try:
-        DATA = dict(name='Al Gore', birthplace='Washington, D.C.')
-        EXPECTED_RENAMED_DATA = dict(internetInventor='Al Gore', birthplace='Washington, D.C.')
+    DATA = dict(name='Al Gore', birthplace='Washington, D.C.')
+    EXPECTED_RENAMED_DATA = dict(internetInventor='Al Gore', birthplace='Washington, D.C.')
 
-        pipeline_builder = sdc_builder.get_pipeline_builder()
+    pipeline_builder = sdc_builder.get_pipeline_builder()
 
-        dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
-        dev_raw_data_source.data_format = 'JSON'
-        dev_raw_data_source.raw_data = json.dumps(DATA)
-        field_renamer = pipeline_builder.add_stage('Field Renamer')
-        field_renamer.fields_to_rename = [{'fromFieldExpression': '/name', 'toFieldExpression': '/internetInventor'}]
-        trash = pipeline_builder.add_stage('Trash')
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+    dev_raw_data_source.data_format = 'JSON'
+    dev_raw_data_source.raw_data = json.dumps(DATA)
+    dev_raw_data_source.stop_after_first_batch = True
+    field_renamer = pipeline_builder.add_stage('Field Renamer')
+    field_renamer.fields_to_rename = [{'fromFieldExpression': '/name', 'toFieldExpression': '/internetInventor'}]
+    wiretap = pipeline_builder.add_wiretap()
 
-        dev_raw_data_source >> field_renamer >> trash
-        pipeline = pipeline_builder.build()
+    dev_raw_data_source >> field_renamer >> wiretap.destination
+    pipeline = pipeline_builder.build()
 
-        sdc_executor.add_pipeline(pipeline)
-        snapshot = sdc_executor.capture_snapshot(pipeline, start_pipeline=True).snapshot
-        record = snapshot[field_renamer].output[0]
-        assert record.field == EXPECTED_RENAMED_DATA
-    finally:
-        sdc_executor.stop_pipeline(pipeline)
+    sdc_executor.add_pipeline(pipeline)
+    sdc_executor.start_pipeline(pipeline).wait_for_finished()
+    record = wiretap.output_records[0]
+    assert record.field == EXPECTED_RENAMED_DATA
 
 
 @stub
