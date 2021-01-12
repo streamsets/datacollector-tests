644a81ea59b62129e1d5f6b4620df141c5d81990
Alex Sánchez <alex.sanchez@streamsets.com>
2021-01-12 13:27:01 +0100

    SDC-16518. Migrate NiFi tests to wiretap
    Change-Id: Ie80cfc3ed975ba1ec24f7b76bee0378fe5e41a51
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/3060
    Reviewed-by: Martin Balzamo <martin@streamsets.com>

--- stage/test_nifi_stages.py
+++ stage/test_nifi_stages.py
@@ -19,26 +19,23 @@ from streamsets.testframework.markers import nifi, sdc_min_version
 
 logger = logging.getLogger(__name__)
 
-SNAPSHOT_TIMEOUT_SEC = 150
-
 # This data is same as input in datacollector-tests/resources/nifi_templates/text_data_format_template.xml
 EXPECTED_TEXT_DATA = ['Megan Rapinoe', 'Alex Morgan', 'Carli Lloyd']
 
 DATA_FORMAT_TEMPLATES_MAP = {'TEXT': 'text_data_format_template.xml'}
 
 
-@sdc_min_version('3.10.0')
 @nifi
-@pytest.mark.parametrize('data_format,expected_output',
-                         [('TEXT', EXPECTED_TEXT_DATA)])
+@sdc_min_version('3.10.0')
+@pytest.mark.parametrize('data_format,expected_output', [('TEXT', EXPECTED_TEXT_DATA)])
 def test_basic_nifi_http_server(sdc_builder, sdc_executor, nifi, data_format, expected_output):
     """Integration test for the NiFi HTTP Server.
 
      1) Create NiFi pipeline which sends data (a Flowfile) to port 8000 of SDC container
      2) Create SDC pipeline that has a NiFi HTTP Server origin listening at port 8000 and trash as destination
-     3) Start SDC pipeline with capture_snapshot.
+     3) Start SDC pipeline writing to wiretap
      4) Start NiFi pipeline
-     4) Finish snapshot in SDC pipeline and verify data received in snapshot is same as sent by NiFi pipeline.
+     4) Finish SDC pipeline and verify data received by wiretap is same as sent by NiFi pipeline.
 
      The pipeline looks like:
         nifi_http_server >> trash
@@ -48,31 +45,25 @@ def test_basic_nifi_http_server(sdc_builder, sdc_executor, nifi, data_format, ex
     pipeline_builder = sdc_builder.get_pipeline_builder()
     nifi_http_server = pipeline_builder.add_stage('NiFi HTTP Server')
     nifi_http_server.data_format = data_format
-    trash = pipeline_builder.add_stage('Trash')
-    nifi_http_server >> trash
+    wiretap = pipeline_builder.add_wiretap()
+    nifi_http_server >> wiretap.destination
     pipeline = pipeline_builder.build().configure_for_environment(nifi)
     sdc_executor.add_pipeline(pipeline)
 
     try:
-        # Start SDC pipeline along with snapshot capture.
-        snapshot_pipeline_command = sdc_executor.capture_snapshot(pipeline, start_pipeline=True, wait=False)
+        # Start SDC pipeline
+        sdc_executor.start_pipeline(pipeline)
 
         # Start NiFi pipeline.
         process_group, template = nifi.upload_template_and_start_process_group(DATA_FORMAT_TEMPLATES_MAP[data_format],
                                                                                sdc_executor.hostname,
                                                                                sdc_executor.docker_network)
 
-        logger.debug('Finish the snapshot and verify')
-        snapshot_command = snapshot_pipeline_command.wait_for_finished(timeout_sec=SNAPSHOT_TIMEOUT_SEC)
-        snapshot = snapshot_command.snapshot
-
         # Stop SDC and NiFi pipelines.
         nifi.stop_process_group(process_group)
         sdc_executor.stop_pipeline(pipeline)
 
-        output_records = snapshot[nifi_http_server.instance_name].output
-        data_for_comparison = [record.field['text'].value for record in output_records]
-        assert expected_output == data_for_comparison
+        assert expected_output == [record.field['text'].value for record in wiretap.output_records]
     finally:
         if template:
             nifi.delete_template(template, DATA_FORMAT_TEMPLATES_MAP[data_format])
