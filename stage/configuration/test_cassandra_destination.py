bd94883cb94cca0d4104fae8232cc72a66187e02
Joaquín Bo <joaquin.bo.diaz@streamsets.com>
2021-01-14 17:48:44 +0100

    SDC-16549. Test SDC-16421. Cassandra destination gives NullPointerException when destination table does not exist
    Change-Id: I90a039c3e19472585b4bf84bd2d36bcb23ed066b
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/3084
    Reviewed-by: Xavier Baques Arcusa <xavi@streamsets.com>

--- stage/configuration/test_cassandra_destination.py
+++ stage/configuration/test_cassandra_destination.py
@@ -14,7 +14,14 @@
 
 import pytest
 
+import json
+import logging
+import string
+
 from streamsets.testframework.decorators import stub
+from streamsets.testframework.markers import cassandra
+from streamsets.testframework.markers import sdc_min_version
+from streamsets.testframework.utils import get_random_string
 
 
 @stub
@@ -88,9 +95,82 @@ def test_field_to_column_mapping(sdc_builder, sdc_executor):
     pass
 
 
-@stub
-def test_fully_qualified_table_name(sdc_builder, sdc_executor):
-    pass
+@cassandra
+@sdc_min_version('3.12.0')
+@pytest.mark.parametrize('qualified_table_name', ['CORRECT', 'INCORRECT', 'NONEXISTENT'])
+def test_fully_qualified_table_name(sdc_builder, sdc_executor, cassandra, qualified_table_name):
+    """Test for Cassandra stage table name configuration. Support to test Kerberos or plain text only. Tests:
+    correct name creation and look up, incorrect name creation, and correct creation but looking for nonexistent name.
+    The pipeline looks like:
+
+        dev_raw_data_source >> cassandra_destination
+    """
+    raw_dict = [dict(contact=dict(name='Jane Smith', phone=2124050000, zip_code=27023)),
+                dict(contact=dict(name='San', phone=2120998998, zip_code=14305))]
+    raw_data = json.dumps(raw_dict)
+
+    cassandra_keyspace = get_random_string(string.ascii_letters, 10)
+    cassandra_table = 'contact'
+
+    if qualified_table_name == 'CORRECT':
+        fully_qualified_table_name = f'{cassandra_keyspace}.{cassandra_table}'
+    elif qualified_table_name == 'INCORRECT':
+        fully_qualified_table_name = f'{cassandra_keyspace}:{cassandra_table}'
+    elif qualified_table_name == 'NONEXISTENT':
+        cassandra_wrong_keyspace = get_random_string(string.ascii_letters, 10)
+        cassandra_wrong_table = 'notcontacts'
+        fully_qualified_table_name = f'{cassandra_wrong_keyspace}.{cassandra_wrong_table}'
+
+    builder = sdc_builder.get_pipeline_builder()
+    dev_raw_data_source = builder.add_stage('Dev Raw Data Source')
+    dev_raw_data_source.set_attributes(data_format='JSON', json_content='ARRAY_OBJECTS', raw_data=raw_data,
+                                       stop_after_first_batch=True)
+    cassandra_destination = builder.add_stage('Cassandra', type='destination')
+    cassandra_destination.set_attributes(field_to_column_mapping=[
+        {'field': '/contact/name', 'columnName': 'name'},
+        {'field': '/contact/zip_code', 'columnName': 'zip_code'},
+        {'field': '/contact/phone', 'columnName': 'phone'}],
+        fully_qualified_table_name=fully_qualified_table_name,
+        protocol_version='V4')
+    if cassandra.kerberos_enabled:
+        cassandra_destination.set_attributes(authentication_provider='KERBEROS')
+    else:
+        cassandra_destination.set_attributes(authentication_provider='PLAINTEXT', password=cassandra.password,
+                                             username=cassandra.username)
+
+    dev_raw_data_source >> cassandra_destination
+    pipeline = builder.build(title='Cassandra Destination pipeline').configure_for_environment(cassandra)
+    sdc_executor.add_pipeline(pipeline)
+
+    try:
+        client = cassandra.client
+        cluster = client.cluster
+        session = client.session
+
+        # create Cassandra required tables before pipeline can put data
+        session.execute(f"CREATE KEYSPACE {cassandra_keyspace} WITH replication = "
+                        f"{{'class': 'SimpleStrategy', 'replication_factor': 1}}")
+        session.execute(f'CREATE TABLE {cassandra_keyspace}.{cassandra_table} '
+                        f'(name text PRIMARY KEY, zip_code int, phone int)')
+
+        sdc_executor.start_pipeline(pipeline).wait_for_finished()
+
+        # read data from Cassandra and assert to what pipeline has ingested
+        rows = session.execute(f'SELECT * FROM {cassandra_keyspace}.{cassandra_table}').current_rows
+        assert len(rows) == len(raw_dict)
+        for index, row in enumerate(rows):
+            for key, value in raw_dict[index]['contact'].items():
+                assert getattr(row, key) == value
+    except Exception as e:
+        if qualified_table_name == 'INCORRECT':
+            assert 'CASSANDRA_02' in str(e)
+        elif qualified_table_name == 'NONEXISTENT':
+            assert 'CASSANDRA_12' in str(e)
+    finally:
+        # drop table and keyspace from Cassandra
+        session.execute(f'DROP TABLE {cassandra_keyspace}.{cassandra_table}')
+        session.execute(f'DROP KEYSPACE {cassandra_keyspace}')
+        cluster.shutdown()
 
 
 @stub
