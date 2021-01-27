7808ecf387eefa46b1d14b6d997cfac58d6d5afc
Dima Spivak <dima@streamsets.com>
2021-01-27 16:55:38 -0600

    SDC-16404. stage.standard.test_directory_origin.test_object_names_file[max_size] is failing after refactor
    Change-Id: Ice7f2c172f5dea271c24aea2111ce7c120b8d4c8
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/3116
    Tested-by: <streamsets-ci@streamsets.com>
    Reviewed-by: Jarek Jarcec Cecho

--- stage/standard/test_directory_origin.py
+++ stage/standard/test_directory_origin.py
@@ -74,40 +74,34 @@ def test_object_names_directory(sdc_builder, sdc_executor, name_category, direct
 
 @pytest.mark.parametrize('name_category,file_name', NAMES, ids=[i[0] for i in NAMES])
 def test_object_names_file(sdc_builder, sdc_executor, name_category, file_name):
-    """ Test for different kind of valid names for file name.
-        Wiretap is used to assert correctness.
-        Pipeline structure: directory_origin >> trash
-    """
+    """Different kinds of valid file names are correctly processed."""
+    files_directory = os.path.join('/tmp', get_random_string())
+    FILE_CONTENTS = 'Hello World!'
 
-    raw_str = 'Hello World!'
-
-    file_path = tempfile.gettempdir()
     if name_category == 'max_size':
         # 12 characters are deleted because of /tmp/, _tmp_ and _0 suffix
         file_name = file_name[12:]
+    file_path = os.path.join(files_directory, file_name)
 
-    _write_file_with_pipeline(sdc_executor, file_path, file_name, raw_str)
-
-    builder = sdc_builder.get_pipeline_builder()
-    directory_origin = builder.add_stage('Directory', type='origin')
-    directory_origin.set_attributes(data_format='TEXT', file_name_pattern=f'{file_name}*',
-                                    file_name_pattern_mode='GLOB',
-                                    file_post_processing='DELETE', files_directory=file_path,
-                                    process_subdirectories=True, read_order='TIMESTAMP')
-    wiretap = builder.add_wiretap()
-
-    directory_origin >> wiretap.destination
-    directory_origin_pipeline = builder.build()
-    sdc_executor.add_pipeline(directory_origin_pipeline)
+    sdc_executor.execute_shell(f'mkdir -p {files_directory}')
+    sdc_executor.write_file(file_path, FILE_CONTENTS)
 
     try:
-        sdc_executor.start_pipeline(directory_origin_pipeline)
-        sdc_executor.wait_for_pipeline_metric(directory_origin_pipeline, 'input_record_count', 1)
-        _stop_pipeline(sdc_executor, directory_origin_pipeline)
-        for record in wiretap.output_records:
-            assert raw_str == record.field['text'].value
+        pipeline_builder = sdc_builder.get_pipeline_builder()
+        directory = pipeline_builder.add_stage('Directory', type='origin')
+        directory.set_attributes(data_format='TEXT', file_name_pattern='*', files_directory=files_directory)
+        wiretap = pipeline_builder.add_wiretap()
+        pipeline_finisher = pipeline_builder.add_stage('Pipeline Finisher Executor')
+
+        directory >> [wiretap.destination, pipeline_finisher]
+        pipeline = pipeline_builder.build()
+        sdc_executor.add_pipeline(pipeline)
+
+        sdc_executor.start_pipeline(pipeline)
+        output_records = [record.field['text'] for record in wiretap.output_records]
+        assert output_records == [FILE_CONTENTS]
     finally:
-        _stop_pipeline(sdc_executor, directory_origin_pipeline)
+        sdc_executor.execute_shell(f'rm -r {files_directory}')
 
 
 @pytest.mark.parametrize('max_batch_size', [10, 100, 1000])
