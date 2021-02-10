ec56f92ebe141fd58f095e1d3fd08d795c414dd2
Jarek Jarcec Cecho <jarcec@apache.org>
2021-02-10 15:33:47 -0800

    SDC-16665: Univocity: Add STF tests for remaining user-facing options
    Change-Id: Id4fd625b9215ddfd8cf89f1e7cb249ab8d153efc
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/3132
    Reviewed-by: Alex Sánchez <alex.sanchez@streamsets.com>

--- pipeline/test_dataformat_csv.py
+++ pipeline/test_dataformat_csv.py
@@ -198,49 +198,6 @@ def test_field_separator(sdc_builder, sdc_executor, csv_parser):
 
 @sdc_min_version('3.22.0')
 @pytest.mark.parametrize('csv_parser', ALL_PARSERS)
-def test_field_separator(sdc_builder, sdc_executor, csv_parser):
-    work_dir = _prepare_work_dir(sdc_executor, FIELD_SEMICOLON_CSV)
-
-    # Create Pipeline
-    builder = sdc_builder.get_pipeline_builder()
-    origin = builder.add_stage('Directory', type='origin')
-    origin.file_name_pattern = '*.csv'
-    origin.files_directory = work_dir
-
-    origin.data_format = 'DELIMITED'
-    origin.csv_parser = csv_parser
-    origin.header_line = 'NO_HEADER'
-    # For legacy parser
-    origin.delimiter_format_type = 'CUSTOM'
-    origin.delimiter_character = ';'
-    # For univocity parser
-    origin.field_separator = ";"
-
-    wiretap = builder.add_wiretap()
-    origin >> wiretap.destination
-    pipeline = builder.build()
-    sdc_executor.add_pipeline(pipeline)
-
-    sdc_executor.start_pipeline(pipeline)
-    sdc_executor.wait_for_pipeline_metric(pipeline, 'input_record_count', 3)
-    sdc_executor.stop_pipeline(pipeline)
-
-    records = wiretap.output_records
-    assert len(records) == 3
-
-    assert records[0].field['0'] == "A"
-    assert records[0].field['1'] == "B"
-    assert records[0].field['2'] == "C"
-    assert records[1].field['0'] == "1"
-    assert records[1].field['1'] == "2"
-    assert records[1].field['2'] == "3"
-    assert records[2].field['0'] == "10"
-    assert records[2].field['1'] == "20"
-    assert records[2].field['2'] == "30"
-
-
-@sdc_min_version('3.22.0')
-@pytest.mark.parametrize('csv_parser', ALL_PARSERS)
 @pytest.mark.parametrize('separator', [';', ';;']) # Univocity limits line separator to max 2 characters (speed optimization on their side)
 def test_line_separator(sdc_builder, sdc_executor, csv_parser, separator):
     data = f"A,B,C{separator}" \
@@ -480,6 +437,55 @@ def test_quote(sdc_builder, sdc_executor, csv_parser, quote):
 
 
 @sdc_min_version('3.22.0')
+@pytest.mark.parametrize('csv_parser', ['UNIVOCITY'])
+@pytest.mark.parametrize('quote', ['a', '"', ';'])
+@pytest.mark.parametrize('escape', ['x', '+', '!'])
+def test_quote_escape(sdc_builder, sdc_executor, csv_parser, quote, escape):
+    data = f'A,B,C\n' \
+           f'{quote}1,{escape}{quote}Z{quote},2,3\n' \
+           f'100,{quote}200,{escape}{quote}Z{quote},300\n' \
+           f'1,2,{quote}3,{escape}{quote}Z{quote}\n'
+
+    work_dir = _prepare_work_dir(sdc_executor, data)
+
+    # Create Pipeline
+    builder = sdc_builder.get_pipeline_builder()
+    origin = builder.add_stage('Directory', type='origin')
+    origin.file_name_pattern = '*.csv'
+    origin.files_directory = work_dir
+
+    origin.data_format = 'DELIMITED'
+    origin.csv_parser = csv_parser
+    origin.header_line = 'WITH_HEADER'
+    origin.delimiter_format_type = 'CUSTOM'
+    origin.delimiter_character = ","
+    origin.quote_character = quote
+    origin.escape_character = escape
+
+    wiretap = builder.add_wiretap()
+    origin >> wiretap.destination
+    pipeline = builder.build()
+    sdc_executor.add_pipeline(pipeline)
+
+    sdc_executor.start_pipeline(pipeline)
+    sdc_executor.wait_for_pipeline_metric(pipeline, 'input_record_count', 3)
+    sdc_executor.stop_pipeline(pipeline)
+
+    records = wiretap.output_records
+    assert len(records) == 3
+
+    assert records[0].field['A'] == f"1,{quote}Z"
+    assert records[0].field['B'] == "2"
+    assert records[0].field['C'] == "3"
+    assert records[1].field['A'] == "100"
+    assert records[1].field['B'] == f"200,{quote}Z"
+    assert records[1].field['C'] == "300"
+    assert records[2].field['A'] == "1"
+    assert records[2].field['B'] == "2"
+    assert records[2].field['C'] == f"3,{quote}Z"
+
+
+@sdc_min_version('3.22.0')
 @pytest.mark.parametrize('csv_parser', ALL_PARSERS)
 def test_skip_lines_with_header(sdc_builder, sdc_executor, csv_parser):
     work_dir = _prepare_work_dir(sdc_executor, SIMPLE_CSV)
@@ -698,10 +704,11 @@ def test_allow_extra_columns(sdc_builder, sdc_executor, csv_parser):
 
 @sdc_min_version('3.22.0')
 @pytest.mark.parametrize('csv_parser', ALL_PARSERS)
-def test_allow_extra_columns(sdc_builder, sdc_executor, csv_parser):
+@pytest.mark.parametrize('null', ['NULL'])
+def test_parse_nulls(sdc_builder, sdc_executor, csv_parser, null):
     data = f"A,B,C\n" \
-           f"---,2,3\n" \
-           f"10,20,---\n" \
+           f"1,{null},3\n" \
+           f"10,20,{null}\n" \
 
     work_dir = _prepare_work_dir(sdc_executor, data)
 
@@ -715,7 +722,7 @@ def test_allow_extra_columns(sdc_builder, sdc_executor, csv_parser):
     origin.csv_parser = csv_parser
     origin.header_line = 'WITH_HEADER'
     origin.parse_nulls = True
-    origin.null_constant = '---'
+    origin.null_constant = null
 
     wiretap = builder.add_wiretap()
     origin >> wiretap.destination
@@ -729,12 +736,88 @@ def test_allow_extra_columns(sdc_builder, sdc_executor, csv_parser):
     records = wiretap.output_records
     assert len(records) == 2
 
-    assert records[0].field['A'] == None
-    assert records[0].field['B'] == "2"
+    assert records[0].field['A'] == "1"
+    assert records[0].field['B'] is None
     assert records[0].field['C'] == "3"
     assert records[1].field['A'] == "10"
     assert records[1].field['B'] == "20"
-    assert records[1].field['C'] == None
+    assert records[1].field['C'] is None
+
+
+@sdc_min_version('3.22.0')
+@pytest.mark.parametrize('csv_parser', ['UNIVOCITY'])
+def test_max_columns(sdc_builder, sdc_executor, csv_parser):
+    # Reaching number of columns is non-recoverable error with Univocity, we will be done with file after reaching such line
+    data = f"A,B\n" \
+           f"1,2\n" \
+           f"10,20,30\n" \
+           f"100,200\n"
+
+    work_dir = _prepare_work_dir(sdc_executor, data)
+
+    # Create Pipeline
+    builder = sdc_builder.get_pipeline_builder()
+    origin = builder.add_stage('Directory', type='origin')
+    origin.file_name_pattern = '*.csv'
+    origin.files_directory = work_dir
+
+    origin.data_format = 'DELIMITED'
+    origin.csv_parser = csv_parser
+    origin.header_line = 'WITH_HEADER'
+    origin.max_columns = 2
+
+    wiretap = builder.add_wiretap()
+    origin >> wiretap.destination
+    pipeline = builder.build()
+    sdc_executor.add_pipeline(pipeline)
+
+    sdc_executor.start_pipeline(pipeline)
+    sdc_executor.wait_for_pipeline_metric(pipeline, 'input_record_count', 1)
+    sdc_executor.stop_pipeline(pipeline)
+
+    records = wiretap.output_records
+    assert len(records) == 1
+
+    assert records[0].field['A'] == "1"
+    assert records[0].field['B'] == "2"
+
+
+@sdc_min_version('3.22.0')
+@pytest.mark.parametrize('csv_parser', ['UNIVOCITY'])
+def test_max_characters_per_column(sdc_builder, sdc_executor, csv_parser):
+    # Reaching max number of characters is non-recoverable error with Univocity, we will be done with file after reaching such line
+    data = f"A,B\n" \
+           f"1,2\n" \
+           f"10,2\n" \
+           f"3,4\n"
+
+    work_dir = _prepare_work_dir(sdc_executor, data)
+
+    # Create Pipeline
+    builder = sdc_builder.get_pipeline_builder()
+    origin = builder.add_stage('Directory', type='origin')
+    origin.file_name_pattern = '*.csv'
+    origin.files_directory = work_dir
+
+    origin.data_format = 'DELIMITED'
+    origin.csv_parser = csv_parser
+    origin.header_line = 'WITH_HEADER'
+    origin.max_characters_per_column = 1
+
+    wiretap = builder.add_wiretap()
+    origin >> wiretap.destination
+    pipeline = builder.build()
+    sdc_executor.add_pipeline(pipeline)
+
+    sdc_executor.start_pipeline(pipeline)
+    sdc_executor.wait_for_pipeline_metric(pipeline, 'input_record_count', 1)
+    sdc_executor.stop_pipeline(pipeline)
+
+    records = wiretap.output_records
+    assert len(records) == 1
+
+    assert records[0].field['A'] == "1"
+    assert records[0].field['B'] == "2"
 
 
 def _prepare_work_dir(sdc_executor, data):
