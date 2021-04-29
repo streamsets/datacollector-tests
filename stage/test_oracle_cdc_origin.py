793bad4551c6d148c6b8ae9a606bc30be75514dd
Dimas Cabré i Chacón <dimas.cabre.chacon@streamsets.com>
2021-04-30 01:53:15 +0200

    SDC-16961. Test for SDC-16960 - Add sequence of records in Oracle CDC origin
    Change-Id: Ibda937ed5e201e8f5eaedc336b93b71a6f291673
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/3254
    Reviewed-by: Alex Sánchez <alex.sanchez@streamsets.com>

--- stage/test_oracle_cdc_origin.py
+++ stage/test_oracle_cdc_origin.py
@@ -330,7 +330,10 @@ def test_oracle_cdc_client_basic(sdc_builder, sdc_executor, database, buffer_loc
 
 @pytest.mark.parametrize('buffer_locally', [True, False])
 @database('oracle')
-def test_oracle_cdc_client_bulk(sdc_builder, sdc_executor, database, buffer_locally):
+def test_oracle_cdc_client_bulk(sdc_builder,
+                                sdc_executor,
+                                database,
+                                buffer_locally):
     """Test that reads inserts/updates/deletes to an Oracle table with bulk sentences,
     and verifies that no record is lost .
     Runs oracle_cdc_client >> wiretap
@@ -342,6 +345,7 @@ def test_oracle_cdc_client_bulk(sdc_builder, sdc_executor, database, buffer_loca
     pipeline = None
 
     try:
+
         database_connection = database.engine.connect()
 
         source_table_name = get_random_string(string.ascii_uppercase, 16)
@@ -427,7 +431,9 @@ def test_oracle_cdc_client_bulk(sdc_builder, sdc_executor, database, buffer_loca
 
         for record in wiretap.output_records:
             logger.info(f'{record.header.values["oracle.cdc.operation"]} - {record.header.values["sdc.operation.type"]} - {record}')
+
     finally:
+
         if pipeline is not None:
             sdc_executor.stop_pipeline(pipeline=pipeline,
                                        force=True)
@@ -441,9 +447,12 @@ def test_oracle_cdc_client_bulk(sdc_builder, sdc_executor, database, buffer_loca
 @sdc_min_version('4.0.0')
 @pytest.mark.parametrize('buffer_locally', [True, False])
 @database('oracle')
-def test_oracle_cdc_headers(sdc_builder, sdc_executor, database, buffer_locally):
+def test_oracle_cdc_client_headers(sdc_builder,
+                                   sdc_executor,
+                                   database,
+                                   buffer_locally):
     """
-    Test to check all headers arepresent in the output records.
+    Test to check all headers are present in the output records.
     """
 
     source_table = None
@@ -541,6 +550,118 @@ def test_oracle_cdc_headers(sdc_builder, sdc_executor, database, buffer_locally)
             assert {record.header.values["oracle.cdc.undoValue"]} is not None
             assert {record.header.values["oracle.cdc.precisionTimestamp"]} is not None
             assert {record.header.values["oracle.cdc.query"]} is not None
+            assert {record.header.values["oracle.cdc.sequence.oracle"]} is not None
+            if buffer_locally:
+                assert {record.header.values["oracle.cdc.sequence.internal"]} is not None
+
+    finally:
+
+        if pipeline is not None:
+            sdc_executor.stop_pipeline(pipeline=pipeline,
+                                       force=True)
+        if source_table is not None:
+            source_table.drop(database.engine)
+
+        if target_table is not None:
+            target_table.drop(database.engine)
+
+@sdc_min_version('4.0.0')
+@database('oracle')
+def test_oracle_cdc_client_sequence(sdc_builder,
+                                    sdc_executor,
+                                    database):
+    """
+    Test that checks that the internal sequence is generated properly.
+    """
+
+    source_table = None
+    target_table = None
+
+    pipeline = None
+
+    try:
+
+        database_connection = database.engine.connect()
+
+        source_table_name = get_random_string(string.ascii_uppercase, 16)
+        logger.info('Creating source table %s in %s database ...', source_table_name, database.type)
+        source_table = sqlalchemy.Table(source_table_name, sqlalchemy.MetaData(),
+                                        sqlalchemy.Column('ID', sqlalchemy.Integer, primary_key=True),
+                                        sqlalchemy.Column('NAME', sqlalchemy.String(32)),
+                                        sqlalchemy.Column('SURNAME', sqlalchemy.String(64)),
+                                        sqlalchemy.Column('COUNTRY', sqlalchemy.String(2)),
+                                        sqlalchemy.Column('CITY', sqlalchemy.String(3)))
+        source_table.create(database.engine)
+        target_table_name = get_random_string(string.ascii_uppercase, 16)
+        logger.info('Creating target table %s in %s database ...', target_table_name, database.type)
+        target_table = sqlalchemy.Table(target_table_name, sqlalchemy.MetaData(),
+                                        sqlalchemy.Column('ID', sqlalchemy.Integer, primary_key=True),
+                                        sqlalchemy.Column('NAME', sqlalchemy.String(32)),
+                                        sqlalchemy.Column('SURNAME', sqlalchemy.String(64)),
+                                        sqlalchemy.Column('COUNTRY', sqlalchemy.String(2)),
+                                        sqlalchemy.Column('CITY', sqlalchemy.String(3)))
+        target_table.create(database.engine)
+
+        database_last_scn = _get_last_scn(database_connection)
+        number_of_rows = 100
+
+        database_transaction = database_connection.begin()
+        for id in range(0, number_of_rows):
+            table_id = id
+            table_name = "'" + str(uuid.uuid4())[:32] + "'"
+            table_surname = "'" + str(uuid.uuid4())[:64] + "'"
+            table_country = "'" + str(uuid.uuid4())[:2] + "'"
+            table_city = "'" + str(uuid.uuid4())[:3] + "'"
+            sentence = f'insert into {source_table} values ({table_id}, {table_name}, {table_surname}, {table_country}, {table_city})'
+            sql = text(sentence)
+            database_connection.execute(sql)
+        database_transaction.commit()
+
+        database_transaction = database_connection.begin()
+        sentence = f'insert into {target_table_name} select * from {source_table_name}'
+        sql = text(sentence)
+        database_connection.execute(sql)
+        database_transaction.commit()
+
+        database_transaction = database_connection.begin()
+        sentence = f'update {target_table_name} set CITY = COUNTRY'
+        sql = text(sentence)
+        database_connection.execute(sql)
+        database_transaction.commit()
+
+        database_transaction = database_connection.begin()
+        sentence = f'delete from {target_table_name}'
+        sql = text(sentence)
+        database_connection.execute(sql)
+        database_transaction.commit()
+
+        pipeline_builder = sdc_builder.get_pipeline_builder()
+        oracle_cdc_client = pipeline_builder.add_stage('Oracle CDC Client')
+        oracle_cdc_client.set_attributes(dictionary_source='DICT_FROM_ONLINE_CATALOG',
+                                         tables=[{'schema': database.username.upper(), 'table': target_table_name, 'excludePattern': ''}],
+                                         buffer_changes_locally=True,
+                                         logminer_session_window='${10 * MINUTES}',
+                                         maximum_transaction_length='${2 * MINUTES}',
+                                         db_time_zone='UTC',
+                                         max_batch_size_in_records=1,
+                                         initial_change='SCN',
+                                         start_scn=database_last_scn)
+        wiretap = pipeline_builder.add_wiretap()
+        oracle_cdc_client >> wiretap.destination
+        pipeline = pipeline_builder.build('Oracle CDC Client Pipeline').configure_for_environment(database)
+        sdc_executor.add_pipeline(pipeline)
+        sdc_executor.start_pipeline(pipeline).wait_for_pipeline_output_records_count(3 * number_of_rows)
+
+        sequence = 0
+        operation = ''
+        for record in wiretap.output_records:
+            record_operation = record.header.values["oracle.cdc.operation"]
+            if record_operation != operation:
+                operation = record_operation
+                sequence = 0
+            record_sequence = int(record.header.values["oracle.cdc.sequence.internal"])
+            assert record_sequence == sequence
+            sequence = sequence + 1
 
     finally:
 
