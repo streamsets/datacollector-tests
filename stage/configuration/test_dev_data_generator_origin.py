f14c046e712a889f7e8c44a44ecfa6143d5da33e
Juan Francisco Martínez Vera <fran@streamsets.com>
2021-06-22 00:47:50 +0200

    SDC-17062. Test for SDC-16275 - Dev data sources should support Batch size limit.
    Change-Id: I9f0d8b00f467f2ea4d4cea2065526d1e331117af
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/3290
    Reviewed-by: Konstantin Golub <konstantin@streamsets.com>

--- stage/configuration/test_dev_data_generator_origin.py
+++ stage/configuration/test_dev_data_generator_origin.py
@@ -12,8 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pytest
+import math
 
 from streamsets.testframework.decorators import stub
+from streamsets.testframework.markers import sdc_min_version
 
 
 @stub
@@ -59,3 +61,35 @@ def test_on_record_error(sdc_builder, sdc_executor, stage_attributes):
 def test_root_field_type(sdc_builder, sdc_executor, stage_attributes):
     pass
 
+
+@sdc_min_version('4.1.0')
+@pytest.mark.parametrize("stage_attributes", [
+    ({"batch_size": 10, "records_to_be_generated": 12, "number_of_threads": 1}),
+    ({"batch_size": 5, "records_to_be_generated": 15, "number_of_threads": 1}),
+    ({"batch_size": 10, "records_to_be_generated": 5, "number_of_threads": 1}),
+    ({"batch_size": 10, "records_to_be_generated": 147, "number_of_threads": 4}),
+    ({"batch_size": 10, "records_to_be_generated": 330, "number_of_threads": 4}),
+    ({"batch_size": 1000, "records_to_be_generated": 528, "number_of_threads": 4})
+])
+def test_number_of_records(sdc_builder, sdc_executor, stage_attributes):
+    """Assert the Dev data generator is generating the proper number of records arranged properly in batches
+    depending on the `records_to_be_generated` parameter """
+
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Data Generator').set_attributes(**stage_attributes)
+    trash = pipeline_builder.add_stage('Trash')
+
+    dev_raw_data_source >> trash
+
+    pipeline = pipeline_builder.build()
+    sdc_executor.add_pipeline(pipeline)
+    sdc_executor.start_pipeline(pipeline)
+
+    # This pipeline is gonna finish as soon as all the required records are generated
+    sdc_executor.wait_for_pipeline_status(pipeline, 'FINISHED')
+    history = sdc_executor.get_pipeline_history(pipeline)
+
+    expected_batch_count = math.ceil(stage_attributes['records_to_be_generated'] / stage_attributes['batch_size'])
+    expected_number_of_records = stage_attributes['records_to_be_generated']
+    assert history.latest.metrics.counter('pipeline.batchCount.counter').count == expected_batch_count
+    assert history.latest.metrics.counter('pipeline.batchOutputRecords.counter').count == expected_number_of_records
