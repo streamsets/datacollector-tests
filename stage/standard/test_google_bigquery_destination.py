f3dadab1a7e9347fc36a423985bfe90b549dbad7
Alex <alex.sanchez@streamsets.com>
2021-07-16 15:06:34 +0200

    COLLECTOR-163. Add standard tests for types in GBQ destination
    Change-Id: I9b4935efc323630735987df8c7c5a4e2ce736211
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/3305
    Reviewed-by: Dimas Cabré i Chacón <dimas.cabre.chacon@streamsets.com>

--- stage/standard/test_google_bigquery_destination.py
+++ stage/standard/test_google_bigquery_destination.py
@@ -13,15 +13,15 @@
 # limitations under the License.
 
 import base64
+import datetime
 import logging
 import string
 from decimal import Decimal
-import datetime
-from isodate import UTC
 
 import pytest
 from google.cloud.bigquery import Dataset, SchemaField, Table
-from streamsets.testframework.markers import gcp, sdc_min_version
+from isodate import UTC
+from streamsets.testframework.markers import gcp
 from streamsets.testframework.utils import get_random_string
 
 logger = logging.getLogger(__name__)
@@ -51,29 +51,70 @@ CSV_DATA_TO_INSERT = ['full_name,age'] + [','.join(str(element) for element in r
 # https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types
 # https://docs.streamsets.com/portal/#datacollector/latest/help/datacollector/UserGuide/Destinations/BigQuery.html
 # For every data type supported
-@pytest.mark.parametrize('data_type, data, expected_data', [
-    ('INTEGER', 34, 34),
-    ('STRING', 'gcp standard test 123', 'gcp standard test 123'),
-    ('FLOAT', 2424.2424, 2424.2424),
-    ('NUMERIC', -999999999999999.999999995, Decimal(-999999999999999.999999995)),
-    ('BOOLEAN', True, True),
-    ('DATE', '2019-02-05', datetime.date(2019, 2, 5)),
-    ('DATETIME', '2019-02-05 23:59:59.999', datetime.datetime(2019, 2, 5, 23, 59, 59, 999000)),
-    ('TIMESTAMP', '2007-05-28 07:52:31.123 UTC', datetime.datetime(2007, 5, 28, 7, 52, 31, 123000, tzinfo=UTC)),
-    ('TIME', '08:39:01.123',  datetime.time(8, 39, 1, 123000)),
-    ('BYTES', 'ZGF0YUFzQnl0ZXM=', b'dataAsBytes')
+@pytest.mark.parametrize('origin_data_type, gcp_data_type, origin_data, gcp_expected_data', [
+    # (DECIMAL is an alias for NUMERIC in GCP - BIGDECIMAL for BIGNUMERIC)
+    # Boolean
+    ('BOOLEAN', 'BOOLEAN', True, True),
+    # Byte Array
+    ('BYTE_ARRAY', 'BYTES', 'dataAsBytes', b'dataAsBytes'),
+    ('BYTE_ARRAY', 'STRING', 'dataAsBytes', 'ZGF0YUFzQnl0ZXM='),
+    # Date
+    ('DATE', 'DATE', '2020-01-01 10:00:00', datetime.date(2020, 1, 1)),
+    ('DATE', 'DATETIME', '2020-01-01 10:00:00', datetime.datetime(2020, 1, 1)),
+    ('DATE', 'STRING', '2020-01-01 10:00:00', '2020-01-01'),
+    # Datetime
+    ('DATETIME', 'DATETIME', '2019-02-05 23:59:59', datetime.datetime(2019, 2, 5, 23, 59, 59)),
+    ('DATETIME', 'TIMESTAMP', '2007-05-28 07:52:31 UTC', datetime.datetime(2007, 5, 28, 7, 52, 31, tzinfo=UTC)),
+    ('DATETIME', 'STRING', '2019-02-05 23:59:59', '2019-02-05T23:59:59.000000'),
+    # Time
+    ('TIME', 'TIME', '2020-01-01 10:00:00',  datetime.time(10, 0)),
+    # Double
+    ('DOUBLE', 'FLOAT', 2424.2424, 2424.2424),
+    ('DOUBLE', 'STRING', 2424.2424, '2424.2424'),
+    ('DOUBLE', 'DECIMAL', -123456789.12345, Decimal('-123456789.12345')),
+    ('DOUBLE', 'BIGDECIMAL', -123456789.12345, '-123456789.12345'),
+    # Float
+    ('FLOAT', 'FLOAT', 2424.2424, 2424.2424),
+    ('FLOAT', 'STRING', 2424.2424, '2424.2424'),
+    ('FLOAT', 'DECIMAL', 2424.2424, Decimal('2424.2424')),
+    ('FLOAT', 'BIGDECIMAL', 2424.2424, '2424.2424'),
+    # Long
+    ('LONG', 'INTEGER', 2424, 2424),
+    ('LONG', 'STRING', 2424, '2424'),
+    ('LONG', 'DECIMAL', 2424, 2424),
+    ('LONG', 'BIGDECIMAL', 2424, '2424'),
+    # Integer
+    ('INTEGER', 'INTEGER', 2424, 2424),
+    ('INTEGER', 'STRING', 2424, '2424'),
+    ('INTEGER', 'DECIMAL', 2424, Decimal('2424')),
+    ('INTEGER', 'BIGDECIMAL', 2424, '2424'),
+    # Decimal
+    ('DECIMAL', 'INTEGER', 2424, 2424),
+    ('DECIMAL', 'DECIMAL', -123456789.12345, Decimal('-123456789.12345')),
+    ('DECIMAL', 'BIGDECIMAL', -123456789.12345, '-123456789.12345'),
+    ('DECIMAL', 'STRING', -123456789.12345, '-123456789.12345'),
+    # String
+    ('STRING', 'STRING', 'gcp standard test 123', 'gcp standard test 123'),
+    ('STRING', 'DATE', '2020-01-01', datetime.date(2020, 1, 1)),
+    ('STRING', 'DATETIME', '2003-04-12 04:05:06', datetime.datetime(2003, 4, 12, 4, 5, 6)),
+    ('STRING', 'FLOAT', '2424.2424', 2424.2424),
+    ('STRING', 'INTEGER', '2424', 2424),
+    ('STRING', 'DECIMAL', '-123456789.12345', Decimal('-123456789.12345')),
+    ('STRING', 'BIGDECIMAL', '-123456789.12345', '-123456789.12345'),
 ])
-def test_data_type(sdc_builder, sdc_executor, gcp, data_type, data, expected_data):
+def test_data_type(sdc_builder, sdc_executor, gcp, origin_data_type, gcp_data_type, origin_data, gcp_expected_data):
     """
     Create data using Google BigQuery destination with different data types
     and then check if it has been correctly created using BigQuery client.
 
     The pipeline looks like:
-        dev_raw_data_source >> google_bigquery
+    dev_raw_data_source >> field_type_converter >> google_bigquery
     """
-    data_to_insert = [(1, data)]
+    data_to_insert = [(1, origin_data)]
+    data_to_expect = [(1, gcp_expected_data)]
+
     rows_to_insert = ['id,data'] + [','.join(str(element) for element in row) for row in data_to_insert]
-    data_to_expect = [(1, expected_data)]
+
     pipeline_builder = sdc_builder.get_pipeline_builder()
 
     dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
@@ -82,6 +123,17 @@ def test_data_type(sdc_builder, sdc_executor, gcp, data_type, data, expected_dat
                                        stop_after_first_batch=True,
                                        raw_data='\n'.join(rows_to_insert))
 
+    field_type_converter = pipeline_builder.add_stage('Field Type Converter')
+    field_type_converter.conversion_method = 'BY_FIELD'
+    field_type_converter.field_type_converter_configs = [{
+        'fields': ['/data'],
+        'targetType': origin_data_type,
+        'dataLocale': 'en,US',
+        'dateFormat': 'YYYY_MM_DD_HH_MM_SS',
+        'zonedDateTimeFormat': 'ISO_OFFSET_DATE_TIME',
+        'scale': 38
+    }]
+
     dataset_name = get_random_string(string.ascii_letters, 5)
     table_name = get_random_string(string.ascii_letters, 5)
 
@@ -89,14 +141,15 @@ def test_data_type(sdc_builder, sdc_executor, gcp, data_type, data, expected_dat
     google_bigquery.set_attributes(dataset=dataset_name,
                                    table_name=table_name)
 
-    dev_raw_data_source >> google_bigquery
+    dev_raw_data_source >> field_type_converter >> google_bigquery
 
     pipeline = pipeline_builder.build().configure_for_environment(gcp)
     sdc_executor.add_pipeline(pipeline)
 
     bigquery_client = gcp.bigquery_client
+    # We create data type for gcp, and insert it against that type to check if it works
     schema = [SchemaField('id', 'INTEGER', mode='NULLABLE'),
-              SchemaField('data', data_type, mode='NULLABLE')]
+              SchemaField('data', gcp_data_type, mode='NULLABLE')]
 
     try:
         dataset_ref = Dataset(bigquery_client.dataset(dataset_name))
@@ -326,12 +379,12 @@ def test_multiple_batch(sdc_builder, sdc_executor, gcp, number_batches):
 
     pipeline_builder = sdc_builder.get_pipeline_builder()
 
-    dev_raw_data_source = pipeline_builder.add_stage('Dev Data Generator')
-    dev_raw_data_source.set_attributes(batch_size=batch_size,
-                                       fields_to_generate=[
-                                           {"type": "INTEGER",  "field": "age"},
-                                           {"type": "STRING",  "field": "full_name"}
-                                       ])
+    dev_data_generator = pipeline_builder.add_stage('Dev Data Generator')
+    dev_data_generator.set_attributes(batch_size=batch_size,
+                                      fields_to_generate=[
+                                          {"type": "INTEGER", "field": "age"},
+                                          {"type": "STRING", "field": "full_name"}
+                                      ])
 
     dataset_name = get_random_string(string.ascii_letters, 5)
     table_name = get_random_string(string.ascii_letters, 5)
@@ -339,7 +392,7 @@ def test_multiple_batch(sdc_builder, sdc_executor, gcp, number_batches):
     google_bigquery.set_attributes(dataset=dataset_name,
                                    table_name=table_name)
 
-    dev_raw_data_source >> google_bigquery
+    dev_data_generator >> google_bigquery
 
     pipeline = pipeline_builder.build().configure_for_environment(gcp)
     sdc_executor.add_pipeline(pipeline)
