b002fe442cffff75acae320c9d909b708002ef3e
Alex Sánchez <alex.sanchez@streamsets.com>
2021-12-03 14:02:43 +0100

    COLLECTOR-545. Tests for COLLECTOR-544: JMS allow configure prefix for all headers
    Change-Id: I650186fb49cf8511f3af0ac1c3da153283b4f0fc
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/3379
    Reviewed-by: Joaquín

--- pipeline/test_jms_stages.py
+++ pipeline/test_jms_stages.py
@@ -35,10 +35,13 @@ JNDI_CONNECTION_FACTORY = 'ConnectionFactory'
 
 @jms('activemq')
 @sdc_min_version("4.4.0")
-@pytest.mark.parametrize('include_headers', [True, False])
-def test_jms_headers_origin_to_destination(sdc_builder, sdc_executor, jms, include_headers):
+@pytest.mark.parametrize('include_headers, prefix_all_headers', [[True, True], [True, False], [False, False]])
+def test_jms_headers_origin_to_destination(sdc_builder, sdc_executor, jms, include_headers, prefix_all_headers):
     """Validating that JMS standard headers are serialized into Record headers."""
     message_data = 'This will be a message sent to JMS'
+    custom_header = 'This is my custom header'
+    custom_header_from_origin = 'This is my custom header from origin'
+    headers = {'custom_hedader_from_origin': custom_header_from_origin}
     first_queue = get_random_string(ascii_letters, 5)
     second_queue = get_random_string(ascii_letters, 5)
     builder = sdc_builder.get_pipeline_builder()
@@ -51,7 +54,8 @@ def test_jms_headers_origin_to_destination(sdc_builder, sdc_executor, jms, inclu
                                 jms_initial_context_factory=JMS_INITIAL_CONTEXT_FACTORY,
                                 jndi_connection_factory=JNDI_CONNECTION_FACTORY,
                                 password=DEFAULT_PASSWORD,
-                                username=DEFAULT_USERNAME)
+                                username=DEFAULT_USERNAME,
+                                prefix_all_headers=prefix_all_headers)
 
     jms_producer = builder.add_stage('JMS Producer', type='destination')
     jms_producer.set_attributes(data_format='TEXT',
@@ -63,7 +67,15 @@ def test_jms_headers_origin_to_destination(sdc_builder, sdc_executor, jms, inclu
                                 username=DEFAULT_USERNAME,
                                 include_headers=include_headers)
 
-    jms_consumer >> jms_producer
+    expression_evaluator = builder.add_stage('Expression Evaluator')
+    expression_evaluator.set_attributes(header_attribute_expressions=[
+        {
+            "attributeToSet": "jms.header.customHeader",
+            "headerAttributeExpression": custom_header
+        }
+    ])
+
+    jms_consumer >> expression_evaluator >> jms_producer
 
     pipeline = builder.build().configure_for_environment(jms)
     sdc_executor.add_pipeline(pipeline)
@@ -76,7 +88,7 @@ def test_jms_headers_origin_to_destination(sdc_builder, sdc_executor, jms, inclu
         connection.set_listener('', listener)
         connection.start()
         connection.connect(login=DEFAULT_USERNAME, passcode=DEFAULT_PASSWORD)
-        connection.send(first_queue, message_data, persistent='false')
+        connection.send(first_queue, message_data, headers=headers, persistent='false')
         connection.subscribe(destination=f'/queue/{second_queue}', id=second_queue)
 
         sdc_executor.start_pipeline(pipeline)
@@ -90,7 +102,7 @@ def test_jms_headers_origin_to_destination(sdc_builder, sdc_executor, jms, inclu
 
         message = str(message)
         if include_headers:
-            # JMS Headers
+            assert custom_header in message
             assert 'jms.header.messageId' in message
             assert 'jms.header.timestamp' in message
             assert 'jms.header.correlationId' in message
@@ -99,8 +111,13 @@ def test_jms_headers_origin_to_destination(sdc_builder, sdc_executor, jms, inclu
             assert 'jms.header.type' in message
             assert 'jms.header.expiration' in message
             assert 'jms.header.priority' in message
+
+            if prefix_all_headers:
+                assert custom_header_from_origin in message
+            else:
+                assert custom_header_from_origin not in message
         else:
-            # JMS Headers
+            assert custom_header not in message
             assert 'jms.header.messageId' not in message
             assert 'jms.header.timestamp' not in message
             assert 'jms.header.correlationId' not in message
