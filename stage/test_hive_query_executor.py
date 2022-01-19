479d9c8e5f44d6855bed538d9492487aa737268e
marina.torras <marina.torras@streamsets.com>
2022-01-19 11:32:41 -0800

    COLLECTOR-494. Test TimeEL on Hive Query Executor.
    I added a new test to stage/test_hive_query_executor.py called
    test_hive_query_executor_timeEL. The test includes the configuration
    time:now() in the sql_queries variable to test that a
    pipeline can be created correctly using the Hive Query Executor with
    TimeEL.
    
    The upgrade tests for TimeEL are included in commit
    I3fe541da15660dd5f03951e3cde33bb7b2d38269.
    
    Change-Id: I36339ef5f3ca8d95bf3a98c9e7dec536a24e9c04

--- stage/test_hive_query_executor.py
+++ stage/test_hive_query_executor.py
@@ -60,3 +60,45 @@ def test_hive_query_executor(sdc_builder, sdc_executor, cluster):
     finally:
         # drop the Hive table
         hive_cursor.execute(f'DROP TABLE `{hive_table_name}`')
+
+
+@cluster('cdh')
+def test_hive_query_executor(sdc_builder, sdc_executor, cluster):
+    """
+    Test Hive query executor stage. The pipeline would look like:
+    dev_raw_data_source >> hive_query >= wiretap
+    """
+    hive_table_name = get_random_string(string.ascii_letters, 10)
+    hive_cursor = cluster.hive.client.cursor()
+    sql_queries = ["CREATE TABLE ${time:now()}"]
+
+    builder = sdc_builder.get_pipeline_builder()
+
+    dev_raw_data_source = builder.add_stage('Dev Raw Data Source').set_attributes(data_format='TEXT',
+                                                                                  raw_data=hive_table_name,
+                                                                                  stop_after_first_batch=True)
+    wiretap = builder.add_wiretap()
+    hive_query = builder.add_stage('Hive Query', type='executor').set_attributes(sql_queries=sql_queries)
+
+    dev_raw_data_source >> hive_query >= wiretap.destination
+
+    pipeline = builder.build(title='Hive query executor pipeline').configure_for_environment(cluster)
+    sdc_executor.add_pipeline(pipeline)
+
+    try:
+        # assert successful query execution of the pipeline
+        sdc_executor.start_pipeline(pipeline).wait_for_finished()
+        assert (wiretap.output_records[0].header['values']['sdc.event.type'] == 'successful-query')
+
+        # assert Hive table creation
+        assert hive_cursor.table_exists(hive_table_name)
+
+        wiretap.reset()
+
+        # Re-running the same query to create Hive table should fail the query. So assert the failure.
+        sdc_executor.start_pipeline(pipeline).wait_for_finished()
+        assert wiretap.output_records[0].header['values']['sdc.event.type'] == 'failed-query'
+    finally:
+        # drop the Hive table
+        hive_cursor.execute(f'DROP TABLE `{hive_table_name}`')
+
