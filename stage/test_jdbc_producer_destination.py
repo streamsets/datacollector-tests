a1adbdc7b1c081e57db45a55276cc9b20c07188c
Joaquín Bo <joaquin.bo.diaz@streamsets.com>
2022-08-26 16:27:05 +0200

    COLLECTOR-1406. Test for COLLECTOR-1405. Primary Key updates in JDBC Stages using headers
    Change-Id: I72218e8b457f22c07b1c031f8e3ecfeb1fe340d8
    Reviewed-on: https://review.streamsets.dev/c/datacollector-tests/+/3595
    Reviewed-by: J. Francisco Martínez <fran@streamsets.com>

--- stage/test_jdbc_producer_destination.py
+++ stage/test_jdbc_producer_destination.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import copy
 import json
 import logging
 import math
@@ -25,6 +26,10 @@ from streamsets.testframework.utils import get_random_string, Version
 
 logger = logging.getLogger(__name__)
 
+# from CommonDatabaseHeader.java
+PRIMARY_KEY_COLUMN_OLD_VALUE = 'jdbc.primaryKey.before'
+PRIMARY_KEY_COLUMN_NEW_VALUE = 'jdbc.primaryKey.after'
+
 ROWS_IN_DATABASE = [
     {'id': 1, 'name': 'Dima'},
     {'id': 2, 'name': 'Jarcec'},
@@ -36,6 +41,41 @@ ROWS_TO_UPDATE = [
 ]
 LOOKUP_RAW_DATA = ['id'] + [str(row['id']) for row in ROWS_IN_DATABASE]
 RAW_DATA = ['name'] + [row['name'] for row in ROWS_IN_DATABASE]
+
+CDC_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY = [
+    {'TYPE': 'Hobbit', 'ID': 1, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 0'},
+    {'TYPE': 'Fallohide', 'ID': 1, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 1'},
+    {'TYPE': 'Fallohide', 'ID': 2, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 2'},
+    {'TYPE': 'Hobbit - Fallohide', 'ID': 3, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 3'},
+    {'TYPE': 'Hobbit, Fallohide', 'ID': 3, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 4'},
+    {'TYPE': 'Hobbit, Fallohide', 'ID': 4, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 5'},
+    {'TYPE': 'Hobbit, Fallohide', 'ID': 4, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 6'}
+]
+CDC_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY_EXPECTED_DATA = [
+    ('Hobbit, Fallohide', 4, 'Bilbo', 'Baggins', 'Bag End 6')
+]
+CDC_PK_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY_HEADER = [
+    {'sdc.operation.type': 1, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 1, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 1,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Hobbit', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Hobbit'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 1, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 1,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Hobbit', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Fallohide'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 1, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 2,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Fallohide', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Fallohide'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 2, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 3,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Fallohide', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Hobbit - Fallohide'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 3, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 3,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Hobbit - Fallohide', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Hobbit, Fallohide'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 3, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 4,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Hobbit, Fallohide', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Hobbit, Fallohide'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 4, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 4,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Hobbit, Fallohide', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Hobbit, Fallohide'},
+]
 
 DEFAULT_DB2_SCHEMA = 'DB2INST1'
 
@@ -1335,3 +1375,96 @@ def test_jdbc_producer_multirow_with_duplicates_error(sdc_builder, sdc_executor,
         logger.info('Dropping table %s in %s database...', table_name, database.type)
         table.drop(database.engine)
 
+
+@sdc_min_version('5.2.0')
+@database
+@pytest.mark.parametrize('multi_row', [False, True])
+def test_jdbc_producer_primary_key_header_update(sdc_builder, sdc_executor, database, multi_row):
+    """
+    Test to make sure Primary Key updates are handled correctly. We will insert one record, and then
+    perform multiple updates to its columns (including Primary Key columns) and check we end up
+    having just one row in the table, with the expected values.
+    The pipeline looks like:
+
+        dev_raw_data_source >> expression_evaluator >> field_remover >> jdbc_producer
+
+    """
+    if isinstance(database, OracleDatabase) and multi_row:
+        pytest.skip('multi_row is not supported on oracle databases')
+
+    table_name = get_random_string(string.ascii_lowercase, 15)
+
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    # Build Dev Raw Data Source
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+    rows = copy.deepcopy(CDC_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY)
+    for row, header in zip(rows, CDC_PK_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY_HEADER):
+        row['HEADER'] = header
+    raw_data = '\n'.join((json.dumps(row) for row in rows))
+    dev_raw_data_source.set_attributes(data_format='JSON',
+                                       raw_data=raw_data,
+                                       stop_after_first_batch=True)
+
+    # Build Expression Evaluator
+    expression_evaluator = pipeline_builder.add_stage('Expression Evaluator')
+    # We assume CDC headers are generated properly
+    expression_evaluator.set_attributes(header_attribute_expressions=[
+        {'attributeToSet': 'sdc.operation.type',
+         'headerAttributeExpression': "${record:value('/HEADER/sdc.operation.type')}"},
+        {'attributeToSet': f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID',
+         'headerAttributeExpression': "${record:value('/HEADER/" + PRIMARY_KEY_COLUMN_OLD_VALUE + ".ID')}"},
+        {'attributeToSet': f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID',
+         'headerAttributeExpression': "${record:value('/HEADER/" + PRIMARY_KEY_COLUMN_NEW_VALUE + ".ID')}"},
+        {'attributeToSet': f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE',
+         'headerAttributeExpression': "${record:value('/HEADER/" + PRIMARY_KEY_COLUMN_OLD_VALUE + ".TYPE')}"},
+        {'attributeToSet': f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE',
+         'headerAttributeExpression': "${record:value('/HEADER/" + PRIMARY_KEY_COLUMN_NEW_VALUE + ".TYPE')}"}
+    ])
+
+    # Build Field Remover
+    field_remover = pipeline_builder.add_stage('Field Remover')
+    field_remover.fields = ['/HEADER']
+
+    # Jdbc Producer
+    jdbc_producer = pipeline_builder.add_stage('JDBC Producer')
+    jdbc_producer.set_attributes(default_operation='INSERT',
+                                 table_name=table_name,
+                                 field_to_column_mapping=[],
+                                 use_multi_row_operation=multi_row,
+                                 stage_on_record_error='TO_ERROR')
+    if isinstance(database, PostgreSqlDatabase):
+        jdbc_producer.set_attributes(enclose_object_names=True)
+
+    dev_raw_data_source >> expression_evaluator >> field_remover >> jdbc_producer
+
+    pipeline = pipeline_builder.build().configure_for_environment(database)
+    sdc_executor.add_pipeline(pipeline)
+
+    table = sqlalchemy.Table(table_name, sqlalchemy.MetaData(),
+                             sqlalchemy.Column('TYPE', sqlalchemy.String(64), primary_key=True),
+                             sqlalchemy.Column('ID', sqlalchemy.Integer, primary_key=True),
+                             sqlalchemy.Column('NAME', sqlalchemy.String(64)),
+                             sqlalchemy.Column('SURNAME', sqlalchemy.String(64)),
+                             sqlalchemy.Column('ADDRESS', sqlalchemy.String(64)))
+    try:
+        logger.info('Creating source table %s in %s database ...', table_name, database.type)
+        table.create(database.engine)
+        sdc_executor.start_pipeline(pipeline).wait_for_finished()
+
+        result = database.engine.execute(table.select())
+        data_from_database = result.fetchall()
+        result.close()
+
+        # We assert that the pipeline processed 1 insert and 6 updates
+        history = sdc_executor.get_pipeline_history(pipeline)
+        assert history.latest.metrics.counter('pipeline.batchInputRecords.counter').count == 7
+        assert history.latest.metrics.counter('pipeline.batchErrorRecords.counter').count == 0
+        assert history.latest.metrics.counter('pipeline.batchOutputRecords.counter').count == 7
+
+        # And assert that in the database we end up having just 1 row, with the latest update value
+        assert len(data_from_database) == 1
+        assert data_from_database == CDC_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY_EXPECTED_DATA
+    finally:
+        logger.info('Dropping table %s in %s database ...', table_name, database.type)
+        table.drop(database.engine)
--- stage/test_jdbc_stages.py
+++ stage/test_jdbc_stages.py
@@ -27,6 +27,11 @@ from streamsets.testframework.utils import get_random_string
 
 logger = logging.getLogger(__name__)
 
+
+# from CommonDatabaseHeader.java
+PRIMARY_KEY_COLUMN_OLD_VALUE = 'jdbc.primaryKey.before'
+PRIMARY_KEY_COLUMN_NEW_VALUE = 'jdbc.primaryKey.after'
+
 ROWS_IN_DATABASE = [
     {'id': 1, 'name': 'Dima'},
     {'id': 2, 'name': 'Jarcec'},
@@ -38,6 +43,41 @@ ROWS_TO_UPDATE = [
 ]
 LOOKUP_RAW_DATA = ['id'] + [str(row['id']) for row in ROWS_IN_DATABASE]
 RAW_DATA = ['name'] + [row['name'] for row in ROWS_IN_DATABASE]
+
+CDC_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY = [
+    {'TYPE': 'Hobbit', 'ID': 1, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 0'},
+    {'TYPE': 'Fallohide', 'ID': 1, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 1'},
+    {'TYPE': 'Fallohide', 'ID': 2, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 2'},
+    {'TYPE': 'Hobbit - Fallohide', 'ID': 3, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 3'},
+    {'TYPE': 'Hobbit, Fallohide', 'ID': 3, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 4'},
+    {'TYPE': 'Hobbit, Fallohide', 'ID': 4, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 5'},
+    {'TYPE': 'Hobbit, Fallohide', 'ID': 4, 'NAME': 'Bilbo', 'SURNAME': 'Baggins', 'ADDRESS': 'Bag End 6'}
+]
+CDC_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY_EXPECTED_DATA = [
+    ('Hobbit, Fallohide', 4, 'Bilbo', 'Baggins', 'Bag End 6')
+]
+CDC_PK_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY_HEADER = [
+    {'sdc.operation.type': 1, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 1, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 1,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Hobbit', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Hobbit'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 1, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 1,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Hobbit', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Fallohide'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 1, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 2,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Fallohide', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Fallohide'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 2, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 3,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Fallohide', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Hobbit - Fallohide'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 3, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 3,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Hobbit - Fallohide', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Hobbit, Fallohide'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 3, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 4,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Hobbit, Fallohide', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Hobbit, Fallohide'},
+
+    {'sdc.operation.type': 3, f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID': 4, f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID': 4,
+     f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE': 'Hobbit, Fallohide', f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE': 'Hobbit, Fallohide'},
+]
 
 DEFAULT_DB2_SCHEMA = 'DB2INST1'
 
@@ -1056,3 +1096,108 @@ def test_jdbc_lookup_processor_incorrect_query_for_data(sdc_builder, sdc_executo
         if not keep_data:
             logger.info('Dropping table %s in %s database ...', table_name, database.type)
             table.drop(database.engine)
+
+
+@sdc_min_version('5.2.0')
+@database
+@pytest.mark.parametrize('multi_row', [False, True])
+def test_jdbc_tee_processor_primary_key_header_update(sdc_builder, sdc_executor, database, multi_row):
+    """
+    Test to make sure Primary Key updates are handled correctly. We will insert one record, and then
+    perform multiple updates to its columns (including Primary Key columns) and check we end up
+    having just one row in the table, with the expected values.
+    The pipeline looks like:
+
+        dev_raw_data_source >> expression_evaluator >> field_remover >> jdbc_tee >> wiretap
+    """
+    if isinstance(database, OracleDatabase):
+        pytest.skip('JDBC Tee Processor does not support Oracle')
+    elif type(database) == SQLServerDatabase:
+        pytest.skip('JDBC Tee Processor does not support SQL Server')
+
+    table_name = get_random_string(string.ascii_lowercase, 20)
+
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    # Build Dev Raw Data Source
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+    rows = copy.deepcopy(CDC_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY)
+    for row, header in zip(rows, CDC_PK_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY_HEADER):
+        row['HEADER'] = header
+    raw_data = '\n'.join((json.dumps(row) for row in rows))
+    dev_raw_data_source.set_attributes(data_format='JSON',
+                                       raw_data=raw_data,
+                                       stop_after_first_batch=True)
+
+    # Build Expression Evaluator
+    expression_evaluator = pipeline_builder.add_stage('Expression Evaluator')
+    # We assume CDC headers are generated properly
+    expression_evaluator.set_attributes(header_attribute_expressions=[
+        {'attributeToSet': 'sdc.operation.type',
+         'headerAttributeExpression': "${record:value('/HEADER/sdc.operation.type')}"},
+        {'attributeToSet': f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.ID',
+         'headerAttributeExpression': "${record:value('/HEADER/" + PRIMARY_KEY_COLUMN_OLD_VALUE + ".ID')}"},
+        {'attributeToSet': f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.ID',
+         'headerAttributeExpression': "${record:value('/HEADER/" + PRIMARY_KEY_COLUMN_NEW_VALUE + ".ID')}"},
+        {'attributeToSet': f'{PRIMARY_KEY_COLUMN_OLD_VALUE}.TYPE',
+         'headerAttributeExpression': "${record:value('/HEADER/" + PRIMARY_KEY_COLUMN_OLD_VALUE + ".TYPE')}"},
+        {'attributeToSet': f'{PRIMARY_KEY_COLUMN_NEW_VALUE}.TYPE',
+         'headerAttributeExpression': "${record:value('/HEADER/" + PRIMARY_KEY_COLUMN_NEW_VALUE + ".TYPE')}"}
+    ])
+
+    # Build Field Remover
+    field_remover = pipeline_builder.add_stage('Field Remover')
+    field_remover.fields = ['/HEADER']
+
+    # Jdbc Producer
+    jdbc_tee = pipeline_builder.add_stage('JDBC Tee')
+    # Note that here ids are not inserted. Database generates them automatically.
+    jdbc_tee.set_attributes(default_operation='INSERT',
+                            field_to_column_mapping=[],
+                            generated_column_mappings=[],
+                            use_multi_row_operation=multi_row,
+                            table_name=table_name)
+    if isinstance(database, PostgreSqlDatabase):
+        jdbc_tee.set_attributes(enclose_table_name=True)
+
+    wiretap = pipeline_builder.add_wiretap()
+
+    dev_raw_data_source >> expression_evaluator >> field_remover >> jdbc_tee >> wiretap.destination
+
+    pipeline = pipeline_builder.build().configure_for_environment(database)
+    sdc_executor.add_pipeline(pipeline)
+
+    table = sqlalchemy.Table(table_name, sqlalchemy.MetaData(),
+                             sqlalchemy.Column('TYPE', sqlalchemy.String(64), primary_key=True),
+                             sqlalchemy.Column('ID', sqlalchemy.Integer, primary_key=True),
+                             sqlalchemy.Column('NAME', sqlalchemy.String(64)),
+                             sqlalchemy.Column('SURNAME', sqlalchemy.String(64)),
+                             sqlalchemy.Column('ADDRESS', sqlalchemy.String(64)))
+    try:
+        logger.info('Creating source table %s in %s database ...', table_name, database.type)
+        table.create(database.engine)
+        sdc_executor.start_pipeline(pipeline).wait_for_finished()
+
+        result = database.engine.execute(table.select())
+        data_from_database = result.fetchall()
+        result.close()
+
+        # We assert that the pipeline processed 1 insert and 6 updates (the extra output is due to wiretap)
+        history = sdc_executor.get_pipeline_history(pipeline)
+        assert history.latest.metrics.counter('pipeline.batchInputRecords.counter').count == 7
+        assert history.latest.metrics.counter('pipeline.batchErrorRecords.counter').count == 0
+        assert history.latest.metrics.counter('pipeline.batchOutputRecords.counter').count == 7 + 1
+
+        # And assert that in the database we end up having just 1 row, with the latest update value
+        assert len(data_from_database) == 1
+        assert data_from_database == CDC_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY_EXPECTED_DATA
+
+        # And we will also check that the processor generated records with the corresponding updated values
+        assert len(wiretap.output_records) == 7
+        for record, expected_data in zip(wiretap.output_records, CDC_UPDATES_ROWS_IN_DATABASE_COMPOSITE_KEY):
+            assert len(record.field) == len(expected_data.keys())  # make sure we have the number of fields needed
+            for field in record.field:
+                assert record.field[field] == expected_data[field]
+    finally:
+        logger.info('Dropping table %s in %s database ...', table_name, database.type)
+        table.drop(database.engine)
