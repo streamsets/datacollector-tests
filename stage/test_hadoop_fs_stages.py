2ecf2e31005cef1680fa271abbe3572d60170753
Albert Farres <albert.farres@streamsets.com>
2022-11-02 16:13:32 +0100

    COLLECTOR-1746. Update min SDC version
    Change-Id: Iabb4d34668b512531f386ef535ef1174af46e550
    Reviewed-on: https://review.streamsets.net/c/datacollector-tests/+/54675
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Mikel Solabarrieta Román <mikelsr@streamsets.com>

--- stage/test_hadoop_fs_stages.py
+++ stage/test_hadoop_fs_stages.py
@@ -376,7 +376,7 @@ def test_hadoop_fs_origin_standalone_archive(sdc_builder, sdc_executor, cluster,
         fs.delete(archive_dir, recursive=True)
 
 
-@sdc_min_version('3.2.0.0')
+@sdc_min_version('5.3.0')
 @cluster('cdh', 'hdp')
 @pytest.mark.parametrize('filename', ['file.txt', '_tmp_file.txt', '.tmp_file.txt'])
 def test_hadoop_fs_origin_standalone_subdirectories(sdc_builder, sdc_executor, cluster, filename):
