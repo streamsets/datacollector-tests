81674f7e2116f2ad8da6edfa9f0dd1afb9b9ea92
Alex Sánchez <alex.sanchez@streamsets.com>
2022-11-05 18:42:39 +0100

    COLLECTOR-1800. Tests for COLLECTOR-1799: Field Flattener should have an Output type configurable
    Change-Id: Ieca402f5bee1522ea40a4cbb2afa17f512a99c1b
    Reviewed-on: https://review.streamsets.net/c/datacollector-tests/+/54781
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Fran Martinez Vera <fran@streamsets.com>

--- stage/test_field_processors.py
+++ stage/test_field_processors.py
@@ -19,6 +19,7 @@ import pytest
 import time
 from datetime import datetime
 from decimal import Decimal
+from collections import OrderedDict
 
 from streamsets.sdk.utils import Version
 from streamsets.testframework.markers import sdc_min_version
@@ -70,11 +71,52 @@ def test_field_flattener(sdc_builder, sdc_executor):
     sdc_executor.start_pipeline(pipeline).wait_for_finished()
 
     new_value = wiretap.output_records[0].field
-    # assert remove_flatten_field
     assert 'address' not in new_value['contact']
-    # assert flatten_target_field with name_seperator
     assert f'home{name_separator}state' in new_value['newcontact']['address']
     assert f'home{name_separator}zipcode' in new_value['newcontact']['address']
+
+
+@sdc_min_version('5.3.0')
+def test_field_flattener_listmap(sdc_builder, sdc_executor):
+    """Test field flattener processor. The pipeline would look like:
+
+        dev_raw_data_source >> field_flattener >> wiretap
+    """
+    raw_data = """
+        {
+          "contact": {
+             "name": "Jane Smith",
+             "id": "557",
+             "address": {
+               "home": {
+                 "state": "NC",
+                 "zipcode": "27023"
+                  }
+              }
+          }
+        }
+    """
+
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+    dev_raw_data_source.set_attributes(data_format='JSON', raw_data=raw_data, stop_after_first_batch=True)
+    field_flattener = pipeline_builder.add_stage('Field Flattener')
+    field_flattener.set_attributes(name_separator='.', remove_flattened_field=True, output_type='LIST_MAP')
+    wiretap = pipeline_builder.add_wiretap()
+
+    dev_raw_data_source >> field_flattener >> wiretap.destination
+    pipeline = pipeline_builder.build('Field Flattener pipeline')
+    sdc_executor.add_pipeline(pipeline)
+    sdc_executor.start_pipeline(pipeline).wait_for_finished()
+
+    output_value = wiretap.output_records[0].field
+
+    assert isinstance(output_value, OrderedDict)
+
+    assert output_value['"contact.name"'] == 'Jane Smith'
+    assert output_value['"contact.id"'] == '557'
+    assert output_value['"contact.address.home.state"'] == 'NC'
+    assert output_value['"contact.address.home.zipcode"'] == '27023'
 
 
 def test_field_flattener_all(sdc_builder, sdc_executor):
@@ -104,7 +146,7 @@ def test_field_flattener_all(sdc_builder, sdc_executor):
     dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
     dev_raw_data_source.set_attributes(data_format='JSON', raw_data=raw_data, stop_after_first_batch=True)
     field_flattener = pipeline_builder.add_stage('Field Flattener')
-    field_flattener.set_attributes(flatten='ENTIRE_RECORD', name_separator=name_separator)
+    field_flattener.set_attributes(flatten='ENTIRE_RECORD', name_separator=name_separator, output_type='MAP')
     wiretap = pipeline_builder.add_wiretap()
 
     dev_raw_data_source >> field_flattener >> wiretap.destination
@@ -113,6 +155,7 @@ def test_field_flattener_all(sdc_builder, sdc_executor):
     sdc_executor.start_pipeline(pipeline).wait_for_finished()
 
     new_value = wiretap.output_records[0].field
+    assert isinstance(new_value, dict)
     # assert everything has been flattened and there's no extra fields
     assert new_value[f'contact{name_separator}name'] == 'Jane Smith'
     assert new_value[f'contact{name_separator}id'] == '557'
@@ -527,82 +570,6 @@ def test_field_replacer(sdc_builder, sdc_executor):
             rec['statistics'] = None
     actual_data = [rec.field for rec in wiretap.output_records]
     assert actual_data == winners
-
-
-@sdc_min_version('5.3.0')
-def test_field_replacer_non_existent_fields(sdc_builder, sdc_executor):
-    """Test field replacer processor for non-existent fields. The pipeline would look like:
-
-        dev_raw_data_source >> [field_replacer1, field_replacer2]
-        field_replacer1 >> wiretap1
-        field_replacer2 >> wiretap2
-
-    With the given replacement rules, we do following:
-    Field Replacer 1 -
-        change value of 'name' field to be 'unknown',
-        skip processing 'level' (a non-existent field),
-        set value of 'laps' to null,
-        skip processing 'cars' (a non-existent field)
-
-    Field Replacer 2 -
-        change value of 'name' field to be 'unknown',
-        add field 'level' (a non-existent field) with value 'high',
-        set value of 'laps' to null,
-        add field 'cars' (a non-existent field) and set it to null
-    """
-    records = [dict(id=1, name='Harvey', laps=10),
-               dict(id=2, name='Mike', laps=8)]
-    raw_data = ''.join([json.dumps(record) for record in records])
-
-    pipeline_builder = sdc_builder.get_pipeline_builder()
-    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
-    dev_raw_data_source.set_attributes(data_format='JSON', raw_data=raw_data, stop_after_first_batch=True)
-
-    field_replacer_1 = pipeline_builder.add_stage('Field Replacer')
-    field_replacer_2 = pipeline_builder.add_stage('Field Replacer')
-
-    # Case 1 - Include without processing : Skip non-existent fields
-    field_replacer_1.set_attributes(field_does_not_exist='CONTINUE')
-    field_replacer_1.replacement_rules = [{'setToNull': False, 'fields': '/name', 'replacement': 'unknown'},
-                                          {'setToNull': False, 'fields': '/level', 'replacement': 'high'},
-                                          {'setToNull': True, 'fields': '/laps'},
-                                          {'setToNull': True, 'fields': '/cars'}]
-    wiretap_1 = pipeline_builder.add_wiretap()
-
-    # Case 2 - Add if not exist : Add non-existent fields
-    field_replacer_2.set_attributes(field_does_not_exist='ADD_FIELD')
-    field_replacer_2.replacement_rules = [{'setToNull': False, 'fields': '/name', 'replacement': 'unknown'},
-                                          {'setToNull': False, 'fields': '/level', 'replacement': 'high'},
-                                          {'setToNull': True, 'fields': '/laps'},
-                                          {'setToNull': True, 'fields': '/cars'}]
-    wiretap_2 = pipeline_builder.add_wiretap()
-
-    dev_raw_data_source >> [field_replacer_1, field_replacer_2]
-    field_replacer_1 >> wiretap_1.destination
-    field_replacer_2 >> wiretap_2.destination
-
-    pipeline = pipeline_builder.build('Field Replacer Pipeline')
-    sdc_executor.add_pipeline(pipeline)
-    sdc_executor.start_pipeline(pipeline).wait_for_finished()
-
-    records_1 = records
-    records_2 = records
-
-    for rec in records_1:
-        rec['name'] = 'unknown'
-        rec['laps'] = None
-    actual_data_1 = [rec.field for rec in wiretap_1.output_records]
-    # assert records_1 data
-    assert actual_data_1 == records_1
-
-    for rec in records_2:
-        rec['name'] = 'unknown'
-        rec['level'] = 'high'
-        rec['laps'] = None
-        rec['cars'] = None
-    actual_data_2 = [rec.field for rec in wiretap_2.output_records]
-    # assert records_2 data
-    assert actual_data_2 == records_2
 
 
 @sdc_min_version('3.1.0.0')
