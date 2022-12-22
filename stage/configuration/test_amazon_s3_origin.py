68fa32a612927e75f88b1e939689043578c9fab0
Neeraj Jain <neeraj@streamsets.com>
2022-12-22 18:11:19 +0530

    COLLECTOR-2068. Update AWS Regions in Tests
    Change-Id: Ib036b6cee56e7ce3dff76d99df69c9d0a9b2b60f
    Reviewed-on: https://review.streamsets.net/c/datacollector-tests/+/56229
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Alex Sanchez Cabana <alex.sanchez@streamsets.com>

--- stage/configuration/test_amazon_s3_origin.py
+++ stage/configuration/test_amazon_s3_origin.py
@@ -158,10 +158,12 @@ def test_configuration_proxy_user(sdc_builder, sdc_executor, use_proxy):
 
 
 @pytest.mark.parametrize('region',
-                         ['AP_NORTHEAST_1', 'AP_NORTHEAST_2', 'AP_NORTHEAST_3', 'AP_SOUTHEAST_1', 'AP_SOUTHEAST_2',
-                          'AP_SOUTH_1', 'CA_CENTRAL_1', 'CN_NORTHWEST_1', 'CN_NORTH_1', 'EU_CENTRAL_1', 'EU_WEST_1',
-                          'EU_WEST_2', 'EU_WEST_3', 'OTHER', 'SA_EAST_1', 'US_EAST_1', 'US_EAST_2', 'US_GOV_WEST_1',
-                          'US_WEST_1', 'US_WEST_2'])
+                         ['AF_SOUTH_1', 'AP_EAST_1', 'AP_NORTHEAST_1', 'AP_NORTHEAST_2', 'AP_NORTHEAST_3',
+                          'AP_SOUTHEAST_1', 'AP_SOUTHEAST_2', 'AP_SOUTHEAST_3', 'AP_SOUTH_1', 'AP_SOUTH_2',
+                          'CA_CENTRAL_1', 'CN_NORTHWEST_1', 'CN_NORTH_1', 'EU_CENTRAL_1', 'EU_CENTRAL_2',
+                          'EU_WEST_1', 'EU_WEST_2', 'EU_WEST_3', 'EU_NORTH_1', 'EU_SOUTH_1', 'EU_SOUTH_2',
+                          'ME_CENTRAL_1', 'ME_SOUTH_1', 'OTHER', 'SA_EAST_1', 'US_EAST_1', 'US_EAST_2',
+                          'US_GOV_EAST_1', 'US_GOV_WEST_1', 'US_WEST_1', 'US_WEST_2'])
 @pytest.mark.skip('Not yet implemented')
 def test_configuration_region(sdc_builder, sdc_executor, region):
     pass
