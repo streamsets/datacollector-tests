67aa2731427d3b114e6cdac8c09eef6e12f0793a
Esha Jain <esha@streamsets.com>
2023-04-25 11:04:35 +0530

    COLLECTOR-2777. Migrate generator test to datacollector test
    Change-Id: I06a141872e7cb903e2a07e83b35e1018b0ef1505
    Reviewed-on: https://review.streamsets.net/c/datacollector-tests/+/60385
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Shreyans Shrimal <shreyans@streamsets.com>

--- stage/configuration/test_data_generator_processor.py
+++ stage/configuration/test_data_generator_processor.py
@@ -24,10 +24,43 @@ def test_on_record_error(sdc_builder, sdc_executor, stage_attributes):
     pass
 
 
-@stub
 @pytest.mark.parametrize('stage_attributes', [{'output_type': 'BYTE_ARRAY'}, {'output_type': 'STRING'}])
 def test_output_type(sdc_builder, sdc_executor, stage_attributes):
-    pass
+    """Test Data Generator processor. The pipeline would look like:
+
+        dev_raw_data_source >> data_generator >> wiretap
+    """
+    raw_data = '{ "contact": { "name": "Jane Smith", "age":30 } }'
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+    dev_raw_data_source.set_attributes(data_format='JSON', raw_data=raw_data, stop_after_first_batch=True)
+
+    # Set output_type
+    data_generator = pipeline_builder.add_stage('Data Generator', type='processor')
+    data_generator.set_attributes(target_field='/', output_type=stage_attributes['output_type'], data_format='JSON')
+
+    wiretap = pipeline_builder.add_wiretap()
+
+    dev_raw_data_source >> data_generator >> wiretap.destination
+
+    try:
+        pipeline = pipeline_builder.build()
+        sdc_executor.add_pipeline(pipeline)
+        sdc_executor.start_pipeline(pipeline).wait_for_finished()
+
+        # Gather wiretap data as a list for verification.
+        assert len(wiretap.output_records) == 1
+        items = [record.field for record in wiretap.output_records]
+
+        if stage_attributes['output_type'] == 'BYTE_ARRAY':
+            expected_data = [b'{"contact":{"name":"Jane Smith","age":30}}']
+        elif stage_attributes['output_type'] == 'STRING':
+            expected_data = ['{"contact":{"name":"Jane Smith","age":30}}']
+
+        assert items == expected_data
+    finally:
+        sdc_executor.remove_pipeline(pipeline)
 
 
 @stub
