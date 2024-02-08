def4ecfe5de17cafe5cb4fb8ae046b86bb113f35
Albert Farres <albert.farres@streamsets.com>
2024-02-08 08:47:54 +0100

    COLLECTOR-4927. Add overwrite test for Azure Blob Storage destination
    Change-Id: Ide454c8d26f7932de419183ea4dac0eee382aa96
    Reviewed-on: https://review.streamsets.net/c/datacollector-tests/+/69440
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Dimas Cabré i Chacón <dimas.cabre.chacon@streamsets.com>

--- stage/test_azure_blob_storage_destination.py
+++ stage/test_azure_blob_storage_destination.py
@@ -18,6 +18,7 @@ import os
 import re
 
 import pytest
+from streamsets.sdk.exceptions import RunError
 from streamsets.testframework.markers import azure, sdc_min_version
 from streamsets.testframework.utils import get_random_string
 
@@ -149,6 +150,71 @@ def test_append(sdc_builder, sdc_executor, azure):
             f'Expected {batch_count * num_records} records but got {len(actual_data)}'
         for actual, expected in zip(actual_data, batch_count * data):
             assert actual == expected, f'Actual record {actual} does not match expected record {expected}'
+
+    finally:
+        logger.info('Azure Data Lake directory %s and underlying files will be deleted.', blob_dir)
+        dl_fs.rmdir(blob_dir, recursive=True)
+
+
+@pytest.mark.parametrize('overwrite', [True, False])
+def test_overwrite(sdc_builder, sdc_executor, azure, overwrite):
+    """ Writes records to Azure Blob Storage and then reads them back using the STF client. We assert read data is correct.
+    The pipeline looks like:
+
+    dev >> azure_blob_storage_destination
+    """
+
+    num_records = 10
+    blob_dir = get_random_string()
+    blob_prefix = os.path.join(blob_dir, 'test')
+    blob_suffix = ".json"
+    blob_name = f'{blob_prefix}-000000-000{blob_suffix}'
+    builder = sdc_builder.get_pipeline_builder()
+    dl_fs = azure.datalake.file_system
+
+    # create raw data
+    data = []
+    for id in range(num_records):
+        data.append({"id": id, "text": get_random_string()})
+
+    # dev raw data
+    dev_raw_data_source = builder.add_stage('Dev Raw Data Source')
+    raw_data = '\n'.join(json.dumps(row) for row in data)
+    dev_raw_data_source.set_attributes(data_format='JSON',
+                                       raw_data=raw_data,
+                                       stop_after_first_batch=True)
+
+    azure_blob_storage_destination = builder.add_stage(name=STAGE_NAME)
+    azure_blob_storage_destination.set_attributes(data_format='JSON',
+                                                  overwrite=overwrite,
+                                                  blob_prefix=blob_prefix,
+                                                  blob_suffix=blob_suffix)
+
+    dev_raw_data_source >> azure_blob_storage_destination
+
+    try:
+        dl_fs.touch(blob_name)
+        dl_fs.write(blob_name, get_random_string(length=1024))
+
+        pipeline = builder.build().configure_for_environment(azure)
+        sdc_executor.add_pipeline(pipeline)
+
+        if not overwrite:
+            with pytest.raises(RunError) as error:
+                sdc_executor.start_pipeline(pipeline).wait_for_finished()
+            assert "ADLS_13" in error.value.message, f'Expected a ADLS_13 error, got "{error.value.message}" instead'
+
+        else:
+            sdc_executor.start_pipeline(pipeline).wait_for_finished()
+
+            response = dl_fs.cat(blob_name).response
+            assert response.status_code == 200, f'Failed to read blob {blob_name}. Status code: {response.status_code}'
+            actual_data = response.content.decode('utf-8').split('\n')
+            actual_data = [json.loads(record) for record in actual_data]
+            assert len(actual_data) == num_records, f'Expected {num_records} records but got {len(actual_data)}'
+
+            for actual, expected in zip(actual_data, data):
+                assert actual == expected, f'Actual record {actual} does not match expected record {expected}'
 
     finally:
         logger.info('Azure Data Lake directory %s and underlying files will be deleted.', blob_dir)
