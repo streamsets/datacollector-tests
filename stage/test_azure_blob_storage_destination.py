7802cc6d5237d99755b610c3d1c8772d20c65ca7
Albert Farres <albert.farres@streamsets.com>
2024-02-15 08:54:44 +0100

    COLLECTOR-4963. Test for Add page blob type support in Azure Blob Storage destination
    Change-Id: I2357b70b1d894eb7bcc7fe40b38db70b006b75ba
    Reviewed-on: https://review.streamsets.net/c/datacollector-tests/+/69672
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Jaume Ballester Puig <jaume@streamsets.com>

--- stage/test_azure_blob_storage_destination.py
+++ stage/test_azure_blob_storage_destination.py
@@ -219,3 +219,77 @@ def test_overwrite(sdc_builder, sdc_executor, azure, overwrite):
     finally:
         logger.info('Azure Data Lake directory %s and underlying files will be deleted.', blob_dir)
         dl_fs.rmdir(blob_dir, recursive=True)
+
+
+@azure('datalake_page')
+def test_page(sdc_builder, sdc_executor, azure):
+    """ Append several batches to a page blob using Azure Blob Storage and then reads them back using the STF client.
+    We assert read data is correct.
+    The pipeline looks like:
+
+    dev >> azure_blob_storage_destination
+    """
+
+    num_records = 10
+    min_num_batches = 10
+    blob_dir = get_random_string()
+    blob_prefix = os.path.join(blob_dir, 'test')
+    blob_suffix = ".json"
+    blob_name = f'{blob_prefix}-000{blob_suffix}'
+    blob_size = 100 * 1024
+    page_size = 1024
+    pattern = r'}{'
+    builder = sdc_builder.get_pipeline_builder()
+    dl_fs = azure.datalake.file_system
+
+    # create raw data
+    data = []
+    for id in range(num_records):
+        data.append({"id": id, "text": get_random_string()})
+
+    # dev raw data
+    dev_raw_data_source = builder.add_stage('Dev Raw Data Source')
+    raw_data = '\n'.join(json.dumps(row) for row in data)
+    dev_raw_data_source.set_attributes(data_format='JSON',
+                                       raw_data=raw_data,
+                                       stop_after_first_batch=False)
+
+    azure_blob_storage_destination = builder.add_stage(name=STAGE_NAME)
+    azure_blob_storage_destination.set_attributes(data_format='JSON',
+                                                  blob_type='PAGE',
+                                                  blob_size=blob_size,
+                                                  page_size=page_size,
+                                                  blob_prefix=blob_prefix,
+                                                  blob_suffix=blob_suffix)
+
+    dev_raw_data_source >> azure_blob_storage_destination
+
+    pipeline = builder.build().configure_for_environment(azure)
+
+    sdc_executor.add_pipeline(pipeline)
+
+    try:
+        sdc_executor.start_pipeline(pipeline).wait_for_pipeline_batch_count(min_num_batches)
+        sdc_executor.stop_pipeline(pipeline)
+
+        history = sdc_executor.get_pipeline_history(pipeline)
+        batch_count = history.latest.metrics.counter('pipeline.batchCount.counter').count
+        assert min_num_batches <= batch_count, f'Expected at least {min_num_batches} batches but got {batch_count}'
+
+        response = dl_fs.cat(blob_name).response
+        assert response.status_code == 200, f'Failed to read blob {blob_name}. Status code: {response.status_code}'
+        actual_data = response.content.decode('utf-8')
+        assert len(actual_data) == blob_size, f'Expected {blob_size} bytes but got {len(actual_data)}'
+        for page_id in range(batch_count):
+            start_pos = page_id * page_size
+            end_pos = start_pos + page_size
+            actual_page = actual_data[start_pos:end_pos].rstrip('\x00')
+            actual_page = re.sub(pattern, '}\n{', actual_page).split("\n")
+            actual_page = [json.loads(record) for record in actual_page]
+            assert len(actual_page) == num_records, f'Expected {num_records} records but got {len(actual_page)}'
+            for actual, expected in zip(actual_page, data):
+                assert actual == expected, f'Actual record {actual} does not match expected record {expected}'
+
+    finally:
+        logger.info('Azure Data Lake directory %s and underlying files will be deleted.', blob_dir)
+        dl_fs.rmdir(blob_dir, recursive=True)
