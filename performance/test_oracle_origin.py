ef47a9c6bddd9c02b439308cff2e524b2a288808
TanyaVerma10 <tanya@streamsets.com>
2024-03-21 14:46:47 +0530

    COLLECTOR-3492. Oracle bulkload stage's performance.test_oracle_origin.test_oracle_consumer is failing
    Change-Id: I9c69fdfdfab5f54d8bc4a5ec94aec6d67e8d94a3
    Reviewed-on: https://review.streamsets.net/c/datacollector-tests/+/71158
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Adrià Aguilà <adria.aguila@streamsets.com>

--- performance/test_oracle_origin.py
+++ performance/test_oracle_origin.py
@@ -5,7 +5,6 @@ The tests in this module are for running high-volume pipelines, for the purpose 
 """
 
 import logging
-import uuid
 import pytest
 
 from streamsets.testframework.markers import database, sdc_min_version
@@ -20,22 +19,20 @@ def sdc_builder_hook():
         data_collector.SDC_JAVA_OPTS = '-Xmx8192m -Xms8192m'
     return hook
 
-@database('oracle')
+
+@database("oracle")
 @sdc_min_version('5.6.0')
-def test_oracle_consumer(sdc_builder, sdc_executor, database):
-    """Performance benchmark a simple Oracle Bulk Loader to trash pipeline.
-    The test uses an existing table in Oracle instance called VOLUME_TEST with 700000 records.
-    The pipeline connects oracle bulk loader to trash and includes a finisher"""
+def test_oracle_consumer(sdc_executor, sdc_builder, database, origin_table):
+    """Performance benchmark a simple Oracle Bulk Loader with some dummy records"""
 
     record_count = 700_000
     pipeline_builder = sdc_builder.get_pipeline_builder()
     oracle_consumer = pipeline_builder.add_stage('Oracle Bulkload')
-    oracle_consumer.set_attributes(tables=[dict(schemaName='', tableName='VOLUME_TEST')])
-    trash = pipeline_builder.add_stage('Trash')
-    finisher = pipeline_builder.add_stage('Pipeline Finisher Executor')
+    oracle_consumer.set_attributes(tables=[dict(schemaName='', tableName=origin_table.name)])
 
-    oracle_consumer >> trash
-    oracle_consumer >= finisher
+    benchmark_stages = pipeline_builder.add_benchmark_stages()
+    oracle_consumer >> benchmark_stages.destination
 
     pipeline = pipeline_builder.build().configure_for_environment(database)
+    origin_table.load_records(record_count)
     sdc_executor.benchmark_pipeline(pipeline, record_count=record_count, runs=1)
