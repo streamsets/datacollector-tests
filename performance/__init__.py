1b5b12c0eac8b7f28ad88122591a63cc3ab7a0aa
TanyaVerma10 <tanya@streamsets.com>
2024-03-22 13:03:37 +0530

    COLLECTOR-5187. Revert the copyright year of the files to original year for Datacollector-tests
    Change-Id: I0218ed78964b4e9365bc199bff183efdce9998d6
    Reviewed-on: https://review.streamsets.net/c/datacollector-tests/+/71234
    Reviewed-by: Neeraj Jain <neeraj@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- performance/__init__.py
+++ performance/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2023 StreamSets Inc.
+# Copyright 2017 StreamSets Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
