2918d2efacb8cf1acc244d8a674f6118b1756faf
Maria Claver <maria.claver@streamsets.com>
2024-10-30 09:37:49 +0100

    COLLECTOR-6183. Test for new config "propagate headers" in kafka destination added in COLLECTOR-5968.
    * Added test for different possible values of new config "propagate headers" of kafka destination stage in test_kafka_stages.py.
    
    Change-Id: Icca3a0919819da0b5a4cf1ae08c3be5e5069aec7
    Reviewed-on: https://review.streamsets.net/c/datacollector-tests/+/78976
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>
    Reviewed-by: Farners Vallespí i Soro <farners.vallespi.i.soro@ibm.com>

--- stage/test_kafka_stages.py
+++ stage/test_kafka_stages.py
@@ -16,11 +16,13 @@ import logging
 import string
 
 import pytest
+
 from streamsets.testframework.environments.kafka import KafkaCluster
 from streamsets.testframework.markers import cluster, sdc_min_version
 from streamsets.testframework.utils import get_random_string, Version
 
 logger = logging.getLogger(__name__)
+
 
 @cluster('cdh', 'kafka')
 @sdc_min_version('5.3.0')
@@ -51,7 +53,7 @@ def test_kafka_headers(sdc_builder, sdc_executor, cluster,
     consumer_builder = sdc_builder.get_pipeline_builder()
     consumer_origin = consumer_builder.add_stage('Kafka Multitopic Consumer')
     consumer_origin.set_attributes(data_format="JSON",
-                                        topic_list=[topic])
+                                   topic_list=[topic])
     consumer_wiretap = consumer_builder.add_wiretap()
     consumer_origin >> consumer_wiretap.destination
     consumer_pipeline = consumer_builder.build().configure_for_environment(cluster)
@@ -70,3 +72,83 @@ def test_kafka_headers(sdc_builder, sdc_executor, cluster,
         assert False, "Header key not found"
     except AssertionError:
         assert False, "Header value not found"
+
+
+@cluster('cdh', 'kafka')
+@sdc_min_version('6.1.0')
+@pytest.mark.parametrize('propagate', [True, False])
+def test_kafka_destination_propagate_headers(propagate, sdc_builder, sdc_executor, cluster):
+    """
+    Test the behavior of the new configuration property "Propagate Headers" for Kafka Destination stage.
+    When set to True, the Kafka attributes are propagated into the records as headers. This is the default behavior
+    since SDC 5.3.0. For previous SDC versions, the behavior was to ignore them, which corresponds to setting the
+    new configuration to False.
+
+    Testing with simple pipeline:
+        dev_data_generator >> kafka_destination
+    """
+    if isinstance(cluster, KafkaCluster) and Version(cluster.version) < Version("0.11.0"):
+        pytest.skip(f'Kafka version {cluster.version} does not support headers')
+
+    records_to_be_generated = 100
+    topic = f'stf_{get_random_string(string.ascii_letters, 10)}'
+    key = f'stf_{get_random_string(string.ascii_letters, 10)}'
+    value = f'stf_{get_random_string(string.ascii_letters, 10)}'
+
+    producer_builder = sdc_builder.get_pipeline_builder()
+
+    producer_origin = producer_builder.add_stage('Dev Data Generator')
+    producer_origin.set_attributes(records_to_be_generated=records_to_be_generated)
+    producer_origin.fields_to_generate = [{'field': 'foo', 'type': 'STRING'}]
+    producer_origin.header_attributes = [{"key": key, "value": value}]
+
+    producer_destination = producer_builder.add_stage('Kafka Producer')
+    producer_destination.set_attributes(data_format="JSON",
+                                        propagate_headers=propagate,
+                                        topic=topic)
+
+    producer_origin >> producer_destination
+
+    producer_pipeline = producer_builder.build().configure_for_environment(cluster)
+
+    consumer_builder = sdc_builder.get_pipeline_builder()
+
+    consumer_origin = consumer_builder.add_stage('Kafka Multitopic Consumer')
+    consumer_origin.set_attributes(data_format="JSON",
+                                   topic_list=[topic])
+    consumer_wiretap = consumer_builder.add_wiretap()
+
+    consumer_origin >> consumer_wiretap.destination
+
+    consumer_pipeline = consumer_builder.build().configure_for_environment(cluster)
+
+    try:
+        sdc_executor.add_pipeline(producer_pipeline)
+        sdc_executor.start_pipeline(producer_pipeline).wait_for_finished()
+
+        sdc_executor.add_pipeline(consumer_pipeline)
+        sdc_executor.start_pipeline(consumer_pipeline)
+        sdc_executor.wait_for_pipeline_metric(consumer_pipeline, 'output_record_count', records_to_be_generated)
+
+        output_records = consumer_wiretap.output_records
+
+        for i in range(len(output_records)):
+            if propagate:
+                try:
+                    assert output_records[i].header.values[key] == value
+                except KeyError:
+                    assert False, "Header key not found"
+                except AssertionError:
+                    assert False, "Header value not found"
+            else:
+                assert key not in output_records[i].header.values
+
+    finally:
+        try:
+            sdc_executor.stop_pipeline(producer_pipeline, force=True)
+        except Exception:
+            pass
+        try:
+            sdc_executor.stop_pipeline(consumer_pipeline, force=True)
+        except Exception:
+            pass
