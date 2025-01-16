3a2577c4470d6b3101bd1b713aad47c78edfdb1a
Alex Sánchez <alex.sanchez@streamsets.com>
2025-01-16 13:39:08 +0100

    COLLECTOR-6474. Groovy tests for COLLECTOR-1473: Scripting processors don't preserve field headers
    Change-Id: I18b0089b3b721d75f659cfaa4fd07d4cb9602b82
    Reviewed-on: https://review.streamsets.net/c/datacollector-tests/+/80955
    Reviewed-by: Oriol Porta <oriol.porta@streamsets.com>
    Tested-by: StreamSets CI <streamsets-ci-spam@streamsets.com>

--- stage/test_groovy_processors.py
+++ stage/test_groovy_processors.py
@@ -392,3 +392,86 @@ def test_script_error(sdc_builder, sdc_executor, on_script_error, groovy_version
     except Exception as e:
         assert len(wiretap.error_records) == 0
         assert 'SCRIPTING_06' in e.message
+
+
+
+@sdc_min_version('6.2.0')
+@pytest.mark.parametrize('groovy_version,library,min_sdc_version', GROOVY_LIBS, ids=[i[0] for i in GROOVY_LIBS])
+def test_groovy_field_attributes(sdc_builder, sdc_executor, groovy_version, library, min_sdc_version):
+    """Test that Groovy evaluator preserves field header attributes
+
+    The pipeline would look like:
+
+        dev_raw_data_source >> expression_evaluator>> groovy_evaluator >> wiretap
+    """
+    utc_datetime_str = '1978-01-05 19:38:01'
+    raw_str_value = 'hello again!'
+    # note, date time here is in UTC. Each map is an SDC record to process.
+    raw_col = [{'amInteger': 123, 'amDouble': 12345.6789115, 'amString': 'hello', 'amBool': True, 'amDateTime': utc_datetime_str, 'amString2': raw_str_value, 'amZonedDateTime': None}]
+    raw_data = json.dumps(raw_col)
+
+    pipeline_builder = sdc_builder.get_pipeline_builder()
+    dev_raw_data_source = pipeline_builder.add_stage('Dev Raw Data Source')
+    dev_raw_data_source.set_attributes(data_format='JSON', json_content='ARRAY_OBJECTS', raw_data=raw_data,
+                                       stop_after_first_batch=True)
+
+    expression_evaluator = pipeline_builder.add_stage('Expression Evaluator')
+    expression_evaluator.set_attributes(field_attribute_expressions=[
+        {
+            "fieldToSet": "/amInteger",
+            "attributeToSet": "amInteger",
+            "fieldAttributeExpression": "${\"amInteger\"}"
+        },{
+            "fieldToSet": "/amDouble",
+            "attributeToSet": "amDouble",
+            "fieldAttributeExpression": "${\"amDouble\"}"
+        },{
+            "fieldToSet": "/amString",
+            "attributeToSet": "amString",
+            "fieldAttributeExpression": "${\"amString\"}"
+        },{
+            "fieldToSet": "/amBool",
+            "attributeToSet": "amBool",
+            "fieldAttributeExpression": "${\"amBool\"}"
+        },{
+            "fieldToSet": "/amDateTime",
+            "attributeToSet": "amDateTime",
+            "fieldAttributeExpression": "${\"amDateTime\"}"
+        },{
+            "fieldToSet": "/amString2",
+            "attributeToSet": "amString2",
+            "fieldAttributeExpression": "${\"amString2\"}"
+        },{
+            "fieldToSet": "/amZonedDateTime",
+            "attributeToSet": "amZonedDateTime",
+            "fieldAttributeExpression": "${\"amZonedDateTime\"}"
+        },
+    ])
+
+    groovy_evaluator = pipeline_builder.add_stage('Groovy Evaluator', type='processor')
+
+    wiretap = pipeline_builder.add_wiretap()
+
+    dev_raw_data_source >> expression_evaluator >> groovy_evaluator >> wiretap.destination
+    pipeline = pipeline_builder.build()
+    sdc_executor.add_pipeline(pipeline)
+    sdc_executor.start_pipeline(pipeline).wait_for_finished()
+
+    # assert data type conversion
+    record = wiretap.output_records[0]
+
+    assert record.field['amInteger'].value == 123
+    assert record.field['amDouble'].value == 12345.6789115
+    assert record.field['amString'].value == 'hello'
+    assert record.field['amBool'].value == True
+    assert record.field['amDateTime'].value == utc_datetime_str
+    assert record.field['amString2'].value == raw_str_value
+    assert record.field['amZonedDateTime'].value is None
+
+    assert str(record.get_field_attributes('amInteger')) == "{'amInteger': 'amInteger'}"
+    assert str(record.get_field_attributes('amDouble')) == "{'amDouble': 'amDouble'}"
+    assert str(record.get_field_attributes('amString')) == "{'amString': 'amString'}"
+    assert str(record.get_field_attributes('amBool')) == "{'amBool': 'amBool'}"
+    assert str(record.get_field_attributes('amDateTime')) == "{'amDateTime': 'amDateTime'}"
+    assert str(record.get_field_attributes('amString2')) == "{'amString2': 'amString2'}"
+    assert str(record.get_field_attributes('amZonedDateTime')) == "{'amZonedDateTime': 'amZonedDateTime'}"
